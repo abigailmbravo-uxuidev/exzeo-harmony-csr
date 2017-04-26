73b9207316051a47c1cb871b1e868834359951e7
HCI01\meads <meads@hcpci.com>
2017-04-26 16:36:55 -0400

    added quote base

---
+++ src/containers/QuoteBase.jsx
@@ -0,0 +1,45 @@
+import React, { PropTypes } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import Header from '../components/Common/Header';
+import SideNav from '../components/Common/SideNav';
+import * as userActions from '../actions/userActions';
+
+/*
+const handleLogout = (props) => {
+  props.actions.user.logout();
+};
+*/
+
+export const QuoteBase = props => (
+  <div className="app-wrapper csr">
+    <Header />
+    <main role="document">
+      <aside className="content-panel-left">
+        <div className="csr">
+          <h3>CSR Portal for Quote</h3>
+        </div>
+        <SideNav />
+      </aside>
+      <div className="content-wrapper">
+        {props.children}
+      </div>
+    </main>
+  </div>
+);
+
+
+QuoteBase.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node])
+};
+
+const mapStateToProps = state => ({
+  user: state.user
+});
+const mapDispatchToProps = dispatch => ({
+  actions: {
+    user: bindActionCreators(userActions, dispatch)
+  }
+});
+export default connect(mapStateToProps, mapDispatchToProps)(QuoteBase);
