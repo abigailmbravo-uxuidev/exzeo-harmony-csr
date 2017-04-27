dae146385137e3e74213b0730d96d7ea437fee3a
Joe Sutphin <jsutphin@exzeo.com>
2017-04-27 08:41:00 -0400

    update quote content

--- src/containers/Coverage.jsx
+++ src/containers/Coverage.jsx
@@ -3,74 +3,18 @@ import PropTypes from 'prop-types';
 import QuoteBaseConnect from './QuoteBase';
 import ClearErrorConnect from '../components/Error/ClearError';
 
-
 const Coverage = () => (
   <QuoteBaseConnect>
-    <ClearErrorConnect />
-
-      <div className="route">
-        <div className="detailHeader">
-      <section id="quoteDetails" className="quoteDetails">
-        <dl>
-          <div>
-            <dt className="fade">Quote Number</dt>
-            <dd className="fade">-</dd>
-          </div>
-        </dl>
-      </section>
-      <section id="propertyDetails" className="propertyDetails">
-        <dl>
-          <div>
-            <dt>Address</dt>
-            <dd className="fade">123 NE 132ND TER</dd>
-            <dd className="fade"></dd>
-            <dd className="fade"></dd>
-          </div>
-        </dl>
-      </section>
-      <section id="yearBuilt" className="yearBuilt">
-        <dl>
-          <div>
-            <dt className="fade">Year Built</dt>
-            <dd className="fade">1980</dd>
-          </div>
-        </dl>
-      </section>
-      <section id="coverageDetails" className="coverageDetails">
-        <dl>
-          <div>
-            <dt className="fade">Coverage A</dt>
-            <dd className="fade"></dd>
-          </div>
-        </dl>
-      </section>
-      <section id="premium" className="premium">
-        <dl>
-          <div>
-            <dt className="fade">Premium</dt>
-            <dd className="fade">
-
-
-              </dd>
-          </div>
-        </dl>
-      </section>
-    </div>
-
+    <ClearErrorConnect/>
     <div className="route-content">
-          <form id="CustomerInfo" novalidate="">
-            <div className="scroll">
-                test monkey
-            </div>
-          </form>
+      <form>
+        <div className="scroll">
+          <div className="form-group survey-wrapper" role="group">
+            test
+          </div>
         </div>
-
-      </div>
-
-
-
-
-
+      </form>
+    </div>
   </QuoteBaseConnect>
 );
 
