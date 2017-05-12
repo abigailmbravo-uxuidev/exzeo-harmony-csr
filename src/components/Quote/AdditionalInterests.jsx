3f45185a7b62f4e1fb004e636c58ce8896de3e91
Joe Sutphin <jsutphin@exzeo.com>
2017-05-12 06:26:34 -0400

    add list & temp list item

--- src/components/Quote/AdditionalInterests.jsx
+++ src/components/Quote/AdditionalInterests.jsx
@@ -7,11 +7,10 @@ const AdditionalInterests = () => <QuoteBaseConnect>
   <ClearErrorConnect/>
 
   <div className="route-content">
-    <form id="AdditionalInterests">
+    <form id="AddAdditionalInterestPage">
       <div className="scroll">
         <div className="form-group survey-wrapper" role="group">
           <h2>Additional Interests</h2>
-          <p>To add additional interests ...</p>
           <div className="button-group">
             <button className="btn btn-sm btn-secondary" type="button"> <div><i className="fa fa-plus"/><span>Mortgagee</span></div></button>
             <button className="btn btn-sm btn-secondary" type="button"><div><i className="fa fa-plus"/><span>Lienholder</span></div></button>
@@ -19,11 +18,31 @@ const AdditionalInterests = () => <QuoteBaseConnect>
             <button className="btn btn-sm btn-secondary" type="button"><div><i className="fa fa-plus"/><span>Additional Interest</span></div></button>
             <button className="btn btn-sm btn-secondary" type="button"><div><i className="fa fa-plus"/><span>Billpayer</span></div></button>
           </div>
-          
+
+
+
+          <div className="results-wrapper">
+            <ul className="results result-cards">
+                <li>
+                  <a>
+                    {/* add className based on type - i.e. mortgagee could have class of mortgagee*/}
+                    <div className="card-icon"><i className={`fa fa-circle`} /><label>question.type question.order + 1</label></div>
+                    <section><h4>question.name1</h4><p>question.name2</p><p className="address">question.mailingAddress.address1,
+                      question.mailingAddress.address2
+                      question.mailingAddress.city, question.mailingAddress.state question.mailingAddress.zip</p></section>
+                    <i className="fa fa-pencil" />
+                  </a>
+                </li>
+            </ul>
+          </div>
+
+
+
+
 
           <div className="btn-footer">
-            <button className="btn btn-secondary" form="AdditionalInterests">Cancel</button>
-            <button className="btn btn-primary" type="submit" form="AdditionalInterests">Update</button>
+            <button className="btn btn-secondary" form="AddAdditionalInterestPage">Cancel</button>
+            <button className="btn btn-primary" type="submit" form="AddAdditionalInterestPage">Update</button>
           </div>
         </div>
       </div>
