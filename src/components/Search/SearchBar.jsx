4a6ca065e28445a5160d1170cf474692f6fc4630
Mark Eads <meads@hcpci.com>
2017-06-21 22:38:39 +0000

    Merged in HAR-1419 (pull request #51)
    Added a loader to summary email and removed zip code from search field
    
    Approved-by: jsutphin@exzeo.com <jsutphin@exzeo.com>
    Approved-by: Justin Mitchell <jmitchell@exzeo.com>
    Approved-by: Giri <graveendran@hcpci.com>
    Approved-by: Eric Lamison <elamison@exzeo.com>
    Approved-by: Bryan Boudreau <bboudreau@exzeo.com>

--- src/components/Search/SearchBar.jsx
+++ src/components/Search/SearchBar.jsx
@@ -180,7 +180,6 @@ const SearchForm = (props) => {
           {generateField('firstName', 'First Name Search', 'First Name', formErrors, 'first-name-search')}
           {generateField('lastName', 'Last Name Search', 'Last Name', formErrors, 'last-name-search')}
           {generateField('address', 'Property Address Search', 'Property Address', formErrors, 'property-search')}
-          {generateField('zip', 'Zip Search', 'Zip', formErrors, 'zip-search')}
           {generateField('quoteNumber', 'Quote No Search', 'Quote Number', formErrors, 'quote-no-search')}
 
           <button
@@ -199,7 +198,6 @@ const SearchForm = (props) => {
           {generateField('firstName', 'First Name Search', 'First Name', formErrors, 'first-name-search')}
           {generateField('lastName', 'Last Name Search', 'Last Name', formErrors, 'last-name-search')}
           {generateField('address', 'Property Address Search', 'Property Address', formErrors, 'property-search')}
-          {generateField('zip', 'Zip Search', 'Zip', formErrors, 'zip-search')}
           {generateField('policyNumber', 'Policy No Search', 'Policy Number', formErrors, 'policy-no-search')}
 
           <button
