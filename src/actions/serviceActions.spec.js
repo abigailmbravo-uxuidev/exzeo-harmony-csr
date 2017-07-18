b29d7ad9a4e8ff5ccd0be642702028004545ca80
Mark Eads <meads@hcpci.com>
2017-07-18 11:50:01 -0400

    updated tests

--- src/actions/serviceActions.spec.js
+++ src/actions/serviceActions.spec.js
@@ -52,10 +52,49 @@ describe('Service Actions', () => {
 
     const initialState = {};
     const store = mockStore(initialState);
+    serviceActions.getNotes(store.dispatch);
 
     return serviceActions.getNotes('test')(store.dispatch)
       .then(() => {
         expect(store.getActions()[0].payload[0].type).toEqual(types.SERVICE_REQUEST);
+      });
+  });
+
+  it('should fail start getNotes', () => {
+    const mockAdapter = new MockAdapter(axios);
+    const note = {
+      noteType: 'test',
+      noteContent: 'test',
+      contactType: 'Agent',
+      createdAt: new Date().getTime(),
+      noteAttachments: [],
+      createdBy: {},
+      updatedBy: {}
+    };
+    const axiosOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      url: `${process.env.REACT_APP_API_URL}/svc`,
+      data: {
+        service: 'transaction-logs.services',
+        method: 'GET',
+        path: 'history?number=test'
+      }
+    };
+
+    mockAdapter.onPost(axiosOptions.url, axiosOptions.data).reply(200, {
+      data: [note]
+    });
+
+    const initialState = {};
+    const store = mockStore(initialState);
+    serviceActions.getNotes(store.dispatch);
+
+    return serviceActions.getNotes('4435')(store.dispatch)
+      .then(() => {
+        expect(store.getActions()[0].payload[0].type).toEqual(types.APP_ERROR);
       });
   });
 
@@ -91,10 +130,51 @@ describe('Service Actions', () => {
 
     const initialState = {};
     const store = mockStore(initialState);
+    serviceActions.addNote(store.dispatch);
 
     return serviceActions.addNote(2, 'quoteNote', {})(store.dispatch)
       .then(() => {
         expect(note).toEqual(axiosOptions.data.data);
+      });
+  });
+
+  it('should fail start addNotes', () => {
+    const mockAdapter = new MockAdapter(axios);
+    const createdAt = new Date().getTime();
+    const note = {
+      noteType: 'quoteNote',
+      noteContent: 'test',
+      contactType: 'Agent',
+      createdAt,
+      noteAttachments: [],
+      createdBy: {},
+      updatedBy: {}
+    };
+    const axiosOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      url: `${process.env.REACT_APP_API_URL}/svc`,
+      data: {
+        service: 'notes.services',
+        method: 'POST',
+        path: 'v1/note/',
+        data: note
+      }
+    };
+
+    mockAdapter.onPost(axiosOptions.url, axiosOptions.data).reply(200, {
+      data: [note]
+    });
+
+    const initialState = {};
+    const store = mockStore(initialState);
+    serviceActions.addNote(store.dispatch);
+
+    return serviceActions.addNote(543543, 'quot43eNote', {})(store.dispatch)
+      .then(() => {
+        expect(store.getActions()[0].payload[0].type).toEqual(types.APP_ERROR);
       });
   });
 
@@ -120,12 +200,45 @@ describe('Service Actions', () => {
 
     const initialState = {};
     const store = mockStore(initialState);
+    serviceActions.getAgents(store.dispatch);
 
     return serviceActions.getAgents('TTIC', 'FL')(store.dispatch)
       .then(() => {
         expect(store.getActions()[0].payload[0].type).toEqual(types.SERVICE_REQUEST);
       });
   });
+
+
+  it('should fail start getAgents', () => {
+    const mockAdapter = new MockAdapter(axios);
+
+    const axiosOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      url: `${process.env.REACT_APP_API_URL}/svc`,
+      data: {
+        service: 'agency.services',
+        method: 'GET',
+        path: 'v1/agents/TTIC/FL'
+      }
+    };
+
+    mockAdapter.onPost(axiosOptions.url, axiosOptions.data).reply(200, {
+      data: []
+    });
+
+    const initialState = {};
+    const store = mockStore(initialState);
+    serviceActions.getAgents(store.dispatch);
+
+    return serviceActions.getAgents('4534', 'FL')(store.dispatch)
+      .then(() => {
+        expect(store.getActions()[0].payload[0].type).toEqual(types.APP_ERROR);
+      });
+  });
+
   it('should call start getAgencies', () => {
     const mockAdapter = new MockAdapter(axios);
 
@@ -148,10 +261,41 @@ describe('Service Actions', () => {
 
     const initialState = {};
     const store = mockStore(initialState);
+    serviceActions.getAgencies(store.dispatch);
 
     return serviceActions.getAgencies('TTIC', 'FL')(store.dispatch)
       .then(() => {
         expect(store.getActions()[0].payload[0].type).toEqual(types.SERVICE_REQUEST);
+      });
+  });
+
+  it('should fail start getAgencies', () => {
+    const mockAdapter = new MockAdapter(axios);
+
+    const axiosOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      url: `${process.env.REACT_APP_API_URL}/svc`,
+      data: {
+        service: 'agency.services',
+        method: 'GET',
+        path: 'v1/agencies/TTIC/FL'
+      }
+    };
+
+    mockAdapter.onPost(axiosOptions.url, axiosOptions.data).reply(200, {
+      data: []
+    });
+
+    const initialState = {};
+    const store = mockStore(initialState);
+    serviceActions.getAgencies(store.dispatch);
+
+    return serviceActions.getAgencies('454545', 'FL')(store.dispatch)
+      .then(() => {
+        expect(store.getActions()[0].payload[0].type).toEqual(types.APP_ERROR);
       });
   });
 
@@ -252,10 +396,116 @@ describe('Service Actions', () => {
 
     const initialState = {};
     const store = mockStore(initialState);
+    serviceActions.getUnderwritingQuestions(store.dispatch);
 
     return serviceActions.getUnderwritingQuestions('TTIC', 'FL', 'HO3', property)(store.dispatch)
       .then(() => {
         expect(store.getActions()[0].payload[0].type).toEqual(types.SERVICE_REQUEST);
+      });
+  });
+
+  it('should fail start underwritingQuestions', () => {
+    const mockAdapter = new MockAdapter(axios);
+
+    const uw = [
+      {
+        _id: '11b1cd9f28479a0a989faa08',
+        name: 'floodCoverage',
+        model: 'quote',
+        step: 'customizeDefaultQuote',
+        question: 'Do you want Flood?',
+        group: [
+          'coverageLimits'
+        ],
+        order: 5,
+        answerType: 'bool',
+        answers: [
+          {
+            answer: 'Yes'
+          },
+          {
+            answer: 'No'
+          }
+        ]
+      }
+    ];
+
+    const property = {
+      id: '12089DF01D986BF1A',
+      source: 'CasaClue',
+      physicalAddress: {
+        address1: '95155 STINGRAY LN',
+        address2: '',
+        city: 'FERNANDINA BEACH',
+        state: 'FL',
+        county: 'NASSAU',
+        zip: '32034',
+        latitude: 30.57729,
+        longitude: -81.50374,
+        _id: '5866c036a46eb72908f3f55b'
+      },
+      residenceType: 'Single Family',
+      yearBuilt: 2005,
+      constructionType: 'Frame',
+      territory: '892-0',
+      protectionClass: 'A',
+      buildingCodeEffectivenessGrading: 99,
+      familyUnits: '1-2',
+      squareFeet: 2066,
+      sprinkler: 'no',
+      floodZone: 'V',
+      windMitigation: {
+        roofGeometry: 'Other',
+        roofDeckAttachment: 'A',
+        secondaryWaterResistance: false,
+        windBorneDebrisRegion: false,
+        terrain: 'B',
+        _id: '5866c036a46eb72908f3f55a'
+      },
+      _id: '5866c036a46eb72908f3f559',
+      gatedCommunity: false,
+      burglarAlarm: false,
+      fireAlarm: false,
+      trampoline: false,
+      divingBoard: false,
+      poolSecured: false,
+      pool: false
+    };
+
+    const axiosOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      url: `${process.env.REACT_APP_API_URL}/svc`,
+      data: {
+        service: 'questions.services',
+        method: 'POST',
+        path: 'questions/uw',
+        data: {
+          model: 'quote',
+          step: 'askUWAnswers',
+          quote: {
+            companyCode: 'TTIC',
+            state: 'FL',
+            product: 'HO3',
+            property
+          }
+        }
+      }
+    };
+
+    mockAdapter.onPost(axiosOptions.url, axiosOptions.data).reply(200, {
+      data: uw
+    });
+
+    const initialState = {};
+    const store = mockStore(initialState);
+    serviceActions.getUnderwritingQuestions(store.dispatch);
+
+    return serviceActions.getUnderwritingQuestions('55', '55', '543', property)(store.dispatch)
+      .then(() => {
+        expect(store.getActions()[0].payload[0].type).toEqual(types.APP_ERROR);
       });
   });
 
@@ -281,10 +531,287 @@ describe('Service Actions', () => {
 
     const initialState = {};
     const store = mockStore(initialState);
+    serviceActions.getSummaryLedger(store.dispatch);
 
     return serviceActions.getSummaryLedger('12345')(store.dispatch)
       .then(() => {
         expect(store.getActions()[0].payload[0].type).toEqual(types.SERVICE_REQUEST);
+      });
+  });
+
+  it('should fail start getSummaryLedger', () => {
+    const mockAdapter = new MockAdapter(axios);
+
+    const axiosOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      url: `${process.env.REACT_APP_API_URL}/svc`,
+      data: {
+        service: 'billing.services',
+        method: 'GET',
+        path: 'summary-ledgers/12345'
+      }
+    };
+
+    mockAdapter.onPost(axiosOptions.url, axiosOptions.data).reply(200, {
+      data: []
+    });
+
+    const initialState = {};
+    const store = mockStore(initialState);
+    serviceActions.getSummaryLedger(store.dispatch);
+
+    return serviceActions.getSummaryLedger('3324234234324')(store.dispatch)
+      .then(() => {
+        expect(store.getActions()[0].payload[0].type).toEqual(types.APP_ERROR);
+      });
+  });
+
+  it('should call add transaction', () => {
+    const mockAdapter = new MockAdapter(axios);
+    const date = new Date().getTime();
+    const props = {
+      policy: {
+        policyNumber: '12345',
+        state: 'FL',
+        product: 'HO3',
+        policyTerm: 'A',
+        policyAccountCode: '345'
+      },
+      auth: {
+        userProfile: {
+          groups: {
+            companyCode: 'TTIC'
+          },
+          name: 'test'
+        }
+      }
+    };
+    const body = {
+      service: 'billing.services',
+      method: 'POST',
+      path: `transaction-history/${props.policy.policyNumber}`,
+      data: {
+        createdBy: props.auth.userProfile.name,
+        updatedBy: props.auth.userProfile.name,
+        companyCode: props.auth.userProfile.groups.companyCode,
+        state: props.policy.state,
+        product: props.policy.product,
+        policyNumber: props.policy.policyNumber,
+        policyTerm: props.policy.policyTerm,
+        policyAccountCode: props.policy.policyAccountCode,
+        effectiveDate: date,
+        date,
+        type: 123,
+        description: 123,
+        batch: 123,
+        amount: 2222
+      }
+    };
+    const axiosOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      url: `${process.env.REACT_APP_API_URL}/svc`,
+      data: body
+    };
+
+    mockAdapter.onPost(axiosOptions.url, axiosOptions.data).reply(200, {
+      data: []
+    });
+
+    const initialState = {};
+    const store = mockStore(initialState);
+    serviceActions.addNote(store.dispatch);
+
+    return serviceActions.addTransaction(props, 123, 123, 123, 2222)(store.dispatch)
+      .then(() => {
+        expect(store.getActions());
+      });
+  });
+  it('should fail call add transaction', () => {
+    const mockAdapter = new MockAdapter(axios);
+    const date = new Date().getTime();
+    const props = {
+      policy: {
+        policyNumber: '12345',
+        state: 'FL',
+        product: 'HO3',
+        policyTerm: 'A',
+        policyAccountCode: '345'
+      },
+      auth: {
+        userProfile: {
+          groups: {
+            companyCode: 'TTIC'
+          },
+          name: 'test'
+        }
+      }
+    };
+    const body = {
+      service: 'billing.services',
+      method: 'POST',
+      path: `transaction-history/${props.policy.policyNumber}`,
+      data: {
+        createdBy: props.auth.userProfile.name,
+        updatedBy: props.auth.userProfile.name,
+        companyCode: props.auth.userProfile.groups.companyCode,
+        state: props.policy.state,
+        product: props.policy.product,
+        policyNumber: props.policy.policyNumber,
+        policyTerm: props.policy.policyTerm,
+        policyAccountCode: props.policy.policyAccountCode,
+        effectiveDate: date,
+        date,
+        type: 123,
+        description: 123,
+        batch: 123,
+        amount: 2222
+      }
+    };
+    const axiosOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      url: `${process.env.REACT_APP_API_URL}/svc`,
+      data: body
+    };
+
+    mockAdapter.onPost(axiosOptions.url, axiosOptions.data).reply(200, {
+      data: []
+    });
+
+    const initialState = {};
+    const store = mockStore(initialState);
+    serviceActions.addNote(store.dispatch);
+
+    return serviceActions.addTransaction(props, 54343, 5345, 5435, 4555)(store.dispatch)
+      .then(() => {
+        expect(store.getActions()[0].payload[0].type).toEqual(types.APP_ERROR);
+      });
+  });
+
+  it('should call start getAgency', () => {
+    const mockAdapter = new MockAdapter(axios);
+
+    const axiosOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      url: `${process.env.REACT_APP_API_URL}/svc`,
+      data: {
+        service: 'agency.services',
+        method: 'GET',
+        path: 'v1/agency/TTIC/FL/20000'
+      }
+    };
+
+    mockAdapter.onPost(axiosOptions.url, axiosOptions.data).reply(200, {
+      data: []
+    });
+
+    const initialState = {};
+    const store = mockStore(initialState);
+    serviceActions.getAgency(store.dispatch);
+
+    return serviceActions.getAgency('TTIC', 'FL', 20000)(store.dispatch)
+      .then(() => {
+        expect(store.getActions()[0].payload[0].type).toEqual(types.SERVICE_REQUEST);
+      });
+  });
+  it('should fail call start getAgency', () => {
+    const mockAdapter = new MockAdapter(axios);
+
+    const axiosOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      url: `${process.env.REACT_APP_API_URL}/svc`,
+      data: {
+        service: 'agency.services',
+        method: 'GET',
+        path: 'v1/agency/TTIC/FL/20000'
+      }
+    };
+
+    mockAdapter.onPost(axiosOptions.url, axiosOptions.data).reply(200, {
+      data: []
+    });
+
+    const initialState = {};
+    const store = mockStore(initialState);
+    serviceActions.getAgency(store.dispatch);
+
+    return serviceActions.getAgency('TTIC', 'FL', 23456)(store.dispatch)
+      .then(() => {
+        expect(store.getActions()[0].payload[0].type).toEqual(types.APP_ERROR);
+      });
+  });
+
+  it('should call start getAgentsByAgency', () => {
+    const mockAdapter = new MockAdapter(axios);
+
+    const axiosOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      url: `${process.env.REACT_APP_API_URL}/svc`,
+      data: {
+        service: 'agency.services',
+        method: 'GET',
+        path: 'v1/agents/TTIC/FL?agencyCode=20000'
+      }
+    };
+
+    mockAdapter.onPost(axiosOptions.url, axiosOptions.data).reply(200, {
+      data: []
+    });
+
+    const initialState = {};
+    const store = mockStore(initialState);
+    serviceActions.getAgency(store.dispatch);
+
+    return serviceActions.getAgentsByAgency('TTIC', 'FL', 20000)(store.dispatch)
+      .then(() => {
+        expect(store.getActions()[0].payload[0].type).toEqual(types.SERVICE_REQUEST);
+      });
+  });
+
+  it('should fail call start getAgentsByAgency', () => {
+    const mockAdapter = new MockAdapter(axios);
+
+    const axiosOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      url: `${process.env.REACT_APP_API_URL}/svc`,
+      data: {
+        service: 'agency.services',
+        method: 'GET',
+        path: 'v1/agents/TTIC/FL?agencyCode=20000'
+      }
+    };
+
+    mockAdapter.onPost(axiosOptions.url, axiosOptions.data).reply(200, {
+      data: []
+    });
+
+    const initialState = {};
+    const store = mockStore(initialState);
+    serviceActions.getAgency(store.dispatch);
+
+    return serviceActions.getAgentsByAgency('TTIC', 'FL', 546456)(store.dispatch)
+      .then(() => {
+        expect(store.getActions()[0].payload[0].type).toEqual(types.APP_ERROR);
       });
   });
 });
