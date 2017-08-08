e7a8029ce36842da0355f0df1c9845ab2621d51a
Mark Eads <meads@hcpci.com>
2017-08-08 15:40:27 -0400

    cancel logic

--- src/components/Policy/Cancel.jsx
+++ src/components/Policy/Cancel.jsx
@@ -1,13 +1,17 @@
 import React, { PropTypes } from 'react';
-var ReactDOM = require('react-dom');
 import { connect } from 'react-redux';
+import { reduxForm, propTypes, Form, change } from 'redux-form';
+import { bindActionCreators } from 'redux';
 import _ from 'lodash';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import PolicyConnect from '../../containers/Policy';
 import ClearErrorConnect from '../Error/ClearError';
 import normalizeNumber from '../Form/normalizeNumbers';
-
-
+import RadioField from '../Form/inputs/RadioField';
+import SelectField from '../Form/inputs/SelectField';
+import * as serviceActions from '../../actions/serviceActions';
+import * as cgActions from '../../actions/cgActions';
+import * as appStateActions from '../../actions/appStateActions';
 
 const payments = [
   {
@@ -43,178 +47,182 @@ value.rank = 5; // eslint-disable-line
 };
 
 const handleGetPolicy = (state) => {
-  const model = state.appState ? state.appState.modelName : undefined;
-  const previousTask = model && state.cg[model] && state.cg[model].data ? state.cg[model].data.previousTask : undefined;
-  return (previousTask && previousTask.value) ? previousTask.value[0] : {};
+  const taskData = (state.cg && state.appState && state.cg[state.appState.modelName]) ? state.cg[state.appState.modelName].data : null;
+  if (!taskData) return {};
+  const policyData = _.find(taskData.model.variables, { name: 'retrievePolicy' }) ? _.find(taskData.model.variables, { name: 'retrievePolicy' }).value[0] : {};
+  return policyData;
 };
 
+const handleInitialize = (state) => {
+  const values = {};
 
+  return values;
+};
 
+const paymentsData = [{
+  date: '3/07/2016',
+  description: 'Payment Received',
+  notes: '2016030739',
+  amount: '$ 4,142.00'
+}, {
+  date: '3/31/2016',
+  description: 'Payment Transfer',
+  notes: '20160331_TFR',
+  amount: '($ 1,876.00)'
+}];
 
-var paymentsData = [{
-  date: "3/07/2016",
-  description: "Payment Received",
-  notes: "2016030739",
-  amount: "$ 4,142.00"
-},{
-      date: "3/31/2016",
-      description: "Payment Transfer",
-      notes: "20160331_TFR",
-      amount: "($ 1,876.00)"
-  }];
-
-  class Payments extends React.Component {
-    render() {
-      const options = {
-        defaultSortName: 'date',
-        defaultSortOrder: 'desc'
-      };
-      return (
-        <BootstrapTable data={ paymentsData } options={ options }>
-          <TableHeaderColumn dataField='date' width="25%" isKey>Date</TableHeaderColumn>
-          <TableHeaderColumn dataField='description' width="25%">Description</TableHeaderColumn>
-          <TableHeaderColumn dataField='notes' width="25%">Notes</TableHeaderColumn>
-          <TableHeaderColumn dataField='amount' width="25%">Amount</TableHeaderColumn>
-        </BootstrapTable>
-      );
-    }
+export class Payments extends React.Component {
+  render() {
+    const options = {
+      defaultSortName: 'date',
+      defaultSortOrder: 'desc'
+    };
+    return (
+      <BootstrapTable data={paymentsData} options={options}>
+        <TableHeaderColumn dataField="date" width="25%" isKey>Date</TableHeaderColumn>
+        <TableHeaderColumn dataField="description" width="25%">Description</TableHeaderColumn>
+        <TableHeaderColumn dataField="notes" width="25%">Notes</TableHeaderColumn>
+        <TableHeaderColumn dataField="amount" width="25%">Amount</TableHeaderColumn>
+      </BootstrapTable>
+    );
+  }
   }
 
 
+const claimsData = [
+  {
+    jeLossNo: '888888',
+    lossID: '44950',
+    dateLoss: '06/03/2016',
+    reportDate: '06/09/2016',
+    closeDate: '06/18/2016',
+    lossStatus: ' ',
+    lossDesc: 'Ins called in to file a claim for water damage to his living room ceiling.Ins ac handler was clogged causing the drain line to over flow and water starting coming out which effected the living room ceiling. Repairs have been made to the air handler.The damage is to the plaster on the ceiling and drywall.'
+  },
+  {
+    jeLossNo: '999999',
+    lossID: '44952',
+    dateLoss: '07/12/2016',
+    reportDate: '07/14/2016',
+    closeDate: '07/29/2016',
+    lossStatus: ' ',
+    lossDesc: 'Ins called in to file a claim for water damage to his living room ceiling.Ins ac handler was clogged causing the drain line to over flow and water starting coming out which effected the living room ceiling. Repairs have been made to the air handler.The damage is to the plaster on the ceiling and drywall.'
+  }];
 
-  var claimsData = [
-    {
-          jeLossNo: "888888",
-          lossID: "44950",
-          dateLoss: "06/03/2016",
-          reportDate: "06/09/2016",
-          closeDate: "06/18/2016",
-          lossStatus: " ",
-          lossDesc: "Ins called in to file a claim for water damage to his living room ceiling.Ins ac handler was clogged causing the drain line to over flow and water starting coming out which effected the living room ceiling. Repairs have been made to the air handler.The damage is to the plaster on the ceiling and drywall."
-      },
-    {
-        jeLossNo: "999999",
-        lossID: "44952",
-        dateLoss: "07/12/2016",
-        reportDate: "07/14/2016",
-        closeDate: "07/29/2016",
-        lossStatus: " ",
-        lossDesc: "Ins called in to file a claim for water damage to his living room ceiling.Ins ac handler was clogged causing the drain line to over flow and water starting coming out which effected the living room ceiling. Repairs have been made to the air handler.The damage is to the plaster on the ceiling and drywall."
-    } ];
+export class Claims extends React.Component {
+  render() {
+    const options = {
+      defaultSortName: 'jeLossNo',
+      defaultSortOrder: 'desc'
+    };
+    return (
+      <BootstrapTable data={claimsData} options={options} >
+        <TableHeaderColumn dataField="jeLossNo" width="10%" isKey>JE Loss No</TableHeaderColumn>
+        <TableHeaderColumn dataField="lossID" width="10%">Loss ID</TableHeaderColumn>
+        <TableHeaderColumn dataField="dateLoss" width="10%">Date Loss</TableHeaderColumn>
+        <TableHeaderColumn dataField="reportDate" width="10%">Report Date</TableHeaderColumn>
+        <TableHeaderColumn dataField="closeDate" width="10%">Close Date</TableHeaderColumn>
+        <TableHeaderColumn dataField="lossStatus" width="20%">Loss Status</TableHeaderColumn>
+        <TableHeaderColumn dataField="lossDesc" width="30%" tdStyle={{ whiteSpace: 'normal' }}>Loss Description</TableHeaderColumn>
+      </BootstrapTable>
+    );
+  }
+}
 
-    class Claims extends React.Component {
-      render() {
-        const options = {
-          defaultSortName: 'jeLossNo',
-          defaultSortOrder: 'desc'
-        };
-        return (
-          <BootstrapTable data={ claimsData } options={options} >
-            <TableHeaderColumn dataField='jeLossNo' width="10%" isKey>JE Loss No</TableHeaderColumn>
-            <TableHeaderColumn dataField='lossID' width="10%">Loss ID</TableHeaderColumn>
-            <TableHeaderColumn dataField='dateLoss' width="10%">Date Loss</TableHeaderColumn>
-            <TableHeaderColumn dataField='reportDate' width="10%">Report Date</TableHeaderColumn>
-            <TableHeaderColumn dataField='closeDate' width="10%">Close Date</TableHeaderColumn>
-            <TableHeaderColumn dataField='lossStatus' width="20%">Loss Status</TableHeaderColumn>
-            <TableHeaderColumn dataField='lossDesc' width="30%" tdStyle={ { whiteSpace: 'normal' } }>Loss Description</TableHeaderColumn>
-          </BootstrapTable>
-        );
-      }
-    }
+export const handleFormSubmit = (data, dispatch, props) => {
 
-export const PolicyholderAgent = (props) => {
-  const { additionalInterests } = props.policy;
+};
+
+export const resetCancelReasons = (props) => {
+  props.dispatch(change('CancelPolicy', 'cancelReason', ''));
+};
+
+const cancelOptions = [
+  {
+    cancelType: 'Voluntary Cancellation',
+    cancelReason: ['Continuous Wind Coverage - 3 Yrs', 'Duplicate - Similar Coverage', 'Insured Deceased', 'Mortgage Satisfied', 'Other', 'Property Demolished', 'Property Foreclosed', 'Reason Not Provided', 'Rewritten - Similar Coverage', 'Sold']
+  },
+  {
+    cancelType: 'Underwriting Cancellation',
+    cancelReason: ['Claims Frequency', 'Claims Severity', 'Condition of Roof', 'Empty Pool', 'Existing/Unrepaired Damage', 'Failure to Comply with Underwriting Request', 'Ineligible Breed of Dog', 'Ineligible Ownership', 'Ineligible Protection Class', 'Ineligible Risk', 'Insured Deceased', 'No Insurable Interest', 'Policy Limits Paid', 'Property in Disrepair', 'Risk Management', 'Slide/Diving Board', 'Tenant Occupied', 'Trampoline', 'Unsecured Pool', 'Vacant']
+  },
+  {
+    cancelType: 'Underwriting Non-Renewal',
+    cancelReason: ['Claims Frequency', 'Claims Severity', 'Condition of Roof', 'Empty Pool', 'Existing/Unrepaired Damage', 'Failure to Comply with Underwriting Request', 'Ineligible Breed of Dog', 'Ineligible Ownership', 'Ineligible Protection Class', 'Ineligible Risk', 'Insured Deceased', 'No Insurable Interest', 'Policy Limits Paid', 'Property in Disrepair', 'Risk Management', 'Slide/Diving Board', 'Tenant Occupied', 'Trampoline', 'Unsecured Pool', 'Vacant']
+  }
+];
+
+export const CancelPolicy = (props) => {
+  const { policy, handleSubmit, fieldValues } = props;
+
+  const { additionalInterests } = policy;
+
   setRank(additionalInterests);
+
+  const cancelGroup = _.map(cancelOptions, option => ({ answer: option.cancelType }));
   return (
     <PolicyConnect>
       <ClearErrorConnect />
       <div className="route-content">
         <div className="scroll">
           <div className="form-group survey-wrapper cancel-policy" role="group">
-            <form id="cancellation">
-            <section>
-              <div className="flex-parent">
-              <h3>Cancel Policy</h3>
-              <div className="btn-footer">
-                <button className="btn btn-secondary">Return</button>
-                <button className="btn btn-danger">Cancel Policy</button>
-              </div>
-            </div>
+            <Form id="Cancellation" onSubmit={handleSubmit(handleFormSubmit)} noValidate>
+              <section>
+                <div className="flex-parent">
+                  <h3>Cancel Policy</h3>
+                  <div className="btn-footer">
+                    <button className="btn btn-secondary">Return</button>
+                    <button className="btn btn-danger">Cancel Policy</button>
+                  </div>
+                </div>
 
 
-            <div className="form-group segmented name" role="group">
-              <label className="group-label label-segmented"></label>
-              <div className="segmented-answer-wrapper">
-                <div className="radio-column-3">
-                  <label className="label-segmented">
-                    <input type="radio" value="" />
-                      <span>Voluntary Cancel</span>
-                      </label>
-                      </div>
-                <div className="radio-column-3">
-                <label className="label-segmented">
-                  <input type="radio" value="" />
-                    <span>Voluntary Nonrenewal</span>
-                    </label>
+                <div className="flex-parent">
+                  <div className="flex-child wind-wbdr">
+                    <RadioField
+                      onChange={() => resetCancelReasons(props)}
+                      validations={['required']} name={'cancelOptions'} styleName={''} label={'Cancel Options'} segmented
+                      answers={cancelGroup}
+                    />
+                  </div>
+                </div>
+
+
+                <div className="flex-parent">
+
+
+                  <div className="flex-child">
+
+                    <div className="form-group">
+                      <label>Effective Date</label>
+                      <input type="date" />
                     </div>
-                        <div className="radio-column-3">
-                          <label className="label-segmented">
-                            <input type="radio"  value="Other" />
-                              <span>Underwriting / NonRenewal</span>
-                              </label></div>
-                              </div>
-                              </div>
+                  </div>
+
+                  <div className="flex-child">
+                    <SelectField
+                      name="cancelReason" component="select" styleName={''} label="Cancel Reason" validations={['required']}
+                      answers={_.concat([], _.get(_.find(cancelOptions, option => option.cancelType === fieldValues.cancelOptions), 'cancelReason')).map(reason => ({
+                        answer: reason,
+                        label: reason
+                      }))}
+                    />
+                  </div>
+                </div>
 
 
-
-<div className="flex-parent">
-
-
-      <div className="flex-child">
-
-              <div className="form-group">
-                <label>Effective Date</label>
-                <input type="date" />
-              </div>
-</div>
-
-<div className="flex-child">
-              <div className="form-group">
-                <label>Reason</label>
-                <select>
-                  <option>Please Select</option>
-                </select>
-              </div>
-
-              <div className="form-group">
-                <label style={{ display: 'none;' }} />
-                  <input type="text" />
-              </div>
-</div>
-</div>
-
-
-
-
-
-            </section>
-
-
-
-</form>
-
-
+              </section>
+            </Form>
             <section>
               <h3>Payments</h3>
 
-            <div className="form-group flex-parent billing">
-              <div className="flex-child"><label>Bill To</label> <span>Semi-Annual to Policyholder</span></div>
-              <div className="flex-child"><label>Bill Plan</label> <span>Semi-Annual</span></div>
-              <div className="flex-child"><div className="form-group"><label>Equity Date</label> <input type="date" /></div></div>
-            </div>
+              <div className="form-group flex-parent billing">
+                <div className="flex-child"><label>Bill To</label> <span>Semi-Annual to Policyholder</span></div>
+                <div className="flex-child"><label>Bill Plan</label> <span>Semi-Annual</span></div>
+                <div className="flex-child"><div className="form-group"><label>Equity Date</label> <input type="date" /></div></div>
+              </div>
 
-                <Payments />
-
+              <Payments />
 
 
             </section>
@@ -223,8 +231,6 @@ export const PolicyholderAgent = (props) => {
               <h3>Claims</h3>
 
               <Claims />
-
-
 
 
             </section>
@@ -236,17 +242,27 @@ export const PolicyholderAgent = (props) => {
   );
 };
 
-PolicyholderAgent.propTypes = {
+CancelPolicy.propTypes = {
   policy: PropTypes.shape()
 };
 
-/**
-------------------------------------------------
-redux mapping
-------------------------------------------------
-*/
 const mapStateToProps = state => ({
+  tasks: state.cg,
+  appState: state.appState,
+  fieldValues: _.get(state.form, 'CancelPolicy.values', {}),
+  initialValues: handleInitialize(state),
   policy: handleGetPolicy(state)
 });
 
-export default connect(mapStateToProps)(PolicyholderAgent);
+const mapDispatchToProps = dispatch => ({
+  actions: {
+    serviceActions: bindActionCreators(serviceActions, dispatch),
+    cgActions: bindActionCreators(cgActions, dispatch),
+    appStateActions: bindActionCreators(appStateActions, dispatch)
+  }
+});
+
+// ------------------------------------------------
+// wire up redux form with the redux connect
+// ------------------------------------------------
+export default connect(mapStateToProps, mapDispatchToProps)(reduxForm({ form: 'CancelPolicy', enableReinitialize: true })(CancelPolicy));
