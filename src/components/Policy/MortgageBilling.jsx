503fbcaa21183e4b09b3b0968fedf7ac9b50d925
Joe Sutphin <jsutphin@exzeo.com>
2017-09-18 09:26:29 -0400

    merge develop

--- src/components/Policy/MortgageBilling.jsx
+++ src/components/Policy/MortgageBilling.jsx
@@ -182,11 +182,7 @@ export class MortgageBilling extends Component {
           <div className="scroll">
             <div className="form-group survey-wrapper" role="group">
               <section className="payment-summary">
-<<<<<<< HEAD
                 <h3>Billing <button aria-label="open-btn form-editBilling" className="btn btn-link btn-sm" onClick={this.handleBillingEdit}><i className="fa fa-pencil-square"></i>Edit</button></h3>
-=======
-                <h3>Billing <button className="btn btn-link btn-sm" onClick={this.handleBillingEdit}><i className="fa fa-pencil-square" />Edit</button></h3>
->>>>>>> develop
                 <div className="payment-summary">
                   <dl>
                     <div>
