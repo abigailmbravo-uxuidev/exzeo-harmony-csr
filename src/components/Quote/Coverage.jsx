c56662c9edf7d795a8577beb2d4376fa23c4f132
Mark Eads <meads@hcpci.com>
2017-11-07 10:22:24 -0500

    added back date to validation

--- src/components/Quote/Coverage.jsx
+++ src/components/Quote/Coverage.jsx
@@ -417,7 +417,7 @@ export class Coverage extends Component {
                   <h3>Produced By</h3>
                   <div className="flex-parent produced-by-wrapper">
                     <div className="flex-child effectiveDate">
-                      <DateField validations={['required']} label={'Effective Date'} name={'effectiveDate'} />
+                      <DateField validations={['required', 'date']} label={'Effective Date'} name={'effectiveDate'} />
                     </div>
                     <div className="flex-child agencyCode">
                       <SelectField
