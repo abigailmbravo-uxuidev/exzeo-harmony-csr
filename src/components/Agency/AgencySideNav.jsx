ec3c9de9a13d61e86f478f341f97e7bfd2b98f99
Rashard Bradford <rbradford@FL01LT6278.local>
2018-01-16 10:50:07 -0500

    add agency sidediv, header, and agency container

---
+++ src/components/Agency/AgencySideNav.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { reduxForm, propTypes } from 'redux-form';
+import * as appStateActions from '../../actions/appStateActions';
+import * as cgActions from '../../actions/cgActions';
+// import NewNoteFileUploader from '../Common/NewNoteFileUploader';
+
+// Example of a possible schema
+/**
+ * {
+ *  link,
+ *  label,
+ *  styleName,
+ *  exact,
+ *  outside
+ * }
+ */
+const csrLinks = [{
+  key: 'staff',
+  link: '/agency/staff',
+  label: 'Staff',
+  styleName: 'staff',
+  exact: true
+}, {
+  key: 'notes',
+  link: '/policy/notes',
+  label: 'Notes / Files',
+  styleName: 'notes disabled',
+  exact: true
+}];
+
+
+export const SideNav = (props) => {
+  return (
+    <nav className="site-nav">
+      <ul>
+        {csrLinks && csrLinks.length > 0 && csrLinks.map((agentLink, index) => (
+        agentLink.outside ?
+          <li key={index}>
+            {/* <a className={agentLink.styleName} href={agentLink.link}>*/}
+            <a className="csr-dashboard" href="/">
+              <span>{agentLink.label}</span>
+            </a>
+          </li> :
+          <li key={index}>
+            <span className={agentLink.styleName}>
+              <NavLink to={agentLink.link} activeClassName="active" exact>{agentLink.label}</NavLink>
+            </span>
+          </li>
+      ))}
+      </ul>
+    </nav>);
+};
+
+// TODO: Needs to be connected to wherever it's gonnna get nav links from
+SideNav.propTypes = {
+  ...propTypes,
+  completedTasks: PropTypes.any, // eslint-disable-line
+  activateRedirectLink: PropTypes.string,
+  activateRedirect: PropTypes.bool
+};
+
+const mapStateToProps = state => ({
+  activateRedirectLink: state.appState.data.activateRedirectLink,
+  activateRedirect: state.appState.data.activateRedirect,
+  agency: state.service.agency || {}
+});
+
+const mapDispatchToProps = dispatch => ({
+  actions: {
+    cgActions: bindActionCreators(cgActions, dispatch),
+    appStateActions: bindActionCreators(appStateActions, dispatch)
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(reduxForm({ form: 'SideNav' })(SideNav));
---
+++ src/containers/Agency.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Helmet } from 'react-helmet';
+import AgencyHeader from '../components/Agency/AgencyHeader';
+import AgencySideNav from '../components/Agency/AgencySideNav';
+import AgencyDetailHeader from '../components/Agency/DetailHeader';
+import Loader from '../components/Common/Loader';
+import * as serviceActions from '../actions/serviceActions';
+
+export const Agency = props => (
+  <div className="app-wrapper csr policy">
+    {/* TODO: dynamically add policy # to title*/}
+    <Helmet><title>{'Harmony - CSR Portal'}</title></Helmet>
+    <AgencyHeader />
+    <AgencyDetailHeader />
+    <main role="document">
+      {/* props.appState.data.submitting && <Loader />*/}
+      <aside className="content-panel-left">
+        <AgencySideNav />
+      </aside>
+      <div className="content-wrapper">
+        {props.children}
+      </div>
+    </main>
+  </div>
+);
+
+Agency.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]),
+  agency: PropTypes.shape()
+};
+
+const mapStateToProps = state => ({
+  agency: state.service.agency
+});
+
+const mapDispatchToProps = dispatch => ({
+  actions: {
+    serviceActions: bindActionCreators(serviceActions, dispatch)
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Agency);
