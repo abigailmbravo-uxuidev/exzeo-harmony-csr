3d5eb9dcd8149f013efe43564d8c1a9ac0a790e0
elamison <eric@lamison.com>
2018-02-13 14:41:32 -0500

    Removed exra space.

--- src/components/Policy/MortgageBilling.jsx
+++ src/components/Policy/MortgageBilling.jsx
@@ -376,7 +376,7 @@ export class MortgageBilling extends Component {
       let billTo;
       if (policy.billToType === 'Policyholder') {
         billTo = policyHolders.find(ph => ph._id === policy.billToId);
-        billToName = `${policy.billToType}:  ${billTo.firstName} ${billTo.lastName}`
+        billToName = `${policy.billToType}: ${billTo.firstName} ${billTo.lastName}`
       } else {
         billTo = billTo ? billTo : additionalInterests.find(ai => ai._id === policy.billToId);
         billToName = `${policy.billToType}: ${billTo.name1}`;
