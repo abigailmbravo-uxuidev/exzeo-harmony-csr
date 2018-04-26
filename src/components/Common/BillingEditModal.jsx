53b949e26c3aef27a027429e80650970c306455b
Jordan Rhodes <jsrhodes15@gmail.com>
2018-04-26 14:43:18 -0400

    Clean up formatting and propTypes

--- src/components/Common/BillingEditModal.jsx
+++ src/components/Common/BillingEditModal.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { batchActions } from 'redux-batched-actions';
+import propTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { reduxForm, Form, propTypes, change, getFormValues } from 'redux-form';
-import { RadioFieldBilling, SelectField } from '../Form/inputs';
+import { reduxForm, Field, propTypes as rfPropTypes, change, getFormValues } from 'redux-form';
+import SelectInput from '../Form/base/Select';
+import BillingRadio from '../Form/inputs/BillingRadio';
 import { updateBillPlan } from '../../actions/serviceActions';
 import 'react-select/dist/react-select.css';
+const FORM_NAME = 'BillingEditModal';
 
 export const handleInitialize = (state) => {
   const { billingOptions, latestPolicy } = state.service;
 
-  if (billingOptions && billingOptions.options && billingOptions.options.length === 1 && !latestPolicy.billTo && !latestPolicy.billPlan) {
+  if (billingOptions.length === 1 && !latestPolicy.billTo && !latestPolicy.billPlan) {
     return {
       billToId: billingOptions.options[0].billToId,
       billToType: billingOptions.options[0].billToType,
@@ -32,116 +33,118 @@ export const handleBillingFormSubmit = async (data, dispatch, props) => {
     billToType: data.billToType
   };
   await updateBillPlan(updateData);
-  await hideBillingModal(props);
+  await hideBillingModal();
 };
 
-export const setFormValues = (options, billToId, billplan, dispatch) => {
-  const currentPaymentPlan = options.find(option => option.billToId === billToId);
-  if (currentPaymentPlan) {
-    dispatch(batchActions([
-      change('BillingEditModal', 'billToId', currentPaymentPlan.billToId),
-      change('BillingEditModal', 'billToType', currentPaymentPlan.billToType),
-      change('BillingEditModal', 'billPlan', billplan)
-    ]));
+export class BillingEditModal extends React.Component {
+  normalizeBilling = (value) => {
+    const { billingOptions, changeField } = this.props;
+    const billToType = billingOptions.find(o => o.billToId === value).billToType;
+    changeField(FORM_NAME, 'billPlan', 'Annual');
+    changeField(FORM_NAME, 'billToType', billToType);
+    return value;
+  };
+
+  validateBillPlan = (value) => {
+    return value ? undefined : 'Field Required';
+  };
+
+  getBillingOptions() {
+    const { billingOptions, fieldValues } = this.props;
+    const planOptions = billingOptions.find(option => option.billToId === fieldValues.billToId);
+    return (planOptions || {}).payPlans || [];
   }
-};
 
-export const selectBillPlan = (value, props) => {
-  const { billingOptions, dispatch, fieldValues } = props;
-  setFormValues(billingOptions.options, fieldValues.billToId, value, dispatch);
-};
+  render() {
+    const { hideBillingModal, billingOptions, submitting, handleSubmit } = this.props;
+    const billToOptions = billingOptions.map(option => ({label: option.displayText, answer: option.billToId}));
 
-export const BillingEditModal = (props) => {
-  const {
-    appState,
-    handleSubmit,
-    hideBillingModal,
-    billingOptions: { options, paymentPlans },
-    fieldValues,
-  } = props;
-  const billToOptions = options.map(option => ({ label: option.displayText, answer: option.billToId, value: option.billToId }));
-  const billPlanOptions = options.find(option => option.billToId === fieldValues.billToId);
-
-  return (
-    <div className="modal" style={{ flexDirection: 'row' }}>
-    <div className="card card-billing-edit-modal">
-      <Form id="BillingEditModal" className="BillingEditModal" noValidate onSubmit={handleSubmit(handleBillingFormSubmit)}>
-        <div className="card-header">
-          <h4>Edit Billing</h4>
+    return (
+      <div className="modal" style={{flexDirection: 'row'}}>
+        <div className="card card-billing-edit-modal">
+          <form id="BillingEditModal" className="BillingEditModal" noValidate onSubmit={handleSubmit(handleBillingFormSubmit)}>
+            <div className="card-header">
+              <h4>Edit Billing</h4>
+            </div>
+            <div className="card-block">
+              <Field
+                name="billToId"
+                normalize={this.normalizeBilling}
+                component={SelectInput}
+                label="Bill To"
+                validations={['required']}
+                answers={billToOptions}
+              />
+              <Field
+                name="billPlan"
+                label="Bill Plan"
+                component={BillingRadio}
+                validate={this.validateBillPlan}
+                answers={this.getBillingOptions()}
+                segmented
+              />
+            </div>
+            <div className="card-footer">
+              <div className="btn-group">
+                <button
+                  tabIndex="0"
+                  aria-label="reset-btn form-editBilling"
+                  className="btn btn-secondary"
+                  type="button"
+                  onClick={hideBillingModal}
+                >Cancel
+                </button>
+                <button
+                  tabIndex="0"
+                  aria-label="submit-btn form-editBilling"
+                  className="btn btn-primary"
+                  type="submit"
+                  disabled={submitting}
+                >Update
+                </button>
+              </div>
+            </div>
+          </form>
         </div>
-        <div className="card-block">
-          <SelectField
-            name="billToId"
-            component="select"
-            label="Bill To"
-            validations={['required']}
-            answers={billToOptions}
-          />
-          <RadioFieldBilling
-            validations={['required']}
-            name="billPlan"
-            label="Bill Plan"
-            onChange={value => selectBillPlan(value, props)}
-            validate={[value => (value ? undefined : 'Field Required')]}
-            segmented
-            answers={billPlanOptions ? billPlanOptions.payPlans : []}
-            paymentPlans={paymentPlans}
-          />
-        </div>
-        <div className="card-footer">
-          <div className="btn-group">
-            <button
-              tabIndex="0"
-              aria-label="reset-btn form-editBilling"
-              className="btn btn-secondary"
-              type="button"
-              onClick={() => hideBillingModal(props)}
-            >Cancel
-            </button>
-            <button
-              tabIndex="0"
-              aria-label="submit-btn form-editBilling"
-              className="btn btn-primary"
-              type="submit"
-              disabled={appState.data.submitting}
-            >Update
-            </button>
-          </div>
-        </div>
-      </Form>
-    </div>
-  </div>
-  );
-};
+      </div>
+    );
+  }
+}
 
 BillingEditModal.propTypes = {
-  ...propTypes,
-  showBillingEditModalModal: PropTypes.func,
-  verify: PropTypes.func,
-  appState: PropTypes.shape({
-    modelName: PropTypes.string,
-    data: PropTypes.shape({
-      recalc: PropTypes.bool,
-      submitting: PropTypes.bool,
+  ...rfPropTypes,
+  showBillingEditModalModal: propTypes.func,
+  verify: propTypes.func,
+  appState: propTypes.shape({
+    modelName: propTypes.string,
+    data: propTypes.shape({
+      recalc: propTypes.bool,
+      submitting: propTypes.bool,
     })
   })
 };
 
+BillingEditModal.defaultProps = {
+  fieldValues: {},
+  billingOptions: []
+};
+
 const mapStateToProps = state => ({
   tasks: state.cg,
-  appState: state.appState,
   selectedAI: state.appState.data.selectedAI,
   initialValues: handleInitialize(state),
   policy: state.service.latestPolicy,
-  fieldValues: getFormValues('BillingEditModal')(state),
+  fieldValues: getFormValues(FORM_NAME)(state),
 });
 
-BillingEditModal.defaultProps = {
-  fieldValues: {},
-  billingOptions: {}
-};
-
-export default connect(mapStateToProps, { updateBillPlan })(reduxForm({
+BillingEditModal = reduxForm({
   form: 'BillingEditModal',
   enableReinitialize: true
-})(BillingEditModal));
+})(BillingEditModal);
+
+BillingEditModal = connect(mapStateToProps, {
+  updateBillPlan,
+  changeField: change
+})(BillingEditModal);
+
+export default BillingEditModal;
--- src/components/Form/base/Radios.jsx
+++ src/components/Form/base/Radios.jsx
@@ -1,115 +1,42 @@
-/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import FieldHint from '../inputs/FieldHint';
 
-export const RadioInputBilling = ({ input, meta, segmented, styleName, label, answers, paymentPlans, hint, displayValue }) => {
-  const { error, touched } = meta;
-  const { onChange, name, value, disabled } = input;
+const Radio = ({
+  input: { onChange, name, value },
+  answer,
+  onKeyPress,
+  segmented,
+  size
+}) => (
+  <div
+    className={classNames(`radio-column-${size}`, { selected: value === answer })}
+    onKeyPress={event => onKeyPress(event, answer)}
+    onClick={() => onChange(answer)}
+  >
+    <label className={classNames({ 'label-segmented': segmented }, { selected: value === answer })} htmlFor={name}>
+      <input
+        name={name}
+        value={answer}
+        type="radio"
+        checked={String(value) === String(answer)}
+        readOnly
+      />
+      <span tabIndex={'0'}>{answer}</span>
+    </label>
+  </div>
+);
 
-  const getSelectedPlan = (answer) => {
-    let selection;
-
-    if (answer === 'Annual') {
-      selection = 'annual';
-    } else if (answer === 'Semi-Annual') {
-      selection = 'semiAnnual';
-    } else if (answer === 'Quarterly') {
-      selection = 'quarterly';
-    }
-    return selection;
-  };
-
-  const onKeyPress = (event, answer) => {
-    if (event.charCode === 13) {
-      onChange(answer);
-    }
-  };
-
-  return (
-    <div className={classNames('form-group', segmented, name, styleName, disabled)} role="group">
-      <label className={classNames('group-label', { 'label-segmented': segmented })} htmlFor={name}>
-        {label}
-        {hint && <FieldHint name={name} hint={hint} />}
-        {displayValue && <input type="text" value={displayValue} readOnly />}
-      </label>
-      <div className={classNames('segmented-answer-wrapper', { error: touched && error })}>
-        {answers && answers.length > 0 && answers.map((answer, index) =>
-          <RadioOptionBilling
-            input={input}
-            paymentPlan={paymentPlans[getSelectedPlan(answer)]}
-            answer={answer}
-            onKeyPress={onKeyPress}
-            key={index}
-            tabIndex={'0'}
-            size={answers.length}
-            onChange={onChange}
-            name={name}
-            segmented={segmented}
-            value={value}
-          />
-        )}
-      </div>
-    </div>
-  );
-};
-
-RadioInputBilling.propTypes = {
-  paymentPlans: PropTypes.any, // eslint-disable-line
-  /**
-   * Answers used to generate options
-   */
-  answers: PropTypes.any, // eslint-disable-line
-  /**
-   * Used for dependent radio field, activates
-   * read only input
-   */
-  displayValue: PropTypes.any, // eslint-disable-line
-
-  /**
-   * Tooltip for user
-   */
-  hint: PropTypes.string,
-
-  /**
-   * Provided by redux-form Field
-   */
+Radio.propTypes = {
   input: PropTypes.shape({
-    disabled: PropTypes.bool,
     name: PropTypes.string,
+    value: PropTypes.string,
     onChange: PropTypes.func,
-    value: PropTypes.any, // eslint-disable-line
   }),
-
-  /**
-   * Label for form field
-   */
-  label: PropTypes.string,
-
-  /**
-   * Validation results
-   */
-  meta: PropTypes.shape({
-    warning: PropTypes.string,
-    error: PropTypes.string,
-    touched: PropTypes.bool
-  }),
-
-  /**
-   * Whether field is segmented radio
-   */
-
-  /**
-   * Classname for form-group
-   */
+  onKeyPress: PropTypes.func,
+  answer: PropTypes.any,
+  size: PropTypes.number,
   segmented: PropTypes.bool,
-  styleName: PropTypes.string
 };
 
-RadioInputBilling.defaultProps = {
-  input: {},
-  meta: {}
-};
-
-export default reduxFormField(RadioInputBilling);
+export default Radio;
