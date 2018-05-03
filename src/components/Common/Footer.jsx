c43d9b74e6e256edf5462fd972f2f2e681614532
Mark Eads <meads@hcpci.com>
2018-05-03 11:18:05 -0400

    added react app version number

--- src/components/Common/Footer.jsx
+++ src/components/Common/Footer.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { version } from '../../../package.json';
 
 const Footer = () =>
-  <footer>
+  (<footer>
     <div role="banner">
-      <small>
+      <small className="copyright">
         &copy;2017 TypTap Management Company. All rights reserved.
       </small>
+      <small>
+        v.{process.env.REACT_APP_VERSION}
+      </small>
     </div>
-  </footer>;
+  </footer>);
 
 export default Footer;
