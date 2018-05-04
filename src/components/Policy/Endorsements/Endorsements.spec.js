5282fc9be6fe4ffab9f969333c3f233d6ac3f76e
Jordan Rhodes <jsrhodes15@gmail.com>
2018-05-04 13:03:12 -0400

    Create main components for Endorsements form

--- src/components/Policy/Endorsements/Endorsements.spec.js
+++ src/components/Policy/Endorsements/Endorsements.spec.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import { reduxForm, propTypes, change, Form } from 'redux-form';
 import { shallow, mount } from 'enzyme';
-import { Endorsements, calculatePercentage, handleInitialize, setPercentageOfValue, updateDependencies, calculate, save, setCalculate, updateCalculatedSinkhole, getNewPolicyNumber, setEndorsementDate, clearSecondaryPolicyholder } from './Endorsements';
+import { Endorsements, calculatePercentage, handleInitialize, setPercentageOfValue, updateDependencies, calculate, save, setCalculate, updateCalculatedSinkhole, getNewPolicyNumber, setEndorsementDate, clearSecondaryPolicyholder } from './index';
 
 const middlewares = [thunk]; // add your middlewares like `redux-thunk`
 const mockStore = configureStore(middlewares);
