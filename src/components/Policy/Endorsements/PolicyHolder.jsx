3a68b70c27c773a92e3e6024ffb1976637c6b1e4
Mark Eads <meads@hcpci.com>
2018-05-08 11:34:02 -0400

    updated policyholder component

--- src/components/Policy/Endorsements/PolicyHolder.jsx
+++ src/components/Policy/Endorsements/PolicyHolder.jsx
@@ -1,62 +1,123 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Field } from 'redux-form';
+import Inputs from '@exzeo/core-ui/lib/Input';
+import lifecycle from '@exzeo/core-ui/lib/InputLifecycle';
 import TextField from '../../Form/inputs/TextField';
-import PhoneField from '../../Form/inputs/PhoneField';
-import {clearSecondaryPolicyholder, setCalculate, setPHToggle} from "./index";
+import { clearSecondaryPolicyholder, setPHToggle } from './index';
 
-const PolicyHolder = (props) => {
-  return (
-    <section name="policy" id="policy">
-      <div className="flex-parent col2">
-        {/* Col1 */}
-        <div className="flex-child">
-          <h3>Primary Policyholder</h3>
-          <div className="flex-parent col2">
-            <TextField validations={['required']} label="First Name" styleName="" name="pH1FirstName" onChange={() => setCalculate(props, false)} />
-            <TextField validations={['required']} label="Last Name" styleName="" name="pH1LastName" onChange={() => setCalculate(props, false)} />
-          </div>
-          <div className="flex-parent col2">
-            <PhoneField validations={['required', 'phone']} label="Primary Phone" styleName="" name="pH1phone" onChange={() => setCalculate(props, false)} />
-            <PhoneField validations={['phone']} label="Secondary Phone" styleName="" name="pH1secondaryPhone" onChange={() => setCalculate(props, false)} />
-          </div>
-          <div className="flex-parent">
-            <TextField validations={['required', 'email']} label="Email Address" styleName="" name="pH1email" onChange={() => setCalculate(props, false)} />
-            {/* electronic delivery question placeholder */ }
-          </div>
+const { Input } = Inputs;
+const {
+  validation, normalize
+} = lifecycle;
+
+const PolicyHolder = props => (
+  <section name="policy" id="policy">
+    <div className="flex-parent col2">
+      {/* Col1 */}
+      <div className="flex-child">
+        <h3>Primary Policyholder</h3>
+        <div className="flex-parent col2">
+          <Field
+            label="First Name"
+            name="pH1FirstName"
+            component={Input}
+            validate={validation.isRequired}
+          />
+          <TextField
+            label="Last Name"
+            name="pH1LastName"
+            component={Input}
+            validate={validation.isRequired}
+          />
         </div>
-        {/* Col2 */}
-        <div className="flex-child">
-          <div className="flex-header-wrap">
-            <h3>Secondary Policyholder</h3>
-            <div className="check-box-wrapper">
-              <Field
-                onChange={event => clearSecondaryPolicyholder(String(event.target.value) === 'false', props)}
-                name="clearFields"
-                id="clearFields"
-                component="input"
-                type="checkbox"
-                disabled={!(props.policy && props.policy.policyHolders && props.policy.policyHolders[1])}
-              />
-              <label htmlFor="clearFields">Remove</label>
-            </div>
-          </div>
-          <div className="flex-parent col2">
-            <TextField label="First Name" dependsOn={['pH2LastName', 'pH2email', 'pH2phone']} styleName="" name="pH2FirstName" onChange={() => setPHToggle(props)} />
-            <TextField label="Last Name" dependsOn={['pH2FirstName', 'pH2email', 'pH2phone']} styleName="" name="pH2LastName" onChange={() => setPHToggle(props)} />
-          </div>
-          <div className="flex-parent col2">
-            <PhoneField validations={['phone']} label="Primary Phone" dependsOn={['pH2FirstName', 'pH2LastName', 'pH2email']} styleName="" name="pH2phone" onChange={() => setPHToggle(props)} />
-            <PhoneField validations={['phone']} label="Secondary Phone" styleName="" name="pH2secondaryPhone" onChange={() => setPHToggle(props)} />
-          </div>
-          <div className="flex-parent">
-            <TextField validations={['email']} label="Email Address" dependsOn={['pH2FirstName', 'pH2LastName', 'pH2phone']} styleName="" name="pH2email" onChange={() => setPHToggle(props)} />
-          </div>
+        <div className="flex-parent col2">
+          <Field
+            label="Primary Phone"
+            name="pH1phone"
+            component={Input}
+            validate={[validation.isRequired, validation.isPhone]}
+            normalize={normalize.phone}
+          />
+          <Field
+            label="Secondary Phone"
+            name="pH1secondaryPhone"
+            component={Input}
+            validate={validation.isPhone}
+          />
+        </div>
+        <div className="flex-parent">
+          <Field
+            label="Email Address"
+            name="pH1email"
+            component={Input}
+            validate={[validation.isRequired, validation.isEmail]}
+            normalize={normalize.email}
+          />
+          {/* electronic delivery question placeholder */ }
         </div>
       </div>
-    </section>
-  );
-};
+      {/* Col2 */}
+      <div className="flex-child">
+        <div className="flex-header-wrap">
+          <h3>Secondary Policyholder</h3>
+          <div className="check-box-wrapper">
+            <Field
+              onChange={event => clearSecondaryPolicyholder(String(event.target.value) === 'false', props)}
+              name="clearFields"
+              id="clearFields"
+              component="input"
+              type="checkbox"
+              disabled={!(props.policy && props.policy.policyHolders && props.policy.policyHolders[1])}
+            />
+            <label htmlFor="clearFields">Remove</label>
+          </div>
+        </div>
+        <div className="flex-parent col2">
+          <Field
+            label="First Name"
+            name="pH2FirstName"
+            component={Input}
+            validate={validation.dependsOn(['pH2LastName', 'pH2email', 'pH2phone'])}
+            onChange={() => setPHToggle(props)}
+          />
+          <Field
+            label="Last Name"
+            name="pH2LastName"
+            component={Input}
+            validate={validation.dependsOn(['pH2FirstName', 'pH2email', 'pH2phone'])}
+            onChange={() => setPHToggle(props)}
+          />
+        </div>
+        <div className="flex-parent col2">
+          <Field
+            label="Primary Phone"
+            component={Input}
+            validate={[validation.dependsOn(['pH2FirstName', 'pH2LastName', 'pH2email']), validation.isPhone]}
+            name="pH2phone"
+            onChange={() => setPHToggle(props)}
+          />
+          <Field
+            label="Secondary Phone"
+            name="pH2secondaryPhone"
+            component={Input}
+            validate={validation.isPhone}
+            onChange={() => setPHToggle(props)}
+          />
+        </div>
+        <div className="flex-parent">
+          <Field
+            label="Email Address"
+            name="pH2email"
+            component={Input}
+            validate={[validation.dependsOn(['pH2FirstName', 'pH2LastName', 'pH2phone']), validation.isEmail]}
+            onChange={() => setPHToggle(props)}
+          />
+        </div>
+      </div>
+    </div>
+  </section>
+);
 
 PolicyHolder.propTypes = {};
 
