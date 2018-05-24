f46fae1c559a8ef65fc50e3da8af83902aca4764
Jordan Rhodes <jsrhodes15@gmail.com>
2018-05-24 09:40:51 -0400

    Add tests for service runner

---
+++ src/utilities/serviceRunner.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+export function handleError(err) {
+  let error = err.response && err.response.data ? err.response.data : err;
+  if (typeof error === 'string') error = { message: error };
+  if (!error.message) error.message = 'There was an error.';
+  return { ...error };
+}
+
+export async function callService(data) {
+  const axiosConfig = {
+    method: 'POST',
+    url: `${process.env.REACT_APP_API_URL}/svc`,
+    headers: { 'Content-Type': 'application/json' },
+    data
+  };
+
+  try {
+    const response = await axios(axiosConfig);
+    return response;
+  } catch (error) {
+    const friendlyError = handleError(error);
+    throw friendlyError;
+  }
+}
