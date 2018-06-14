4c8a9c96cf0a43095013af666415f3fce93ac861
Jordan Rhodes <jsrhodes15@gmail.com>
2018-06-14 13:16:46 -0400

    Update forms util to handle questions being undefined

--- src/utilities/forms.js
+++ src/utilities/forms.js
@@ -1,6 +1,12 @@
-export const getAnswers = (name, questions) => questions[name] ? (questions[name].answers || []) : [];
+export const getAnswers = (name, questions) => {
+  if (!questions) return [];
+  return questions[name] ? (questions[name].answers || []) : [];
+};
 
-export const getQuestionName = (name, questions) => questions[name] ? (questions[name].question || '') : '';
+export const getQuestionName = (name, questions) => {
+  if (!questions) return '';
+  return questions[name] ? (questions[name].question || '') : '';
+};
 
 export default {
   getAnswers,
