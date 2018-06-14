f96fa7b82ce9bdd32a7a086a72336ffd6ee9de7f
Mark Eads <meads@hcpci.com>
2018-06-14 10:02:23 -0400

    added blank templates for agency

---
+++ src/modules/Agency/Agents.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const Agents = () => (
+  <React.Fragment>
+    <div />
+  </React.Fragment>);
+
+export default Agents;
---
+++ src/modules/Agency/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { Route } from 'react-router-dom';
+import AgencyHeader from './AgencyHeader';
+import AgencySideNav from './AgencySideNav';
+import AgencyDetailHeader from './DetailHeader';
+import Loader from '../../components/Common/Loader';
+import Overview from './Overview';
+import Contracts from './Contracts';
+import Agents from './Agents';
+
+export const Agency = props => (
+  <div className="app-wrapper csr agency">
+    <Helmet><title>{props.agency && props.agency.agencyCode ? `A: ${props.agency.agencyCode}` : 'Harmony - CSR Portal'}</title></Helmet>
+    <AgencyHeader />
+    <AgencyDetailHeader />
+    <main role="document">
+      { !props.agency && <Loader />}
+      <aside className="content-panel-left">
+        <AgencySideNav />
+      </aside>
+      <div className="content-wrapper">
+        <Route exact path={`${props.match.url}/overview`} render={prop => <Overview auth={props.auth} {...prop} />} />
+        <Route exact path={`${props.match.url}/contracts`} render={prop => <Contracts auth={props.auth} {...prop} />} />
+        <Route exact path={`${props.match.url}/agents`} render={prop => <Agents auth={props.auth} {...prop} />} />
+      </div>
+    </main>
+  </div>
+);
+
+export default Agency;
