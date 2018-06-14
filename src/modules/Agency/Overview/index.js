ac5f2fd7b37095597caa2753e2676e8ff9a98671
Mark Eads <meads@hcpci.com>
2018-06-14 11:25:10 -0400

    updated overview components and children components

--- src/modules/Agency/Overview/index.js
+++ src/modules/Agency/Overview/index.js
@@ -1,8 +1,9 @@
 import { connect } from 'react-redux';
 import OverviewComponent from './Overview';
+import { getAgency } from '../../../state/actions/serviceActions';
 
 const mapStateToProps = state => ({
   agency: state.service.agency
 });
 
-export default connect(mapStateToProps)(OverviewComponent);
+export default connect(mapStateToProps, { getAgency })(OverviewComponent);
