1f872198df3fffbe514f219673f472e06163c253
Mark Eads <meads@hcpci.com>
2018-06-14 16:06:41 -0400

    fixed broken test

--- src/modules/Agency/Overview/ContactCSR.spec.js
+++ src/modules/Agency/Overview/ContactCSR.spec.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { ContactCSR } from './ContactCSR';
-import mockAgency from './mockAgency';
+import mockAgency from '../mockAgency';
 
 describe('Testing ContactCard component', () => {
   it('should render', () => {
