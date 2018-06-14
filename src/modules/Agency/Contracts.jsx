42424e241b9878803944949e956a8720535e84e2
Mark Eads <meads@hcpci.com>
2018-06-14 10:07:18 -0400

    updated text

--- src/modules/Agency/Contracts.jsx
+++ src/modules/Agency/Contracts.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 
 export const Contracts = () => (
-  <React.Fragment>
-    <div />
-  </React.Fragment>
-);
+  <div>
+    Contracts
+  </div>);
 
 export default Contracts;
