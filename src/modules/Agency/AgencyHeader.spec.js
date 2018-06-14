71265fc55ab556d63e7e52a9a60c954b1e1d7ad6
Mark Eads <meads@hcpci.com>
2018-06-14 14:54:04 -0400

    added agencyTests

---
+++ src/modules/Agency/AgencyHeader.spec.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import AgencyHeader from './AgencyHeader';
+
+describe('Testing AgencyHeader component', () => {
+  it('should render', () => {
+    const wrapper = shallow(<AgencyHeader />);
+    expect(wrapper).toBeTruthy;
+  });
+});
