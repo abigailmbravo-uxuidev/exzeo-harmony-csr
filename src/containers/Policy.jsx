f9d040861a3333c013ea76944e348f6c8381846c
Mark Eads <meads@hcpci.com>
2018-06-18 11:53:36 -0400

    updated undefined bug with zip-code settings
    updated undefined bug with zip-code settings

--- src/containers/Policy.jsx
+++ src/containers/Policy.jsx
@@ -15,36 +15,41 @@ import EditEffectiveDataPopUp from '../components/Policy/EditEffectiveDatePopup'
 import ReinstatePolicyPopup from '../components/Policy/ReinstatePolicyPopup';
 
 export const hideEffectiveDatePopUp = (props) => {
-  props.actions.appStateActions.setAppState(props.appState.modelName, props.appState.instanceId,
-      { ...props.appState.data, showEffectiveDateChangePopUp: false });
+  props.actions.appStateActions.setAppState(
+    props.appState.modelName, props.appState.instanceId,
+    { ...props.appState.data, showEffectiveDateChangePopUp: false }
+  );
 };
 
 export const showEffectiveDatePopUp = (props) => {
-  props.actions.appStateActions.setAppState(props.appState.modelName, props.appState.instanceId,
-      { ...props.appState.data, showEffectiveDateChangePopUp: true });
+  props.actions.appStateActions.setAppState(
+    props.appState.modelName, props.appState.instanceId,
+    { ...props.appState.data, showEffectiveDateChangePopUp: true }
+  );
 };
 
 export const hideReinstatePolicyPopUp = (props) => {
-  props.actions.appStateActions.setAppState(props.appState.modelName, props.appState.instanceId,
-      { ...props.appState.data, showReinstatePolicyPopUp: false, submitting: false });
+  props.actions.appStateActions.setAppState(
+    props.appState.modelName, props.appState.instanceId,
+    { ...props.appState.data, showReinstatePolicyPopUp: false, submitting: false }
+  );
 };
 
 export const reinstatePolicySubmit = (data, dispatch, props) => {
-
   props.actions.appStateActions.setAppState(props.appState.modelName, props.appState.instanceId, { ...props.appState.data, submitting: true });
 
   const { policy, summaryLedger } = props;
   const submitData = {
-    "policyID": policy.policyID,
-    "policyNumber": policy.policyNumber,
-    "billingStatus": summaryLedger.status.code,
-    "transactionType":  "Reinstatement"
-    };
-    props.actions.serviceActions.createTransaction(submitData).then(() => {
-      hideReinstatePolicyPopUp(props);
-      props.actions.policyStateActions.updatePolicy(true, policy.policyNumber);
-    });
-}
+    policyID: policy.policyID,
+    policyNumber: policy.policyNumber,
+    billingStatus: summaryLedger.status.code,
+    transactionType: 'Reinstatement'
+  };
+  props.actions.serviceActions.createTransaction(submitData).then(() => {
+    hideReinstatePolicyPopUp(props);
+    props.actions.policyStateActions.updatePolicy(true, policy.policyNumber);
+  });
+};
 
 export const changeEffectiveDate = (data, dispatch, props) => {
   const effectiveDateUTC = moment.tz(moment.utc(data.effectiveDate).format('YYYY-MM-DD'), props.zipCodeSetting.timezone).format();
@@ -54,7 +59,9 @@ export const changeEffectiveDate = (data, dispatch, props) => {
   props.actions.cgActions.startWorkflow('effectiveDateChangeModel', { policyNumber: props.policy.policyNumber, policyID: props.policy.policyID }).then((result) => {
     const steps = [{
       name: 'saveEffectiveDate',
-      data: { policyNumber: props.policy.policyNumber, policyID: props.policy.policyID, effectiveDateChangeReason: data.effectiveDateChangeReason, effectiveDate: effectiveDateUTC }
+      data: {
+        policyNumber: props.policy.policyNumber, policyID: props.policy.policyID, effectiveDateChangeReason: data.effectiveDateChangeReason, effectiveDate: effectiveDateUTC
+      }
     }];
     const startResult = result.payload ? result.payload[0].workflowData.effectiveDateChangeModel.data : {};
 
@@ -78,9 +85,9 @@ export class Policy extends React.Component {
     const { policy, appState, children } = this.props;
     return (
       <div className="app-wrapper csr policy">
-        {/* TODO: dynamically add policy # to title*/}
+        {/* TODO: dynamically add policy # to title */}
         <Helmet><title>{policy && policy.policyNumber ? `P: ${policy.policyNumber}` : 'Harmony - CSR Portal'}</title></Helmet>
-        {/* <NewNoteFileUploader/>*/}
+        {/* <NewNoteFileUploader/> */}
         <PolicyHeader />
         <PolicyDetailHeader />
         <main role="document">
@@ -113,7 +120,7 @@ const mapStateToProps = state => ({
   appState: state.appState,
   summaryLedger: state.service.getSummaryLedger,
   policy: state.service.latestPolicy || {},
-  zipCodeSetting: state.service.getZipcodeSettings
+  zipCodeSetting: state.service.getZipcodeSettings || { timezone: '' }
 });
 
 const mapDispatchToProps = dispatch => ({
