09450d975f05b488aab119a96a18150844e1a152
Jordan Rhodes <jsrhodes15@gmail.com>
2018-06-21 14:37:46 -0400

    Fix default values to match schema from server

--- src/utilities/endorsementModel.js
+++ src/utilities/endorsementModel.js
@@ -71,18 +71,13 @@ export function initializeEndorsementForm(policy = {}) {
   values.coverageOptions.propertyIncidentalOccupanciesOtherStructures.answer = policy.coverageOptions.propertyIncidentalOccupanciesOtherStructures.answer || false;
   values.coverageOptions.liabilityIncidentalOccupancies.answer = policy.coverageOptions.liabilityIncidentalOccupancies.answer || false;
   // Wind Mitigation
-  values.property.yearOfRoof = policy.property.yearOfRoof || null;
   values.property.protectionClass = policy.property.protectionClass || '';
   values.property.buildingCodeEffectivenessGrading = policy.property.buildingCodeEffectivenessGrading || null;
-  values.buildingCodeEffectivenessGradingNew = values.property.buildingCodeEffectivenessGrading;
   values.property.familyUnits = policy.property.familyUnits || '';
-  // Home/Location Bottom Right
-  values.property.distanceToTidalWater = policy.property.distanceToTidalWater || '';
-  values.property.distanceToFireHydrant = policy.property.distanceToFireHydrant || '';
-  values.property.distanceToFireStation = policy.property.distanceToFireStation || '';
-  values.property.residenceType = policy.property.residenceType || '';
-  values.property.squareFeet = policy.property.squareFeet || '';
   values.property.floodZone = policy.property.floodZone || '';
+  // Home/Location Bottom Right
+  values.property.residenceType = policy.property.residenceType || '';
+  values.property.yearOfRoof = policy.property.yearOfRoof || null;
 
   return values;
 }
