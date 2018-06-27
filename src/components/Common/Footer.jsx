aa590df9ece746c45c64834f2dd1f05a03676b2f
elamison <eric@lamison.com>
2018-06-27 09:18:53 -0400

    Fixed copyright year.

--- src/components/Common/Footer.jsx
+++ src/components/Common/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Footer = () =>
   (<footer>
     <div role="banner">
-      <small className="copyright">
-        &copy;2017 TypTap Management Company. All rights reserved.
+      <small>
+        &copy;{new Date().getFullYear()} TypTap Management Company. All rights reserved.
       </small>
       <small>
         v.{process.env.REACT_APP_VERSION}
