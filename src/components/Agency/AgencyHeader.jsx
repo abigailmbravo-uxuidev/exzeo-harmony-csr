9e649d914f30e3a3862cb92f480def41401ab7c9
Mark Eads <meads@hcpci.com>
2018-07-02 16:29:17 -0400

    updated AI tests

--- src/components/Agency/AgencyHeader.jsx
+++ src/components/Agency/AgencyHeader.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from '../../img/Harmony.svg';
 
-const AgencyHeader = () => (
+export const AgencyHeader = () => (
   <header>
     <div role="banner">
       <div className="tab-tag">
