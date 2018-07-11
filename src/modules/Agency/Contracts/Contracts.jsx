98a19896aebfc2650da7ad28058c7cda5563a5d1
Mark Eads <meads@hcpci.com>
2018-07-11 11:31:32 -0400

    reworked contract card

--- src/modules/Agency/Contracts/Contracts.jsx
+++ src/modules/Agency/Contracts/Contracts.jsx
@@ -7,101 +7,27 @@ import ContractsCard from './ContractsCard';
 import ContractsModal from './ContractsModal';
 
 export class Contracts extends Component {
-  state = {
-    showEditAgencyContract: false,
-    editType: null,
-    showAgencyEdit: false,
-    agentsInContract: []
-  };
-
-  displayAgencyPopup = (showPopup) => {
-    this.setState({ showAgencyEdit: showPopup });
-  };
-
-  toggleContractModal = (editType, contractIndex, agentsInContract) => () => {
-    reset('ContractsModal');
-
-    this.setState({
-      editType,
-      contractIndex,
-      agentsInContract,
-      showEditAgencyContract: !this.state.showEditAgencyContract
-    });
-  };
-
-  toggleAgencyModal = () => {
-    this.setState({ showAgencyEdit: !this.state.showAgencyEdit });
-  };
-
-  saveContract = async (data) => {
-    data.license[this.state.contractIndex].agent || [];
-    this.props.updateAgency(data);
-    this.setState({ editType: null, showEditAgencyContract: false });
-  };
-
-  removeAgentFromList = (e, agentCode) => {
-    const { contractIndex } = this.state;
-    const { contractFieldValues } = this.props;
-    const agentsInContract = contractFieldValues.license[contractIndex].agent;
-    change('ContractsModal', `license[${contractIndex}].agent`, agentsInContract.filter(a => a.agentCode !== agentCode));
-  }
-
-  addAgentFromList = (event) => {
-    const { value } = event.target;
-    const { listOfAgents, contractFieldValues } = this.props;
-    const { contractIndex } = this.state;
-
-    const agentsInContract = contractFieldValues.license[contractIndex].agent;
-
-    const agent = listOfAgents.find(a => a.agentCode === Number(value));
-    const {
-      appointed, agentOfRecord, agentCode, agentInfo
-    } = agent;
-    if (agentsInContract.filter(a => a.agentCode === agent.agentCode).length === 0) {
-      agentsInContract.push({
-        appointed, agentOfRecord, agentCode, agentInfo: agentInfo || { ...agent }
-      });
-      change('ContractsModal', `license[${contractIndex}].agent`, agentsInContract);
-    }
-    return value;
-  }
-
-
   render() {
     const {
-      agency, contractInitialValues, listOfAgents, contractFieldValues
+      agency
     } = this.props;
     if (!agency) return <div />;
     const { license } = agency;
     return (
       <div id="agency-contracts" className="agency-contracts">
-        {this.state.showEditAgencyContract && (
-        <ContractsModal
-          agentsInContract={this.state.agentsInContract}
-          initialValues={contractInitialValues}
-          fieldValues={contractFieldValues}
-          toggleModal={this.toggleContractModal}
-          editType={this.state.editType}
-          contractIndex={this.state.contractIndex}
-          saveContract={this.saveContract}
-          removeAgentFromList={this.removeAgentFromList}
-          listOfAgents={listOfAgents}
-          addAgentFromList={this.addAgentFromList}
-        />
-          )}
         <div className="route-content">
           <div className="scroll">
             <div className="form-group survey-wrapper" role="group">
               <TaxDetails agency={agency} editAgency={this.toggleAgencyModal} />
               <section>
                 <h3>Contracts</h3>
-                { license && license.length > 0 && license.map((contract, index) =>
-                  <ContractsCard key={contract.licenseNumber} contractIndex={index} contract={contract} editContract={this.toggleContractModal} />)}
+                { license && license.length > 0 && license.map((l, index) =>
+                  <ContractsCard key={`${l.licenseNumber}_${index}`} contractIndex={index} license={l} editContract={x => x} />)}
                 <div className="create-contract">
                   <hr />
                   <button
                     className="btn btn-sm btn-primary"
-                    onClick={this.toggleContractModal('New', license ? license.length : 0)}
+                    onClick={x => x}
                   >
                     <i className="fa fa-plus" /> Contract
                   </button>
@@ -111,12 +37,6 @@ export class Contracts extends Component {
             </div>
           </div>
         </div>
-        {this.state.showAgencyEdit && (
-          <AgencyModal
-            isEdit
-            closeModal={this.toggleAgencyModal}
-          />
-        )}
       </div>
     );
   }
--- src/modules/Agency/Contracts/Contracts.old.spec.js
+++ src/modules/Agency/Contracts/Contracts.old.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Contracts } from './Contracts';
+import { Contracts } from './Contracts.old';
 import mockAgency from '../mockAgency';
 
 describe('Testing Contracts component', () => {
@@ -9,5 +9,10 @@ describe('Testing Contracts component', () => {
     const wrapper = shallow(<Contracts {...props} agency={mockAgency} state={{ contractIndex: 0 }} />);
     expect(wrapper.exists()).toBeTruthy();
     const wi = wrapper.instance();
+
+    wi.displayAgencyPopup(true);
+    wi.toggleContractModal('Edit', 0)();
+    wi.toggleAgencyModal();
+    wi.saveContract({ agentList: [], license: [{}] });
   });
 });
