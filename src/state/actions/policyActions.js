aa8fdadb2aeb0f09b392533b4e3488c3b29a30be
Mark Eads <meads@hcpci.com>
2018-07-16 09:45:48 -0400

    Using the model so the endpoint waits for the data to save

--- src/state/actions/policyActions.js
+++ src/state/actions/policyActions.js
@@ -2,6 +2,7 @@ import { convertToRateData } from '../../utilities/endorsementModel';
 import * as serviceRunner from '../../utilities/serviceRunner';
 import * as types from './actionTypes';
 import * as errorActions from './errorActions';
+import * as cgActions from './cgActions';
 import endorsementUtils from '../../utilities/endorsementModel';
 
 
@@ -340,14 +341,17 @@ export function submitEndorsementForm(formData, formProps) {
     const submitData = endorsementUtils.generateModel(formData, formProps);
     const forms = await fetchListOfForms(formProps.policy, submitData.rating, 'New Business');
     submitData.forms = forms;
-    const newPolicy = await dispatch(createTransaction(submitData));
 
-    if (newPolicy && newPolicy.policyNumber) {
-      // createTransaction sends data to a messaging queue which doesn't update fast enough when this is called
-      // Should implement messaging onthe  UI to subscribe to the queue
-      await setTimeout(async x => x, 3000);
-      await dispatch(getPolicy(newPolicy.policyNumber));
-    }
+    // TODO: Make cg actions a utility rather than stored in state
+    const result = await dispatch(cgActions.startWorkflow('endorsePolicyModelSave'));
+
+    const steps = [{
+      name: 'saveEndorsement',
+      data: submitData
+    }];
+    const startResult = result.payload ? result.payload[0].workflowData.endorsePolicyModelSave.data : {};
+    await dispatch(cgActions.batchCompleteTask(startResult.modelName, startResult.modelInstanceId, steps));
+    await dispatch(getPolicy(submitData.policyNumber));
   };
 }
 
