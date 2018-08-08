150ed8c77709baf481e4c3a571eebd08b218b12d
Mark Eads <meads@hcpci.com>
2018-08-08 09:13:48 -0400

    added thousandSeparator false

--- src/modules/Search/Agent/AgentSearch.jsx
+++ src/modules/Search/Agent/AgentSearch.jsx
@@ -5,67 +5,66 @@ import { Input, Integer } from '@exzeo/core-ui/lib/Input';
 import { isAlphaNumeric, isValidChar } from '@exzeo/core-ui/lib/InputLifecycle';
 import Button from '@exzeo/core-ui/lib/Button/index';
 
-const AgentSearch = ({ submitting }) => {
-  return (
-    <div className="search-inputs fade-in">
-      <Field
-        name="agentCode"
-        dataTest="agentCode"
-        component={Integer}
-        placeholder="Agent ID Search"
-        label="Agent ID"
-        styleName="agency-id-search"
-      />
-      <Field
-        name="firstName"
-        dataTest="firstName"
-        component={Input}
-        placeholder="First Name Search"
-        label="First Name"
-        styleName="first-name-search"
-        errorHint
-        validate={isAlphaNumeric}
-      />
-      <Field
-        name="lastName"
-        dataTest="lastName"
-        component={Input}
-        placeholder="Last Name Search"
-        label="Last Name"
-        styleName="last-name-search"
-        errorHint
-        validate={isAlphaNumeric}
-      />
-      <Field
-        name="address"
-        dataTest="address"
-        component={Input}
-        placeholder="Agent Address Search"
-        label="Agent Address"
-        styleName="agency-address-search"
-        errorHint
-        validate={isValidChar}
-      />
-      <Field
-        name="licenseNumber"
-        dataTest="licenseNumber"
-        component={Input}
-        placeholder="Lic No Search"
-        label="Lic Number"
-        styleName="agency-reg-lic-fein-search"
-      />
+const AgentSearch = ({ submitting }) => (
+  <div className="search-inputs fade-in">
+    <Field
+      name="agentCode"
+      dataTest="agentCode"
+      component={Integer}
+      placeholder="Agent ID Search"
+      label="Agent ID"
+      styleName="agency-id-search"
+      thousandSeparator={false}
+    />
+    <Field
+      name="firstName"
+      dataTest="firstName"
+      component={Input}
+      placeholder="First Name Search"
+      label="First Name"
+      styleName="first-name-search"
+      errorHint
+      validate={isAlphaNumeric}
+    />
+    <Field
+      name="lastName"
+      dataTest="lastName"
+      component={Input}
+      placeholder="Last Name Search"
+      label="Last Name"
+      styleName="last-name-search"
+      errorHint
+      validate={isAlphaNumeric}
+    />
+    <Field
+      name="address"
+      dataTest="address"
+      component={Input}
+      placeholder="Agent Address Search"
+      label="Agent Address"
+      styleName="agency-address-search"
+      errorHint
+      validate={isValidChar}
+    />
+    <Field
+      name="licenseNumber"
+      dataTest="licenseNumber"
+      component={Input}
+      placeholder="Lic No Search"
+      label="Lic Number"
+      styleName="agency-reg-lic-fein-search"
+    />
 
-      <Button
-        baseClass="success"
-        customClass="multi-input"
-        type="submit"
-        disabled={submitting}
-        dataTest="submit"
-      ><i className="fa fa-search" />Search
-      </Button>
-    </div>
-  );
-};
+    <Button
+      baseClass="success"
+      customClass="multi-input"
+      type="submit"
+      disabled={submitting}
+      dataTest="submit"
+    ><i className="fa fa-search" />Search
+    </Button>
+  </div>
+);
 
 AgentSearch.propTypes = {
   submitting: PropTypes.bool
