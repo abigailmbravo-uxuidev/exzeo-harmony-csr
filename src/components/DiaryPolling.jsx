cfb13d3b2760ee8823ade87b7e5aa28b65f95d1b
Jordan Rhodes <jsrhodes15@gmail.com>
2018-08-29 15:48:55 -0400

    Uncomment diaries polling method

--- src/components/DiaryPolling.jsx
+++ src/components/DiaryPolling.jsx
@@ -5,22 +5,22 @@ import { connect } from 'react-redux';
 import { fetchDiaries } from '../state/actions/diaryActions';
 
 class DiaryPolling extends Component {
-  // componentDidMount() {
-  //   // Set the name of the hidden property and the change event for visibility
-  //   if (typeof document.hidden !== 'undefined') { // Opera 12.10 and Firefox 18 and later support
-  //     this.hidden = 'hidden';
-  //     this.visibilityChange = 'visibilitychange';
-  //   } else if (typeof document.msHidden !== 'undefined') {
-  //     this.hidden = 'msHidden';
-  //     this.visibilityChange = 'msvisibilitychange';
-  //   } else if (typeof document.webkitHidden !== 'undefined') {
-  //     this.hidden = 'webkitHidden';
-  //     this.visibilityChange = 'webkitvisibilitychange';
-  //   }
-  // 
-  //   this.fetchDiaries();
-  //   this.attemptFetchDiaries();
-  // }
+  componentDidMount() {
+    // Set the name of the hidden property and the change event for visibility
+    if (typeof document.hidden !== 'undefined') { // Opera 12.10 and Firefox 18 and later support
+      this.hidden = 'hidden';
+      this.visibilityChange = 'visibilitychange';
+    } else if (typeof document.msHidden !== 'undefined') {
+      this.hidden = 'msHidden';
+      this.visibilityChange = 'msvisibilitychange';
+    } else if (typeof document.webkitHidden !== 'undefined') {
+      this.hidden = 'webkitHidden';
+      this.visibilityChange = 'webkitvisibilitychange';
+    }
+
+    this.fetchDiaries();
+    this.attemptFetchDiaries();
+  }
 
   componentWillUnmount() {
     if (this.delegate) clearInterval(this.delegate);
