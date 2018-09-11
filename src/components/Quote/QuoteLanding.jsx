046dba83fd44869acaa63d62768f7076f2c68f15
Mark Eads <meads@hcpci.com>
2018-09-11 11:26:18 -0400

    setup for new quote model

--- src/components/Quote/QuoteLanding.jsx
+++ src/components/Quote/QuoteLanding.jsx
@@ -16,99 +16,18 @@ export class QuoteLanding extends Component {
     } = this.props;
 
     try {
-      if (newQuote) {
-        const { stateCode, propertyId } = params;
-        const res = await startWorkflow('csrCreateQuote', { stateCode, igdId: propertyId });
-        const csrCreateQuoteResult = res.payload ? res.payload[0].workflowData.csrCreateQuote.data : {};
-
-        // ************************* REMOVE ONCE COVERAGE IS FIXED ***************
-        const result = await startWorkflow('csrQuote', { dsUrl: `${process.env.REACT_APP_API_URL}/ds` });
-        const steps = [];
-        steps.push({
-          name: 'search',
-          data: {
-            searchType: SEARCH_TYPES.quote,
-            // TODO: properties needed to kick off model. Looking into removing these.
-            address: '',
-            firstName: '',
-            lastName: '',
-            policyNumber: '',
-            quoteNumber: '',
-            quoteState: ''
-          }
-        });
-        steps.push({
-          name: 'chooseQuote',
-          data: {
-            quoteId: csrCreateQuoteResult.previousTask.value.result._id
-          }
-        });
-
-
-        const startResult = result.payload ? result.payload[0].workflowData.csrQuote.data : {};
-
-        setAppState('csrQuote', startResult.modelInstanceId, {
-          ...appState.data,
-          submitting: true
-        });
-
-        await batchCompleteTask(startResult.modelName, startResult.modelInstanceId, steps);
-        setAppState(appState.modelName, startResult.modelInstanceId, {
-          ...appState.data,
-          selectedLink: 'customerData'
-        });
-        // ************************* REMOVE ONCE COVERAGE IS FIXED ***************
-      } else {
-        // still old stuff
-        const result = await startWorkflow('csrQuote', { dsUrl: `${process.env.REACT_APP_API_URL}/ds` });
-        const steps = [];
-        steps.push({
-          name: 'search',
-          data: {
-            searchType: SEARCH_TYPES.quote,
-            // TODO: properties needed to kick off model. Looking into removing these.
-            address: '',
-            firstName: '',
-            lastName: '',
-            policyNumber: '',
-            quoteNumber: '',
-            quoteState: ''
-          }
-        });
-        steps.push({
-          name: 'chooseQuote',
-          data: {
-            quoteId: params.quoteId
-          }
-        });
-
-
-        const startResult = result.payload ? result.payload[0].workflowData.csrQuote.data : {};
-        // set property to pass to redirect link
-        this.workflowId = startResult.modelInstanceId;
-
-        setAppState('csrQuote', startResult.modelInstanceId, {
-          ...appState.data,
-          submitting: true
-        });
-
-        await batchCompleteTask(startResult.modelName, startResult.modelInstanceId, steps);
-        setAppState(appState.modelName, startResult.modelInstanceId, {
-          ...appState.data,
-          selectedLink: 'customerData'
-        });
-      }
+      const { stateCode, propertyId } = params;
+      await startWorkflow('csrCreateQuote', { stateCode, igdId: propertyId });
     } catch (error) {
       setAppError(error);
     }
   }
 
   render() {
-    const { quoteData, appState } = this.props;
-    console.log(appState);
+    const { quoteData } = this.props;
     return (
       <React.Fragment>
-        {quoteData && quoteData._id && appState.instanceId ? <Redirect replace to={`/quote/${quoteData._id}/coverage/${appState.instanceId}`} /> : <Loader />}
+        {quoteData && quoteData._id ? <Redirect replace to={`/quote/${quoteData._id}/coverage`} /> : <Loader />}
       </React.Fragment>
     );
   }
