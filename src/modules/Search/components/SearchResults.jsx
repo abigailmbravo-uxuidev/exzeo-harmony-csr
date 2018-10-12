4c7cef71ccdfd0662c502a3a15d0cae3b380bb61
Mark Eads <meads@hcpci.com>
2018-10-12 10:51:25 -0400

    added target blank

--- src/modules/Search/components/SearchResults.jsx
+++ src/modules/Search/components/SearchResults.jsx
@@ -93,9 +93,9 @@ export class SearchResults extends Component {
             ))}
           </div>
         }
-          {!hasSearched && searchType === SEARCH_TYPES.agency &&
+        {!hasSearched && searchType === SEARCH_TYPES.agency &&
           <div className="btn-divider-wrapper">
-            <NavLink className="btn btn-primary" to="/agency/new" activeClassName="active" exact>+ Agency</NavLink>
+            <NavLink className="btn btn-primary" to="/agency/new" activeClassName="active" target="_blank" exact>+ Agency</NavLink>
           </div>
         }
       </div>
