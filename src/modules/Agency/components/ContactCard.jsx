d89605099218bcb0b201318ae45a9d566c766c20
Joe Sutphin <jsutphin@exzeo.com>
2018-10-17 15:41:08 -0400

    update contact card

--- src/modules/Agency/components/ContactCard.jsx
+++ src/modules/Agency/components/ContactCard.jsx
@@ -4,31 +4,30 @@ export const ContactCard = ({ contact, handleClick }) => (
   <div className="csr contact card">
     <div className="contact-title">
       <i className="fa fa-address-card" />
-      <label />
-      <div className="contact-details">
-        <h4><strong>{`${contact.firstName} ${contact.lastName}`} | Officer</strong></h4>
-        <div className="additional-contacts">
-          <ul>
-            <li>
-              <div className="contact-methods">
-                <p>
-                  <i className="fa fa-envelope" />
-                  <a href={`mailto:${contact.emailAddress}`}>
-                    {contact.emailAddress}
-                  </a>
-                </p>
-              </div>
-            </li>
-          </ul>
-        </div>
+    </div>
+    <div className="contact-details">
+      <h4><strong>{`${contact.firstName} ${contact.lastName}`} | Officer</strong></h4>
+      <div className="additional-contacts">
+        <ul>
+          <li>
+            <div className="contact-methods">
+              <p>
+                <i className="fa fa-envelope" />
+                <a href={`mailto:${contact.emailAddress}`}>
+                  {contact.emailAddress}
+                </a>
+              </p>
+            </div>
+          </li>
+        </ul>
       </div>
-      <div className="contact-actions">
-        <button
-          className="btn btn-link btn-sm"
-          onClick={handleClick}>
-          <i className="fa fa-pencil-square" />Edit
-        </button>
-      </div>
+    </div>
+    <div className="contact-actions">
+      <button
+        className="btn btn-link btn-sm"
+        onClick={handleClick}>
+        <i className="fa fa-pencil-square" />Edit
+      </button>
     </div>
   </div>
 );
