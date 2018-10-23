2cefbc6fecaab03543199d3540d4aad80773a908
Joe Sutphin <jsutphin@exzeo.com>
2018-10-23 14:43:08 -0400

    update style

--- src/modules/Agency/Contracts/ContractModal.jsx
+++ src/modules/Agency/Contracts/ContractModal.jsx
@@ -6,7 +6,7 @@ import { validation } from '@exzeo/core-ui';
 import CheckBoxGroup from '../CheckBoxGroup';
 
 export const renderProducts = ({ fields, meta: { error, submitFailed } }) =>
-  <div className="additional-contacts">
+  <div className="csp-list">
     <ul className="contract-agent-list">
     {fields.map((member, index) =>
       <li key={index}>
@@ -26,15 +26,15 @@ export const renderProducts = ({ fields, meta: { error, submitFailed } }) =>
           validate={validation.isRequired}
         />
         <i className="fa fa-file-text" onClick={() => fields.remove(index)} />
-      </li>      
+      </li>
     )}
     </ul>
   </div>
 
 export const ContractModal = (props) => {
   const {
-    closeModal, 
-    saveContract, 
+    closeModal,
+    saveContract,
     handleSubmit,
     initialValues
   } = props;
@@ -42,58 +42,48 @@ export const ContractModal = (props) => {
   const actionType = initialValues ? 'Edit' : "Add";
   return (
     <div className="modal contract-crud">
-      <Form noValidate onSubmit={handleSubmit(saveContract)}>
-        <div className="card">
+      <div className="card">
+        <Form noValidate onSubmit={handleSubmit(saveContract)}>
           <div className="card-header">
-            <h4><i className="fa fa-file" />{actionType} Contract</h4>
+            <h4><i className="fa fa-file" /> {actionType} Contract</h4>
           </div>
           <div className="card-block">
             <section className="contract-details">
-              <div className="flex-form">
-                <Field
-                  label="Company Code"
-                  styleName="companyCode"
-                  name={`companyCode`}
-                  dataTest={`companyCode`}
-                  component={Input}
-                  validate={validation.isRequired}
-                />
-              </div>
-              <div className="flex-form">
-                <Field
-                  label="Contract"
-                  styleName="contractNumber"
-                  name={`contractNumber`}
-                  dataTest={`contractNumber`}
-                  component={Input}
-                  validate={validation.isRequired}
-                />
-              </div>
-              <div className="flex-form">
-                <Field
-                  label="Addendum"
-                  styleName="addendum"
-                  name={`addendum`}
-                  dataTest={`addendum`}
-                  component={Input}
-                  validate={validation.isRequired}
-                />
-              </div>
-              <div className="flex-form">
-                <Field
-                  label="EO Expiration Date"
-                  styleName="eoExpirationDate"
-                  name={`eoExpirationDate`}
-                  dataTest={`eoExpirationDate`}
-                  component={Input}
-                  validate={validation.isRequired}
-                />
-              </div>
+              <Field
+                label="Company Code"
+                styleName="companyCode"
+                name={`companyCode`}
+                dataTest={`companyCode`}
+                component={Input}
+                validate={validation.isRequired}
+              />
+              <Field
+                label="Contract"
+                styleName="contractNumber"
+                name={`contractNumber`}
+                dataTest={`contractNumber`}
+                component={Input}
+                validate={validation.isRequired}
+              />
+              <Field
+                label="Addendum"
+                styleName="addendum"
+                name={`addendum`}
+                dataTest={`addendum`}
+                component={Input}
+                validate={validation.isRequired}
+              />
+              <Field
+                label="EO Expiration Date"
+                styleName="eoExpirationDate"
+                name={`eoExpirationDate`}
+                dataTest={`eoExpirationDate`}
+                component={Input}
+                validate={validation.isRequired}
+              />
             </section>
-            <section className="contract-details">
-              <div className="flex-form">
-                <FieldArray name="stateProducts" component={renderProducts} />
-              </div>
+            <section className="contract-csp">
+              <FieldArray name="stateProducts" component={renderProducts} />
             </section>
           </div>
           <div className="card-footer">
@@ -102,8 +92,8 @@ export const ContractModal = (props) => {
               <button tabIndex="0" className="btn btn-primary" type="submit">Save</button>
             </div>
           </div>
-        </div>
-      </Form>
+        </Form>
+      </div>
     </div>
   );
 };
