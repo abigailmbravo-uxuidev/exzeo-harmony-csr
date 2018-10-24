7d42ed957e2230840f2d4c489368f4bbdfc0e690
Mark Eads <meads@hcpci.com>
2018-10-24 08:56:00 -0500

    added agencyModal tests

--- src/modules/Agency/Overview/AgencyModal.jsx
+++ src/modules/Agency/Overview/AgencyModal.jsx
@@ -1,38 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { reduxForm, Field } from 'redux-form';
-import { Select, Radio, Input, Integer, validation, Phone } from '@exzeo/core-ui';
+import { reduxForm } from 'redux-form';
 
 import { updateAgency } from '../../../state/actions/agencyActions';
 import AgencyDetails from '../components/FormGroup/AgencyDetails';
 
-const statusAnswers = [
-  { answer: 'Active', label: 'Active' },
-  { answer: 'InActive', label: 'InActive' }
-];
-
-const okToPayAnswers = [
-  { answer: false, label: 'No' },
-  { answer: true, label: 'Yes' }
-];
-
-const taxClassificationAnswers = [
-  { answer: 'LLC', label: 'LLC' },
-  { answer: 'Corporation', label: 'Corporation' }
-];
-
 export class AgencyModal extends Component {
   saveAgency = async (data, dispatch, props) => {
     data.branches = data.branches.filter(b => String(b.branchCode) !== '0');
-    await props.updateAgency(data);
-    props.closeModal();
+    await this.props.updateAgency(data);
+    this.props.closeModal();
   };
 
   render() {
     const {
       closeModal,
       handleSubmit,
-      initialValues,
       submitting
     } = this.props;
 
