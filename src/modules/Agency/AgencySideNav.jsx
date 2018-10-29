04b2c873bde0ea152f86c1a2efa0aeb9b54b1ef9
Joe Sutphin <jsutphin@exzeo.com>
2018-10-29 09:17:43 -0400

    update branch select to hide and show based on number

--- src/modules/Agency/AgencySideNav.jsx
+++ src/modules/Agency/AgencySideNav.jsx
@@ -86,18 +86,20 @@ export class SideNav extends React.Component {
         {branchSelectionRoute && !branchSelectionRoute.includes(url) && <Redirect replace to={branchSelectionRoute} />}
         <nav className="site-nav">
           <ul>
-            {String(branchCode) === '0' && agencyCode !== 'new' &&
-            <li key="newBranch" >
-              <NavLink
-                to={`/agency/${agencyCode}/${branchCode}/new`}
-                tabIndex="0"
-                className="btn btn-secondary btn-block btn-xs btn-branch">
-                <i className="fa fa-plus" />Branch
-              </NavLink>
-            </li>
+            {(String(branchCode) === '0' && agencyCode !== 'new') &&
+            <React.Fragment>
+              <li key="newBranch" >
+                <NavLink
+                  to={`/agency/${agencyCode}/${branchCode}/new`}
+                  tabIndex="0"
+                  className="btn btn-secondary btn-block btn-xs btn-branch">
+                  <i className="fa fa-plus" />Branch
+                </NavLink>
+              </li>
+              <hr className="nav-division" />
+            </React.Fragment>
             }
-            <hr className="nav-division" />
-            {agencyCode !== 'new' &&
+            {(branchesList.length > 1 && agencyCode !== 'new') &&
             <li key="branch">
               <Field
                 dataTest="selectedBranch"
