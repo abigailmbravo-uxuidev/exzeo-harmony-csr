c0119a9b5d1e1bbe2bd0a6c5b9680b18fdd68c19
Joe Sutphin <jsutphin@exzeo.com>
2018-10-30 09:21:08 -0400

    update header

--- src/components/Policy/DetailHeader.jsx
+++ src/components/Policy/DetailHeader.jsx
@@ -56,6 +56,15 @@ export class DetailHeader extends Component {
           data={details}
           dataTest="policyDetails"
           className="policyDetails">
+          <dd>
+            {policy.policyNumber}
+            <a
+              id="effective-date"
+              title={sourceNumber}
+              className="btn btn-link btn-xs btn-alt-light no-padding">
+              <i className="fa fa-crosshairs" />Source
+            </a>
+          </dd>
           <dd>{status}</dd>
         </Details>
 
