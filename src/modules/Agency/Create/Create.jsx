5881dc42b6720ad8545645b4010cc6ed8150ffc7
Mark Eads <meads@hcpci.com>
2018-11-02 14:59:52 -0400

    default AOR mailing address to agency mailing address

--- src/modules/Agency/Create/Create.jsx
+++ src/modules/Agency/Create/Create.jsx
@@ -23,6 +23,7 @@ export class Create extends Component {
       displayText: 'United States of America'
     };
     data.physicalAddress.country = data.mailingAddress.country;
+    data.agentOfRecord.mailingAddress = data.mailingAddress;
     await this.props.createAgency(data);
   };
 
