fefefa5d8fb1d1de80be18f467f3da0e4d4f05f3
Jordan Rhodes <jsrhodes15@gmail.com>
2018-11-08 11:09:45 -0500

    Default to empty object if no userProfile

--- src/state/selectors/entity.selectors.js
+++ src/state/selectors/entity.selectors.js
@@ -12,7 +12,7 @@ export const getDiaries = state => state.diaries;
 // AGENCY
 export const getAgencies = state => state.service.agencies;
 // AUTH
-export const getUserProfile = state => state.authState.userProfile;
+export const getUserProfile = state => state.authState.userProfile || emptyObject;
 
 // APP-STATE [DEPRECATED]
 export const getAppState = state => state.appState || emptyObject;
