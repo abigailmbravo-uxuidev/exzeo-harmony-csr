16a6077ccc501f20e7a6581bbf8347daeb587585
Jordan Rhodes <jsrhodes15@gmail.com>
2019-01-23 15:15:42 -0500

    Update tests

--- src/components/Policy/PolicySideNav.spec.js
+++ src/components/Policy/PolicySideNav.spec.js
@@ -56,9 +56,4 @@ describe('Testing PolicySideNav component', () => {
     const wrapper = shallow(<ConnectedApp store={store} {...props} />).dive().instance();
     expect(wrapper.newNote(props));
   });
-
-  it('should have six nav links', () => {
-    const wrapper = shallow(<ConnectedApp store={store} {...props} />).dive();
-    expect(wrapper.find('NavLink').length).toBe(6);
-  });
 });
