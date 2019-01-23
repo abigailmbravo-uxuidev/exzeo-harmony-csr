de8ff709573471b726e2d4db098fcfb73a05d4f2
Mark E <meads@hcpci.com>
2019-01-23 20:33:42 +0000

    Quote.jsx edited online with Bitbucket

--- src/containers/Quote.jsx
+++ src/containers/Quote.jsx
@@ -55,7 +55,6 @@ export class QuoteBase extends React.Component {
               {(quoteData && quoteData.quoteNumber) &&
                 <DiaryPolling filter={{ resourceId: quoteData.quoteNumber, resourceType: QUOTE_RESOURCE_TYPE }} />
               }
-              
             </React.Fragment>
           )} />
       </div>
