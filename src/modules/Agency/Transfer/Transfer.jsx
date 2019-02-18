82f5fb0c0e06abb24cab348151511aa5a06a977b
Mark Eads <meads@hcpci.com>
2019-02-18 11:21:58 -0500

    added bootstrap table for transfer policy

--- src/modules/Agency/Transfer/Transfer.jsx
+++ src/modules/Agency/Transfer/Transfer.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Loader } from '@exzeo/core-ui';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 
 import Footer from '../../../components/Common/Footer'
 import TransferModal from './TransferModal';
@@ -20,6 +21,7 @@ export class Transfer extends Component {
     const {
       agency,
       agents,
+      policies
     } = this.props;
 
     // if (!(agents && agents.length && agency)) return <Loader />;
@@ -37,8 +39,23 @@ export class Transfer extends Component {
           <div className="scroll">
             <div className="form-group survey-wrapper" role="group">
               <section className="policy-filter">
-              Bootstrap Table
-              </section>
+              <BootstrapTable
+      className="transfer compact-table"
+      data={policies}
+      multiColumnSearch>
+      <TableHeaderColumn dataField="_id" isKey hidden>ID</TableHeaderColumn>
+      <TableHeaderColumn className="policyNumber" columnClassName="policyNumber" dataField="policyNumber" dataSort filterFormatted filter={ { type: 'TextFilter', defaultValue: '' } }>PolicyNumber</TableHeaderColumn>
+      <TableHeaderColumn className="company" columnClassName="company" dataField="company" dataSort>Company</TableHeaderColumn>
+      <TableHeaderColumn className="state" columnClassName="state" dataField="state" dataSort filter={ { type: 'TextFilter', defaultValue: '' } }>State</TableHeaderColumn>
+      <TableHeaderColumn className="product" columnClassName="product" dataField="product" dataSort filter={ { type: 'TextFilter', defaultValue: '' } }>Product</TableHeaderColumn>
+      <TableHeaderColumn className="agent" columnClassName="agent" dataField="agent" dataSort filter={ { type: 'TextFilter', defaultValue: '' } }>Agent</TableHeaderColumn>
+      <TableHeaderColumn className="propertyAddress" columnClassName="propertyAddress" dataField="propertyAddress" dataSort>Property Address</TableHeaderColumn>
+      <TableHeaderColumn className="primaryPolicyholder" columnClassName="primaryPolicyholder" dataField="primaryPolicyholder" dataSort>Primary Policyholder</TableHeaderColumn>
+      <TableHeaderColumn className="effectiveDate" columnClassName="effectiveDate" dataField="effectiveDate" dataSort>Effective Date</TableHeaderColumn>
+      <TableHeaderColumn className="terms" columnClassName="terms" dataField="terms" dataSort>Terms</TableHeaderColumn>
+
+    </BootstrapTable>             
+     </section>
               <section className="policy-filter-actions">
               Bootstrap Table Actions
               </section>
