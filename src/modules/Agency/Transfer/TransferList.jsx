a37c5053491fcbb4f1e5a0d9608d830fe6417e85
Joe Sutphin <jsutphin@exzeo.com>
2019-02-19 15:49:14 -0500

    WIP style transfer grid

--- src/modules/Agency/Transfer/TransferList.jsx
+++ src/modules/Agency/Transfer/TransferList.jsx
@@ -14,49 +14,53 @@ export class TransferList extends Component {
     const { policies, handleSubmit, toggleTransferModal } = this.props;
     return (
       <Fragment>
-              <form id="TransferList" onSubmit={handleSubmit(toggleTransferModal)}>
-            <ul>
-                    <li> 
-                        <span>
-                        <Field
-                        name='selectAll'
-                        component="input"
-                        type="checkbox"
-                        normalize={value => value}
-                        data-test='selectAll' />
-                        </span>
-                        <span>Policy Number</span>
-                        <span>Company</span>
-                        <span>State</span>
-                        <span>Product</span>
-                        <span>Prpoerty Address</span>
-                        <span>Primary Policyholder</span>
-                        <span>Effective Date</span>
-                        <span>Terms</span>
+        <form id="TransferList" onSubmit={handleSubmit(toggleTransferModal)}>
+          <ul className="data-grid">
+            <li className="header">
+              <span className="checkbox">
+                <Field
+                  name='selectAll'
+                  component="input"
+                  type="checkbox"
+                  normalize={value => value}
+                  data-test='selectAll' />
+              </span>
+              <span className="policy-number">Policy Number</span>
+              <span className="company">Company</span>
+              <span className="state">State</span>
+              <span className="product">Product</span>
+              <span className="property-address">Prpoerty Address</span>
+              <span className="primary-policy">Primary Policyholder</span>
+              <span className="effective-date">Effective Date</span>
+              <span className="terms">Terms</span>
 
-                    </li>
-                {policies.map(p => {return (
-                    <li>
-                        <Field
-                        name={p.policyNumber}
-                        component="input"
-                        type="checkbox"
-                        normalize={value => value}
-                        data-test={p.policyNumber} />
-                        <span>{p.policyNumber}</span>
-                        <span>{p.companyCode}</span>
-                        <span>{p.state}</span>
-                        <span>{p.product}</span>
-                        <span>{p.propertyAddress}</span>
-                        <span>{p.policyHolder1}</span>
-                        <span>{p.effectiveDate}</span>
-                        <span></span>
-                    </li>
-                )})
-                }
-            </ul>
-            <button type='button'>Clear Selections</button>
-            <button type='submit'>Stage Selected For Transfer</button>
+            </li>
+            {policies.map(p => {
+              return (
+                <li className="data-row">
+                  <span className="checkbox">
+                    <Field
+                      name={p.policyNumber}
+                      component="input"
+                      type="checkbox"
+                      normalize={value => value}
+                      data-test={p.policyNumber} />
+                  </span>
+                  <span className="policy-number">{p.policyNumber}</span>
+                  <span className="company">{p.companyCode}</span>
+                  <span className="state">{p.state}</span>
+                  <span className="product">{p.product}</span>
+                  <span className="property-address">{p.propertyAddress}</span>
+                  <span className="primary-policy">{p.policyHolder1}</span>
+                  <span className="effective-date">{p.effectiveDate}</span>
+                  <span className="terms"></span>
+                </li>
+              )
+            })
+            }
+          </ul>
+          <button type='button' className="btn btn-link">Clear Selections</button>
+          <button type='submit' className="btn btn-link">Stage Selected For Transfer</button>
         </form>
       </Fragment>
     )
@@ -64,5 +68,5 @@ export class TransferList extends Component {
 }
 
 export default reduxForm({
-    form: 'TransferList',
-  })(TransferList);
+  form: 'TransferList',
+})(TransferList);
