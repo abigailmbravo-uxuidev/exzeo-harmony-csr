b17a76fec3a0812a2c40d774404acbe5b5b4bc40
Joe Sutphin <jsutphin@exzeo.com>
2019-02-19 15:01:44 -0500

    adjust html & css for grid filters

--- src/modules/Agency/Transfer/TransferFilter.jsx
+++ src/modules/Agency/Transfer/TransferFilter.jsx
@@ -14,7 +14,7 @@ export class TransferFilter extends Component {
   handleFilterChange = (value, previousValues, allValues) => {
     const { policyNumber } = allValues;
     console.log(allValues);
-   // if(policyNumber){
+    // if(policyNumber){
     // this.props.getPoliciesForAgency({ policyNumber });
     // }
     return value;
@@ -23,54 +23,54 @@ export class TransferFilter extends Component {
   render() {
     const { policyNumberList, listAnswersAsKey, agentsList } = this.props;
     return (
-        <form id={FORM_NAME} className="search-inputs">
-          <Field
-            name="policyNumber"
-            dataTest="policyNumber"
-            label="Filter By Policy Number"
-            placeholder="Start Typing"
-            component={Input}
-            styleName=""
-            answers={policyNumberList}
-            normalize={this.handleFilterChange}
-           />
-            <Field
-            name="state"
-            dataTest="state"
-            label="Filter By State"
-            placeholder="Start Typing"
-            component={MultiSelectTypeAhead}
-            styleName=""
-            answers={listAnswersAsKey.US_states}
-           />
-            <Field
-            name="product"
-            dataTest="product"
-            label="Filter By Product"
-            placeholder="Start Typing"
-            component={MultiSelectTypeAhead}
-            styleName=""
-            answers={listAnswersAsKey.Products}
-           />
-            <Field
-            name="agentCode"
-            dataTest="agent"
-            label="Filter By Agent"
-            placeholder="Start Typing"
-            component={MultiSelectTypeAhead}
-            styleName=""
-            answers={agentsList}
-           />
-          <Button
-            baseClass="secondary"
-            customClass="multi-input"
-            type="button"
-            dataTest="clear-fields">Clear Filters
+      <form id={FORM_NAME} className="search-inputs">
+        <Field
+          name="policyNumber"
+          dataTest="policyNumber"
+          label="Filter By Policy Number"
+          placeholder="Start Typing"
+          component={Input}
+          styleName=""
+          answers={policyNumberList}
+          normalize={this.handleFilterChange}
+        />
+        <Field
+          name="state"
+          dataTest="state"
+          label="Filter By State"
+          placeholder="Start Typing"
+          component={MultiSelectTypeAhead}
+          styleName=""
+          answers={listAnswersAsKey.US_states}
+        />
+        <Field
+          name="product"
+          dataTest="product"
+          label="Filter By Product"
+          placeholder="Start Typing"
+          component={MultiSelectTypeAhead}
+          styleName=""
+          answers={listAnswersAsKey.Products}
+        />
+        <Field
+          name="agentCode"
+          dataTest="agent"
+          label="Filter By Agent"
+          placeholder="Start Typing"
+          component={MultiSelectTypeAhead}
+          styleName=""
+          answers={agentsList}
+        />
+        <Button
+          baseClass="secondary"
+          customClass="multi-input btn-sm"
+          type="button"
+          dataTest="clear-fields">Clear Filters
           </Button>
-        </form>
+      </form>
     )
   }
-}  
+}
 
 export default reduxForm({
   form: FORM_NAME
