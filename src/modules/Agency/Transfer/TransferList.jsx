50573aedc05a095a953bf6cdcf8a6b3d1a2158d2
Abby Bravo <abravo@Helps-MacBook-Pro.local>
2019-02-22 14:43:14 -0500

    add fade-in class

--- src/modules/Agency/Transfer/TransferList.jsx
+++ src/modules/Agency/Transfer/TransferList.jsx
@@ -17,7 +17,7 @@ const filterPolicy = (policy, selectedArray) => {
 
 const TRANSFER_LIST_HEADER = {
   policyNumber: 'Policy Number',
-  companyCode: 'Company', 
+  companyCode: 'Company',
   state: 'State',
   product: 'Product',
   propertyAddress: 'Prpoerty Address',
@@ -30,42 +30,42 @@ export class TransferList extends Component {
   render() {
     const { policies, filteredPolicies, toggleTransferModal, checkPolicy, uncheckPolicy, checkAllPolicies, selectedPolicies, clearSelectedPolicies } = this.props;
     return (
-        <div id="TransferList">
-          <ul className="data-grid">
-            <TransferListItem 
-              listClassName="header"
-              policy={TRANSFER_LIST_HEADER}
-              clickHandler={checkAllPolicies}
-              isChecked={policies.length === selectedPolicies.length} 
-             />
-            {selectedPolicies.map(p => {
-              return (
-                <TransferListItem 
-                listClassName="data-row"
+      <div id="TransferList">
+        <ul className="data-grid">
+          <TransferListItem
+            listClassName="header"
+            policy={TRANSFER_LIST_HEADER}
+            clickHandler={checkAllPolicies}
+            isChecked={policies.length === selectedPolicies.length}
+          />
+          {selectedPolicies.map(p => {
+            return (
+              <TransferListItem
+                listClassName="data-row fade-in"
                 policy={p}
                 clickHandler={uncheckPolicy(p.policyNumber)}
-                isChecked={true} 
-               />
-              )
-            })
-            }
-            {filteredPolicies.map(p => {
-              return (
-                <TransferListItem 
+                isChecked={true}
+              />
+            )
+          })
+          }
+          {filteredPolicies.map(p => {
+            return (
+              <TransferListItem
                 listClassName="data-row"
                 policy={p}
                 clickHandler={checkPolicy(p.policyNumber)}
-                isChecked={false} 
-               />
-              )
-            })
-            }
-          </ul>
-          <div className="button-wrapper">
-            <button type='button' onClick={clearSelectedPolicies} className="btn btn-link"><i className="fa fa-rotate-left" />Clear Selections</button>
-            <button type='button' disabled={selectedPolicies.length === 0} onClick={toggleTransferModal} className="btn btn-link"><i className="fa fa-random" />Stage Selected For Transfer</button>
-          </div>
+                isChecked={false}
+              />
+            )
+          })
+          }
+        </ul>
+        <div className="button-wrapper">
+          <button type='button' onClick={clearSelectedPolicies} className="btn btn-link"><i className="fa fa-rotate-left" />Clear Selections</button>
+          <button type='button' disabled={selectedPolicies.length === 0} onClick={toggleTransferModal} className="btn btn-link"><i className="fa fa-random" />Stage Selected For Transfer</button>
         </div>
+      </div>
     )
   }
 }
