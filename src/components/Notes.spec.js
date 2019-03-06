18d5802b8ab14ff61c1b650fc002b55b5d638fd8
elamison <eric@lamison.com>
2019-03-06 14:03:36 -0500

    Fixed test warnings.

--- src/components/Notes.spec.js
+++ src/components/Notes.spec.js
@@ -5,12 +5,22 @@ import { Notes } from './Notes';
 
 describe('Test the Notes Component', () => {
   it('Should Render Notes', () => {
-    const wrapper = shallow(<Notes numbers={['test']} numberType="policyNumber" />);
+    const props = { 
+      numbers: ['test']},
+      numberType: 'policyNumber',
+      setAppError: jest.fn() 
+    };
+    const wrapper = shallow(<Notes {...props} />);
     expect(wrapper.exists()).toBeTruthy();
   });
 
   it('Should Render NoteList', () => {
-    const wrapper = shallow(<Notes numbers={['test']} numberType="policyNumber" />);
+    const props = { 
+      numbers: ['test']},
+      numberType: 'policyNumber',
+      setAppError: jest.fn() 
+    };
+    const wrapper = shallow(<Notes {...props} />);
     expect(wrapper.find('NoteList')).toHaveLength(1);
   });
 });
