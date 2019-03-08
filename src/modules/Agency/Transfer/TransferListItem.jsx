4d5f5d51daa5db01b2fe2532ed66536bf905b80c
Joe Sutphin <jsutphin@exzeo.com>
2019-03-08 11:14:59 -0500

    update z-index & checkbox icon

--- src/modules/Agency/Transfer/TransferListItem.jsx
+++ src/modules/Agency/Transfer/TransferListItem.jsx
@@ -3,17 +3,17 @@ import React from 'react'
 const TransferListItem = ({ policy, clickHandler, isChecked, listClassName }) => {
   return (
     <li className={listClassName} onClick={clickHandler}>
-        <span className="checkbox" >
-        {isChecked ? <span className="fa fa-check-square" /> : <span className="fa fa-square" /> }    
-        </span>
-        <span className="policy-number">{policy.policyNumber}</span>
-        <span className="company">{policy.companyCode}</span>
-        <span className="state">{policy.state}</span>
-        <span className="product">{policy.product}</span>
-        <span className="property-address">{policy.propertyAddress}</span>
-        <span className="primary-policy">{policy.policyHolder1}</span>
-        <span className="effective-date">{policy.effectiveDate}</span>
-        <span className="terms">{policy.terms}</span>
+      <span className="checkbox" >
+        {isChecked ? <span className="fa fa-check-square" /> : <span className="fa fa-square-o" />}
+      </span>
+      <span className="policy-number">{policy.policyNumber}</span>
+      <span className="company">{policy.companyCode}</span>
+      <span className="state">{policy.state}</span>
+      <span className="product">{policy.product}</span>
+      <span className="property-address">{policy.propertyAddress}</span>
+      <span className="primary-policy">{policy.policyHolder1}</span>
+      <span className="effective-date">{policy.effectiveDate}</span>
+      <span className="terms">{policy.terms}</span>
     </li>
   )
 };
