c8581bd067cd1de6095b66c406e5735319bc2665
Mark Eads <meads@hcpci.com>
2019-03-21 14:20:12 -0400

    updated tests

--- src/components/ContactAddress.spec.js
+++ src/components/ContactAddress.spec.js
@@ -1,18 +1,45 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import ContactAddress from './ContactAddress';
 
 
 describe('Test the ContactAddress Component', () => {
-  it('Should Render', () => {
+  it('Should Render with All Address Fields', () => {
+    const mailingAddress = {
+      address1: '123 Some Way',
+      address2: 'APT # 123',
+      city: 'Tampa',
+      state: 'FL',
+      zip: '33600'
+    };
+    const wrapper = mount(<ContactAddress mailingAddress={mailingAddress} />);
+    expect(wrapper.exists()).toBeTruthy();
+    expect(wrapper.find('.contact-address').text()).toMatch("123 Some Way, APT # 123, Tampa, FL 33600");
+  });
+
+
+  it('Should Render with All Address Fields', () => {
+    const mailingAddress = {
+      address1: '123 Some Way',
+      address2: 'APT # 123',
+      city: 'Tampa',
+      state: 'FL',
+      zip: '33600'
+    };
+    const wrapper = mount(<ContactAddress mailingAddress={mailingAddress} />);
+    expect(wrapper.exists()).toBeTruthy();
+    expect(wrapper.find('.contact-address').text()).toMatch("123 Some Way, APT # 123, Tampa, FL 33600");
+  });
+
+  it('Should Render with Missing Address 2 Field', () => {
     const mailingAddress = {
       address1: '123 Some Way',
       city: 'Tampa',
       state: 'FL',
       zip: '33600'
     };
-    const wrapper = shallow(<ContactAddress mailingAddress={mailingAddress} />);
-
+    const wrapper = mount(<ContactAddress mailingAddress={mailingAddress} />);
     expect(wrapper.exists()).toBeTruthy();
+    expect(wrapper.find('.contact-address').text()).toMatch("123 Some Way, Tampa, FL 33600");
   });
 });
