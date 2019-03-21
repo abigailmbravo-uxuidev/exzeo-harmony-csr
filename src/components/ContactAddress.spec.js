dda5963e62b32a6201861d6e4146af2728cfab6b
Mark Eads <meads@hcpci.com>
2019-03-21 14:21:08 -0400

    removed dup test

--- src/components/ContactAddress.spec.js
+++ src/components/ContactAddress.spec.js
@@ -4,20 +4,7 @@ import ContactAddress from './ContactAddress';
 
 
 describe('Test the ContactAddress Component', () => {
-  it('Should Render with All Address Fields', () => {
-    const mailingAddress = {
-      address1: '123 Some Way',
-      address2: 'APT # 123',
-      city: 'Tampa',
-      state: 'FL',
-      zip: '33600'
-    };
-    const wrapper = mount(<ContactAddress mailingAddress={mailingAddress} />);
-    expect(wrapper.exists()).toBeTruthy();
-    expect(wrapper.find('.contact-address').text()).toMatch("123 Some Way, APT # 123, Tampa, FL 33600");
-  });
-
-
+  
   it('Should Render with All Address Fields', () => {
     const mailingAddress = {
       address1: '123 Some Way',
