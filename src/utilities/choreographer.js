82e8c24d76c9caa335a2eaa907f80b0c03504f8d
Jordan Rhodes <jsrhodes15@gmail.com>
2019-05-17 15:49:48 -0400

    Remove unused files

--- src/utilities/choreographer.js
+++ src/utilities/choreographer.js
@@ -1,6 +1,16 @@
 import { http } from '@exzeo/core-ui/src/Utilities';
 
-import handleError from './handleError';
+function handleError(err) {
+  // return default error message if non exists
+  if (!err) return { message: 'An error occurred that was not handled properly.' };
+
+  let error = err.response && err.response.data ? err.response.data : err;
+  // if error is a string, convert to an object
+  if (typeof error === 'string') error = { message: error };
+  // format error if needed
+  if (!error.message) error.message = 'There was an error.';
+  return error;
+}
 
 export const startWorkflow = async (modelName, data) => {
   const axiosConfig = {
