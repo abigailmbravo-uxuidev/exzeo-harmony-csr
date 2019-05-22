03e81e24b4887231aca9874c3b0664bdabcff251
elamison <eric@lamison.com>
2019-05-22 18:25:22 -0400

    Fixed ShortenText component.

--- src/components/ShortenText.jsx
+++ src/components/ShortenText.jsx
@@ -3,10 +3,9 @@ import React from 'react';
 const ShortenText = ({ text, limit = 100 }) => {
   if (!text || typeof text !== 'string') return '';
   
-  const re = new RegExp(`^(.{${limit}}[^\\s]*).*`);
   return text.length <= limit 
     ? <span>{text}</span> 
-    : <span title={text}>{text.replace(re, '$1')}&hellip;</span>;
+    : <span title={text}>{text.substring(0, (text + ' ').lastIndexOf(' ', limit))}&hellip;</span>;
 };
 
 export default ShortenText;
