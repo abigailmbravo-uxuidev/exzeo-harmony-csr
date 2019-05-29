1d2c0bf6869b09e627ac4155e2e76a4a948715c4
Joe Sutphin <jsutphin@exzeo.com>
2019-05-29 09:41:20 -0400

    update footer structure

--- src/modules/Quote/QuoteFooter.jsx
+++ src/modules/Quote/QuoteFooter.jsx
@@ -5,36 +5,40 @@ import { Share } from '@exzeo/core-ui/src/@Harmony';
 
 function QuoteFooter({ submitting, isPrimaryDisabled, handlePrimaryClick, handleResetForm, currentStep }) {
   return (
-    <div className="basic-footer btn-footer">
+    <React.Fragment>
       {(currentStep === 'notes' || currentStep === 'additionalInterests')
-        ? <Footer/>
+        ? <div className="basic-footer">
+          <Footer/>
+        </div>
         : (currentStep === 'summary')
-          ? <React.Fragment>
-              <Share handleShare={handlePrimaryClick} disabled={isPrimaryDisabled}/>
-              <Footer/>
-            </React.Fragment>
-          : <React.Fragment>
-              <Footer/>
-              <div className="btn-wrapper">
-                {currentStep !== 'application' &&
-                <Button
-                  onClick={handleResetForm}
-                  data-test="reset"
-                  className={Button.constants.classNames.secondary}
-                  label="Reset"
-                />
-                }
-                <Button
-                  data-test="submit"
-                  className={Button.constants.classNames.primary}
-                  onClick={handlePrimaryClick}
-                  disabled={isPrimaryDisabled}
-                  label={currentStep !== 'application' ? 'Update' : 'Send To Docusign'}
-                />
-              </div>
-          </React.Fragment>
+        ? <React.Fragment>
+          <Share handleShare={handlePrimaryClick} disabled={isPrimaryDisabled}/>
+          <div className="basic-footer">
+            <Footer/>
+          </div>
+        </React.Fragment>
+        : <div className="basic-footer btn-footer">
+          <Footer/>
+          <div className="btn-wrapper">
+            {currentStep !== 'application' &&
+            <Button
+              onClick={handleResetForm}
+              data-test="reset"
+              className={Button.constants.classNames.secondary}
+              label="Reset"
+            />
+            }
+            <Button
+              data-test="submit"
+              className={Button.constants.classNames.primary}
+              onClick={handlePrimaryClick}
+              disabled={isPrimaryDisabled}
+              label={currentStep !== 'application' ? 'Update' : 'Send To Docusign'}
+            />
+          </div>
+        </div>
       }
-    </div>
+      </React.Fragment>
   );
 }
 
