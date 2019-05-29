2a7312628790b0536b27c7546e258eaaba0f02e1
Mark Eads <meads@hcpci.com>
2019-05-29 10:52:41 -0400

    reshape notes files

--- src/modules/NotesFiles/@components/NotesFiles.jsx
+++ src/modules/NotesFiles/@components/NotesFiles.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { emptyObject } from '@exzeo/core-ui/src';
+import { shape } from 'prop-types'
 
 import Notes from './Notes';
 import DiaryTable from './DiaryTable';
@@ -12,32 +12,26 @@ function NotesFiles ({ options, customHandlers, initialValues }) {
   useEffect(() => {
     customHandlers.fetchNotes([initialValues.quoteNumber], 'quoteNumber')
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-  return (
-    <div className="scroll">
-    <div className="form-group survey-wrapper" role="group">
-      <h3>History</h3>
-      <section>
-        <div className="notes-list">
-          <div className="note-grid-wrapper btn-tabs">
-            <div className="filter-tabs">
-              <button type="button" className={`btn btn-tab ${historyTab === NOTE_TYPE.notes ? 'selected' : ''}`} onClick={() => setHistoryTab(NOTE_TYPE.notes)}>Notes</button>
-              <button type="button" className={`btn btn-tab ${historyTab === NOTE_TYPE.files ? 'selected' : ''}`} onClick={() => setHistoryTab(NOTE_TYPE.files)}>Files</button>
-              <button type="button" className={`btn btn-tab ${historyTab === NOTE_TYPE.diaries ? 'selected' : ''}`} onClick={() => setHistoryTab(NOTE_TYPE.diaries)}>Diaries</button>
-            </div>
-            {NOTE_TABS.includes(historyTab) && <Notes notes={notes} customHandlers={customHandlers} attachmentStatus={historyTab === NOTE_TYPE.files} />}
-            {DIARY_TAB === historyTab && <DiaryTable customHandlers={customHandlers} diaries={diaries} entityEndDate={initialValues.endDate} />}
-          </div>
-        </div>
-      </section>
-    </div>
-  </div>
-  );
-};
+  }, []);
 
-NotesFiles.defaultProps = {
-  customHandlers: emptyObject,
-  options: emptyObject
+  return (
+    <div className="notes-list">
+      <div className="note-grid-wrapper btn-tabs">
+        <div className="filter-tabs">
+          <button type="button" className={`btn btn-tab ${historyTab === NOTE_TYPE.notes ? 'selected' : ''}`} onClick={() => setHistoryTab(NOTE_TYPE.notes)}>Notes</button>
+          <button type="button" className={`btn btn-tab ${historyTab === NOTE_TYPE.files ? 'selected' : ''}`} onClick={() => setHistoryTab(NOTE_TYPE.files)}>Files</button>
+          <button type="button" className={`btn btn-tab ${historyTab === NOTE_TYPE.diaries ? 'selected' : ''}`} onClick={() => setHistoryTab(NOTE_TYPE.diaries)}>Diaries</button>
+        </div>
+        {NOTE_TABS.includes(historyTab) && <Notes notes={notes} customHandlers={customHandlers} attachmentStatus={historyTab === NOTE_TYPE.files} />}
+        {DIARY_TAB === historyTab && <DiaryTable customHandlers={customHandlers} diaries={diaries} entityEndDate={initialValues.endDate} />}
+      </div>
+    </div>
+  );
+}
+
+NotesFiles.propTypes = {
+  customHandlers: shape({}).isRequired,
+  options: shape({}).isRequired
 };
 
 export default NotesFiles;
