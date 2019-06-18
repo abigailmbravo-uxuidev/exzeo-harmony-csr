349ba23b02200adc159d91309453de095ce7aaaf
Colin Fendrick <colin.fendrick@gmail.com>
2019-06-18 10:25:18 -0400

    Add quoteselector test

---
+++ src/state/selectors/quote.selectors.spec.js
@@ -0,0 +1,45 @@
+import { getQuoteSelector } from './quote.selectors';
+import { quote } from '../../test-utils';
+
+describe('Test Quote Selectors', () => {
+  describe('getQuoteSelector', () => {
+    it ('Should return empty with no quoteData', () => {
+      const state = {
+        quoteState: {}
+      };
+      const result = getQuoteSelector(state);
+      expect(result).toEqual({});
+    });
+
+    it('Should return empty with no quoteNumber', () => {
+      const state = { quoteState: { quote: { quoteNumber: undefined }}};
+      const result = getQuoteSelector(state);
+      expect(result).toEqual({});
+    });
+
+    it('Returns ymodified and new data correctl', () => {
+      const state= {
+        quoteState: {
+          quote: {
+            ...quote,
+            quoteState: 'Quote Declined',
+            effectiveDate: '2001-01-10T14:30Z',
+            underwritingExceptions: [{ overridden: false, action: 'Missing Info' }]
+          }
+        }
+      };
+      const {
+        editingDisabled,
+        effectiveDate,
+        removeSecondary,
+        hasActiveExceptions,
+        hasUWError
+      } = getQuoteSelector(state);
+      expect(editingDisabled).toEqual(true);
+      expect(effectiveDate).toEqual('2001-01-10');
+      expect(removeSecondary).toEqual(false);
+      expect(hasActiveExceptions).toEqual(true);
+      expect(hasUWError).toEqual(false);
+    });
+  });
+});
