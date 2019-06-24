8776da062462beaa2347295b1f8249c7ff1a055f
Colin Fendrick <colin.fendrick@gmail.com>
2019-06-24 08:30:34 -0400

    Add bad uw data test

--- cypress/integration/underwritingExceptions/skipToUW.spec.js
+++ cypress/integration/underwritingExceptions/skipToUW.spec.js
@@ -4,6 +4,7 @@ import {
   fillOutCoverage,
   fillOutUnderwriting
 } from '../../helpers';
+import { underwriting } from '../../fixtures';
 
 describe('Skip to UW Testing', () => {
   before('Login and get a quote', () => {
@@ -13,7 +14,28 @@ describe('Skip to UW Testing', () => {
   });
 
   it('Can fill out UW before Coverage', () => {
+    // Fills out UW with good data
     fillOutUnderwriting();
-    fillOutCoverage();
+    fillOutCoverage()
+      .then(({ response: { body: { result }}}) => expect(result.quoteState).to.equal('Quote Qualified'));
+  });
+});
+
+describe('Giving Bad UW Data First', () => {
+  before('Login and get a quote', () => {
+    cy.login();
+    setRouteAliases();
+    navigateThroughNewQuote();
+  });
+
+  it('Quote does not update to Quote Stopped after Bad UW and blank coverage', () => {
+    // Fill out UW first with bad data
+    fillOutUnderwriting({
+      ...underwriting,
+      'underwritingAnswers.previousClaims.answer': '3-5 Years'
+    })
+    // Confirm that the quote goes to quote stopped after coverage is filled out
+    fillOutCoverage()
+      .then(({ response: { body: { result } } }) => expect(result.quoteState).to.equal('Quote Stopped'));
   });
 });
