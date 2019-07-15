f9590ebd105b7f0b4137c4d25e577e17eb0de1ce
Mark Eads <meads@hcpci.com>
2019-07-15 13:07:41 -0400

    added policy left and center coverage sections

--- src/mock-data/mockPolicyHO3.js
+++ src/mock-data/mockPolicyHO3.js
@@ -26,11 +26,12 @@ const mock = {
       step: {},
       components: [
         {
-          name: 'Coverage',
-          id: 1001,
+          id: 1,
           type: '$SECTION',
           dependencies: [],
-          data: {},
+          data: {
+            className: 'coverage'
+          },
           formData: {},
           children: [
             {
@@ -38,10 +39,205 @@ const mock = {
               type: '$TITLE',
               dependencies: [],
               data: {
-                text: 'Policy Stuff'
+                text: 'Coverage and Premium'
               },
               formData: {},
               children: []
+            },
+            {
+              id: 3,
+              type: '$SECTION',
+              dependencies: [],
+              data: {
+                className: 'coverage-left'
+              },
+              formData: {},
+              children: [
+                {
+                  id: 4,
+                  type: '$ENTITY_DETAILS',
+                  dependencies: [],
+                  data: {
+                    extendedProperties: {
+                      className: 'coverage',
+                      details: [
+                        {
+                          label: 'Dwelling Limit',
+                          items: [
+                            {
+                              format: 'currency',
+                              path: 'coverageLimits.dwelling.amount'
+                            }
+                          ]
+                        },
+                        {
+                          label: 'Other Structures Limit',
+                          items: [
+                            {
+                              format: 'currency',
+                              path: 'coverageLimits.otherStructures.amount'
+                            }
+                          ]
+                        },
+                        {
+                          label: 'Personal Property Limit',
+                          items: [
+                            {
+                              format: 'currency',
+                              path: 'coverageLimits.personalProperty.amount'
+                            }
+                          ]
+                        },
+                        {
+                          label: 'Loss of Use Limit',
+                          items: [
+                            {
+                              format: 'currency',
+                              path: 'coverageLimits.lossOfUse.amount'
+                            }
+                          ]
+                        },
+                        {
+                          label: 'Personal Liability Limit',
+                          items: [
+                            {
+                              format: 'currency',
+                              path: 'coverageLimits.personalLiability.amount'
+                            }
+                          ]
+                        },
+                        {
+                          label: 'Medical Payments to Others Limit',
+                          items: [
+                            {
+                              format: 'currency',
+                              path: 'coverageLimits.medicalPayments.amount'
+                            }
+                          ]
+                        }
+                      ]
+                    }
+                  },
+                  formData: {},
+                  children: []
+                }
+              ]
+            },
+            {
+              id: 5,
+              type: '$SECTION',
+              dependencies: [],
+              data: {
+                className: 'coverage-center'
+              },
+              formData: {},
+              children: [
+                {
+                  id: 6,
+                  type: '$ENTITY_DETAILS',
+                  dependencies: [],
+                  data: {
+                    extendedProperties: {
+                      className: 'coverage',
+                      details: [
+                        {
+                          label: 'Mold Property Limit',
+                          items: [
+                            {
+                              format: 'currency',
+                              path: 'coverageLimits.moldProperty.amount'
+                            }
+                          ]
+                        },
+                        {
+                          label: 'Mold Liability Limit',
+                          items: [
+                            {
+                              format: 'currency',
+                              path: 'coverageLimits.moldLiability.amount'
+                            }
+                          ]
+                        },
+                        {
+                          label: 'Personal Property Repl Cost',
+                          items: [
+                            {
+                              format: 'bool',
+                              path:
+                                'coverageOptions.personalPropertyReplacementCost.answer'
+                            }
+                          ]
+                        },
+                        {
+                          label: 'Ordinance or Law Coverage Limit',
+                          items: [
+                            {
+                              format: 'percent',
+                              path: 'coverageLimits.ordinanceOrLaw.amount'
+                            }
+                          ]
+                        },
+                        {
+                          label: 'Incidental Occ Main',
+                          items: [
+                            {
+                              format: 'bool',
+                              path:
+                                'coverageOptions.propertyIncidentalOccupanciesMainDwelling.answer'
+                            }
+                          ]
+                        },
+                        {
+                          label: 'Incidental Occ Other',
+                          items: [
+                            {
+                              format: 'bool',
+                              path:
+                                'coverageOptions.propertyIncidentalOccupanciesOtherStructures.answer'
+                            }
+                          ]
+                        },
+                        {
+                          label: 'Incidental Occ Liability',
+                          items: [
+                            {
+                              format: 'bool',
+                              path:
+                                'coverageOptions.liabilityIncidentalOccupancies.answer'
+                            }
+                          ]
+                        }
+                      ]
+                    }
+                  },
+                  formData: {},
+                  children: []
+                }
+              ]
+            },
+            {
+              id: 7,
+              type: '$SECTION',
+              dependencies: [],
+              data: {
+                className: 'coverage-right'
+              },
+              formData: {},
+              children: [
+                {
+                  id: 8,
+                  type: '$ENTITY_DETAILS',
+                  dependencies: [],
+                  data: {
+                    extendedProperties: {
+                      className: 'coverage',
+                      details: []
+                    }
+                  },
+                  formData: {},
+                  children: []
+                }
+              ]
             }
           ]
         }
