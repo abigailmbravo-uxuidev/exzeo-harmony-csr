e64d09ed89d5b628cee12880eac81be07a2f6472
Mark Eads <meads@hcpci.com>
2019-07-17 16:27:30 -0400

    add hook for agencyAgents

--- src/modules/Policy/PolicyholderAgent.jsx
+++ src/modules/Policy/PolicyholderAgent.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { defaultMemoize } from 'reselect';
 import _find from 'lodash/find';
 import _get from 'lodash/get';
+import { useFetchAgents, useFetchAgency } from './hooks';
 
 const PolicyholderAgent = ({ initialValues, options }) => {
+  const { agents } = useFetchAgents(initialValues.agencyCode);
+  const { agency } = useFetchAgency(initialValues.agencyCode);
+  console.log(agency, agents);
   return <React.Fragment>policyHolder</React.Fragment>;
 };
 
