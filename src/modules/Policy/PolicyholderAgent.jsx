c0d3aa9a6d4d0a65cf6ab7c689990f54e210a5f9
Mark Eads <meads@hcpci.com>
2019-07-18 09:35:00 -0400

    add policyholder card component

--- src/modules/Policy/PolicyholderAgent.jsx
+++ src/modules/Policy/PolicyholderAgent.jsx
@@ -6,6 +6,7 @@ import { useFetchAgents, useFetchAgency } from './hooks';
 
 import AgencyCard from './AgencyCard';
 import AgentCard from './AgentCard';
+import PolicyholderCard from './PolicyholderCard';
 
 const PolicyholderAgent = ({ initialValues, options }) => {
   const { agents } = useFetchAgents(initialValues.agencyCode);
@@ -13,10 +14,25 @@ const PolicyholderAgent = ({ initialValues, options }) => {
   const selectedAgent = agents.find(
     a => a.agentCode === initialValues.agentCode
   );
+  const {
+    policyHolders,
+    policyHolderMailingAddress,
+    policyNumber
+  } = initialValues;
   return (
     <React.Fragment>
       <section className="policyholder-cards">
         <h3>Policyholder</h3>
+        {policyHolders &&
+          policyHolders.map((policyHolder, index) => (
+            <PolicyholderCard
+              policyHolder={policyHolder}
+              index={index}
+              label={`Policyholder ${index + 1}`}
+              policyHolderMailingAddress={policyHolderMailingAddress}
+              subject={`${policyNumber}%20${policyHolders[0].firstName}%20${policyHolders[0].lastName}`}
+            />
+          ))}
       </section>
       <section className="agency-cards">
         <h3>
