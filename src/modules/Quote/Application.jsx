c6233ab79a3b6ed023376f59f2fc1ab8be60a7bf
Mark Eads <meads@hcpci.com>
2019-08-01 13:56:14 -0400

    update override logic

--- src/modules/Quote/Application.jsx
+++ src/modules/Quote/Application.jsx
@@ -6,8 +6,9 @@ const Application = ({ initialValues, customHandlers }) => {
   return (
     <div className="detail-wrapper">
       {Array.isArray(initialValues.underwritingExceptions) &&
-        initialValues.underwritingExceptions.filter(uw => !uw.overridden)
-          .length > 0 && (
+        initialValues.underwritingExceptions.filter(
+          uw => uw.canOverride && !uw.overridden
+        ).length > 0 && (
           <div className="messages">
             <div className="message error">
               <i className="fa fa-exclamation-circle" aria-hidden="true" />
