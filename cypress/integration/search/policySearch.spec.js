e8cddc1bf1973e9ab89be3b14723a53f1030b63c
Mark Eads <meads@hcpci.com>
2019-08-01 15:26:18 -0400

    updated policy search test

--- cypress/integration/search/policySearch.spec.js
+++ cypress/integration/search/policySearch.spec.js
@@ -63,7 +63,7 @@ describe('Policy Search testing', () => {
   // TODO this test relies on some Batman Robin policies, which is not ideal. Once we have sufficient Policy testing, this test should be run on one of the Policies resulting from tests.
   it('Policy Search Sorting', () => {
     toggleAdvancedSearch();
-    cy.clearAllText(fields).fillFields([{ name: 'firstName', data: 'batman' }])
+    cy.clearAllText(fields).fillFields([{ name: 'firstName', data: 'Batman' }])
       .findDataTag('sortBy').select('firstName', { force: true }).clickSubmit('#SearchBar').wait('@fetchPolicies')
       .then(({ response }) => expect(response.body.policies[0].policyHolders[0].firstName.charAt(0)).to.match(/b/i));
   });
