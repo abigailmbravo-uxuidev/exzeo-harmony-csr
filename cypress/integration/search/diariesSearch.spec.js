e55319a98e37d56865c4b959b40978c254ea26b0
Jordan Rhodes <jsrhodes15@gmail.com>
2019-08-13 13:59:20 -0400

    Wrap Cypress tests in check for env.CI to stop from running in CI environment until ready

--- cypress/integration/search/diariesSearch.spec.js
+++ cypress/integration/search/diariesSearch.spec.js
@@ -1,46 +1,53 @@
 import { setRouteAliases } from '../../helpers';
 
 describe('Diaries Search Testing', () => {
-  const selectDiariesSearch = () => cy.findDataTag('diaries-link').click();
+  // TODO unwrap this test suite to run in CI when ready
+  if (Cypress.env('CI')) {
+    it('Skip test due to environment', () => {
+      cy.task('log', 'CI === true: not running search/diaries specs');
+    });
+  } else {
+    const selectDiariesSearch = () => cy.findDataTag('diaries-link').click();
 
-  before('Login', () => cy.login());
+    before('Login', () => cy.login());
 
-  beforeEach('Set aliases and go to diaries search', () => {
-    setRouteAliases();
-    selectDiariesSearch();
-  });
+    beforeEach('Set aliases and go to diaries search', () => {
+      setRouteAliases();
+      selectDiariesSearch();
+    });
 
-  it('POS:Diaries Search', () =>
-    cy
-      .clickSubmit()
-      .wait('@fetchDiaries')
-      .get('.diary-list')
-      .children()
-      .should('exist')
+    it('POS:Diaries Search', () =>
+      cy
+        .clickSubmit()
+        .wait('@fetchDiaries')
+        .get('.diary-list')
+        .children()
+        .should('exist')
 
-      .findDataTag('status')
-      .select('false')
-      .clickSubmit()
-      .wait('@fetchDiaries')
-      .get('.diary-list')
-      .children()
-      .should('exist')
+        .findDataTag('status')
+        .select('false')
+        .clickSubmit()
+        .wait('@fetchDiaries')
+        .get('.diary-list')
+        .children()
+        .should('exist')
 
-      .findDataTag('reason')
-      .select('information_needed')
-      .clickSubmit()
-      .wait('@fetchDiaries')
-      .get('.diary-list')
-      .children()
-      .should('exist')
+        .findDataTag('reason')
+        .select('information_needed')
+        .clickSubmit()
+        .wait('@fetchDiaries')
+        .get('.diary-list')
+        .children()
+        .should('exist')
 
-      .findDataTag('date-range-min')
-      .type('2010-01-01')
-      .findDataTag('date-range-max')
-      .type('2020-01-01')
-      .clickSubmit()
-      .wait('@fetchDiaries')
-      .get('.diary-list')
-      .children()
-      .should('exist'));
+        .findDataTag('date-range-min')
+        .type('2010-01-01')
+        .findDataTag('date-range-max')
+        .type('2020-01-01')
+        .clickSubmit()
+        .wait('@fetchDiaries')
+        .get('.diary-list')
+        .children()
+        .should('exist'));
+  }
 });
--- cypress/integration/search/quoteSearch.spec.js
+++ cypress/integration/search/quoteSearch.spec.js
@@ -1,84 +1,91 @@
 import { setRouteAliases, confirmPolicyOrQuote } from '../../helpers';
 
+const fields = [
+  { name: 'firstName', data: 'batman' },
+  { name: 'lastName', data: 'robin' },
+  { name: 'address', data: 'test' }
+];
+
 describe('Quote Search testing', () => {
-  const fields = [
-    { name: 'firstName', data: 'batman' },
-    { name: 'lastName', data: 'robin' },
-    { name: 'address', data: 'test' }
-  ];
+  // TODO unwrap this test suite to run in CI when ready
+  if (Cypress.env('CI')) {
+    it('Skip test due to environment', () => {
+      cy.task('log', 'CI === true: not running search/quote specs');
+    });
+  } else {
+    const selectQuoteSearch = () =>
+      cy.findDataTag('searchType').select('quote');
+    before('Login', () => cy.login());
 
-  const selectQuoteSearch = () => cy.findDataTag('searchType').select('quote');
+    beforeEach('Set aliases and go to quote search', () => {
+      setRouteAliases();
+      selectQuoteSearch();
+    });
 
-  before('Login', () => cy.login());
+    it('POS:Quote Search', () =>
+      cy
+        .clickSubmit()
+        .wait('@fetchQuotes')
+        .get('.quote-list')
+        .children()
+        .should('exist')
 
-  beforeEach('Set aliases and go to quote search', () => {
-    setRouteAliases();
-    selectQuoteSearch();
-  });
+        .findDataTag('firstName')
+        .type('{selectall}{backspace}batman', { force: true })
+        .clickSubmit()
+        .wait('@fetchQuotes')
+        .get('.quote-list')
+        .children()
+        .should('exist')
 
-  it('POS:Quote Search', () =>
-    cy
-      .clickSubmit()
-      .wait('@fetchQuotes')
-      .get('.quote-list')
-      .children()
-      .should('exist')
+        .findDataTag('lastName')
+        .type('{selectall}{backspace}robin', { force: true })
+        .clickSubmit()
+        .wait('@fetchQuotes')
+        .get('.quote-list')
+        .children()
+        .should('exist')
 
-      .findDataTag('firstName')
-      .type('{selectall}{backspace}batman', { force: true })
-      .clickSubmit()
-      .wait('@fetchQuotes')
-      .get('.quote-list')
-      .children()
-      .should('exist')
+        .findDataTag('address')
+        .type('{selectall}{backspace}2113', { force: true })
+        .clickSubmit()
+        .wait('@fetchQuotes')
+        .get('.quote-list')
+        .children()
+        .should('exist')
 
-      .findDataTag('lastName')
-      .type('{selectall}{backspace}robin', { force: true })
-      .clickSubmit()
-      .wait('@fetchQuotes')
-      .get('.quote-list')
-      .children()
-      .should('exist')
+        .findDataTag('quoteNumber')
+        .type('{selectall}{backspace}12-5168124-01', { force: true })
+        .clickSubmit()
+        .wait('@fetchQuotes')
+        .get('.quote-list')
+        .children()
+        .should('exist')
 
-      .findDataTag('address')
-      .type('{selectall}{backspace}2113', { force: true })
-      .clickSubmit()
-      .wait('@fetchQuotes')
-      .get('.quote-list')
-      .children()
-      .should('exist')
+        .findDataTag('quoteState')
+        .select('Quote Qualified')
+        .clickSubmit()
+        .wait('@fetchQuotes')
+        .get('.quote-list')
+        .children()
+        .should('exist'));
 
-      .findDataTag('quoteNumber')
-      .type('{selectall}{backspace}12-5168124-01', { force: true })
-      .clickSubmit()
-      .wait('@fetchQuotes')
-      .get('.quote-list')
-      .children()
-      .should('exist')
-
-      .findDataTag('quoteState')
-      .select('Quote Qualified')
-      .clickSubmit()
-      .wait('@fetchQuotes')
-      .get('.quote-list')
-      .children()
-      .should('exist'));
-
-  it('POS:Quote 3-field retrieve search', () =>
-    cy
-      .fillFields(fields)
-      .clickSubmit('#SearchBar')
-      .wait('@fetchQuotes')
-      .then(({ response }) =>
-        confirmPolicyOrQuote(response.body.result.quotes, fields)
-      )
-      // Click the pagination button.
-      .findDataTag('page-forward')
-      .click({ force: true })
-      .wait('@fetchQuotes')
-      .then(({ response }) =>
-        confirmPolicyOrQuote(response.body.result.quotes, fields)
-      )
-      .get('input[name="pageNumber"]')
-      .should('have.value', '2'));
+    it('POS:Quote 3-field retrieve search', () =>
+      cy
+        .fillFields(fields)
+        .clickSubmit('#SearchBar')
+        .wait('@fetchQuotes')
+        .then(({ response }) =>
+          confirmPolicyOrQuote(response.body.result.quotes, fields)
+        )
+        // Click the pagination button.
+        .findDataTag('page-forward')
+        .click({ force: true })
+        .wait('@fetchQuotes')
+        .then(({ response }) =>
+          confirmPolicyOrQuote(response.body.result.quotes, fields)
+        )
+        .get('input[name="pageNumber"]')
+        .should('have.value', '2'));
+  }
 });
