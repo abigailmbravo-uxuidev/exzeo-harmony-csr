9393c1e37a49d92fb46902b8549e62d2784399fb
elamison <elamison@exzeo>
2019-08-22 13:24:06 -0400

    Add Payment form. Add PolicyCard for BulkPayments.

---
+++ src/modules/Finance/@components/PolicyCard.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const PolicyCard = ({ policy }) => {
+  const {
+    effectiveDate,
+    policyHolders: { firstName, lastName },
+    policyHolderMailingAddress: { address1, city, state, zip },
+    summaryLedger
+  } = policy;
+
+  return (
+    <div className="card-wrapper">
+      <div className="policy-card">
+        <div className="icon-name">
+          <i className="card-icon fa fa-file-text" />
+        </div>
+        <div className="policy-details">
+          <div>
+            {policy.company} | {policy.policyNumber}
+            <a href="#" target="_blank">
+              Open Policy
+            </a>
+          </div>
+          <div>
+            <span>Effective Date:</span> {policy.effectiveDate}
+          </div>
+          <div>
+            <span>Balance Due:</span> {summaryLedger.balance.$numberDecimal}
+          </div>
+        </div>
+        <div className="policyholder">
+          {firstName} {lastName}| {address1} {city}, {state} {zip}
+        </div>
+        <div className="policy-status">
+          <div>
+            <span>Policy Status:</span> {policy.status}
+          </div>
+          <div>
+            <span>Billing Status:</span> {summaryLedger.status.displayText}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PolicyCard;
