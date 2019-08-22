45066a8aeb5e769db2e977651accecd6a65cc8f6
Joe Sutphin <jsutphin@exzeo.com>
2019-08-22 15:55:06 -0400

    update bulk payment css & html

--- src/modules/Finance/@components/BulkPayments.jsx
+++ src/modules/Finance/@components/BulkPayments.jsx
@@ -67,126 +67,131 @@ const BulkPayments = () => {
   };
 
   return (
-    <div>
-      <h3>Bulk Payments</h3>
-      <Form
-        initialValues={initialValues}
-        onSubmit={handlePayment}
-        subscription={{ submitting: true, pristine: true, values: true }}
-      >
-        {({ handleSubmit, reset, submitting, pristine }) => (
-          <div className="fade-in">
-            <section className="section-payment flex-row">
-              <Field name="cashDate" validate={validation.isRequired}>
-                {({ input, meta }) => (
-                  <Date
-                    input={input}
-                    meta={meta}
-                    label="Cash Date"
-                    styleName="input"
-                    dataTest="cashDate"
-                  />
-                )}
-              </Field>
-              <Field
-                name="batchNumber"
-                validate={validation.isDateMatchMin10('cashDate', 'YYYYMMDD')}
-              >
-                {({ input, meta }) => (
-                  <Input
-                    input={input}
-                    meta={meta}
-                    label="Batch Number"
-                    styleName="input"
-                    dataTest="batchNumber"
-                  />
-                )}
-              </Field>
-              <Field name="cashType" validate={validation.isRequired}>
-                {({ input, meta }) => (
-                  <Select
-                    input={input}
-                    meta={meta}
-                    label="Cash Type"
-                    styleName="select"
-                    answers={cashTypes}
-                    dataTest="cashType"
-                  />
-                )}
-              </Field>
-              <button
-                className="btn btn-secondary"
-                type="button"
-                form="addPayment"
-                onClick={handleActivity}
-              >
-                Start
-              </button>
-              <button
-                className="btn btn-primary"
-                type="button"
-                onClick={handleActivity}
-                disabled={pristine || submitting}
-              >
-                Stop
-              </button>
-            </section>
-            <section className="section-policy">
-              <div className="policy-input">
-                <Field name="policyNumber" validate={validation.isRequired}>
+    <div className="content-wrapper finance">
+      <div className="scroll">
+        <Form
+          initialValues={initialValues}
+          onSubmit={handlePayment}
+          subscription={{ submitting: true, pristine: true, values: true }}
+        >
+          {({ handleSubmit, reset, submitting, pristine }) => (
+            <div className="fade-in view-grid">
+              <h3 className="title">Bulk Payments</h3>
+              <section className="section-payment">
+                <Field name="cashDate" validate={validation.isRequired}>
                   {({ input, meta }) => (
-                    <Fragment>
-                      <Input
-                        input={input}
-                        meta={meta}
-                        label="Policy Number"
-                        styleName="input"
-                        dataTest="policyNumber"
-                      />
-                    </Fragment>
+                    <Date
+                      input={input}
+                      meta={meta}
+                      label="Cash Date"
+                      styleName="input view-col-2"
+                      dataTest="cashDate"
+                    />
                   )}
                 </Field>
-              </div>
-              <PolicyCard policy={policy} />
-              <Field name="amount" validate={validation.isRequired}>
-                {({ input, meta }) => (
-                  <Input
-                    input={input}
-                    meta={meta}
-                    label="Amount"
-                    styleName="input"
-                    dataTest="amount"
-                  />
-                )}
-              </Field>
-            </section>
-            <section className="section-current-values">
-              <div>
-                <div>
-                  <span>Cash Date</span> <span>Batch Number</span>Cash Type
-                  <span>Payment Description</span>
+                <Field
+                  name="batchNumber"
+                  validate={validation.isDateMatchMin10('cashDate', 'YYYYMMDD')}
+                >
+                  {({ input, meta }) => (
+                    <Input
+                      input={input}
+                      meta={meta}
+                      label="Batch Number"
+                      styleName="input view-col-3"
+                      dataTest="batchNumber"
+                    />
+                  )}
+                </Field>
+                <Field name="cashType" validate={validation.isRequired}>
+                  {({ input, meta }) => (
+                    <Select
+                      input={input}
+                      meta={meta}
+                      label="Cash Type"
+                      styleName="select view-col-2"
+                      answers={cashTypes}
+                      dataTest="cashType"
+                    />
+                  )}
+                </Field>
+                <div className="btn-wrapper view-col-2">
+                  <button
+                    className="btn btn-secondary"
+                    type="button"
+                    form="addPayment"
+                    onClick={handleActivity}
+                  >
+                    Start
+                  </button>
+                  <button
+                    className="btn btn-primary"
+                    type="button"
+                    onClick={handleActivity}
+                    disabled={pristine || submitting}
+                  >
+                    Stop
+                  </button>
                 </div>
-                <div>
-                  <span>12/12/2020</span> <span>20201212-01</span>Paper Deposit
-                  <span>Payment Received</span>
+              </section>
+              <section className="section-policy">
+                <div className="policy-input">
+                  <Field name="policyNumber" validate={validation.isRequired}>
+                    {({ input, meta }) => (
+                      <Fragment>
+                        <Input
+                          input={input}
+                          meta={meta}
+                          label="Policy Number"
+                          styleName="input"
+                          dataTest="policyNumber"
+                        />
+                      </Fragment>
+                    )}
+                  </Field>
                 </div>
-              </div>
-            </section>
-            <section className="section-payment-list">
-              <div>
+                <PolicyCard policy={policy} />
+                <Field name="amount" validate={validation.isRequired}>
+                  {({ input, meta }) => (
+                    <Input
+                      input={input}
+                      meta={meta}
+                      label="Amount"
+                      styleName="input"
+                      dataTest="amount"
+                    />
+                  )}
+                </Field>
+              </section>
+              <section className="section-current-values">
                 <div>
-                  <span>Polcy Number</span> <span>Policyholder</span>
-                  <span>Amount</span>
+                  <div>
+                    <span>Cash Date</span> <span>Batch Number</span>Cash Type
+                    <span>Payment Description</span>
+                  </div>
+                  <div>
+                    <span>12/12/2020</span> <span>20201212-01</span>Paper
+                    Deposit
+                    <span>Payment Received</span>
+                  </div>
                 </div>
+              </section>
+              <section className="section-payment-list">
                 <div>
-                  <span>12-00000001-01</span> <span>Lane Myer</span>
-                  <span>$2.00</span>
+                  <div>
+                    <span>Polcy Number</span> <span>Policyholder</span>
+                    <span>Amount</span>
+                  </div>
+                  <div>
+                    <span>12-00000001-01</span> <span>Lane Myer</span>
+                    <span>$2.00</span>
+                  </div>
                 </div>
-              </div>
-            </section>
-          </div>
-        )}
-      </Form>
+              </section>
+            </div>
+          )}
+        </Form>
+      </div>
     </div>
   );
 };
