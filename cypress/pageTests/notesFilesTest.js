5c484e74332c5a86a69d9625a4529d35598d4147
Mark Eads <meads@hcpci.com>
2019-08-22 15:07:53 -0400

    update quote state

--- cypress/pageTests/notesFilesTest.js
+++ cypress/pageTests/notesFilesTest.js
@@ -1,24 +1,45 @@
-const addNoteCheck = text => cy.findDataTag('new-note').click({ force: true })
-  .get('.new-note-file textarea[name="noteContent"]').type(text)
-  .get('button[aria-label="submit-btn form-newNote"]').click().wait('@fetchNotes')
-  .goToNav('notes').get('div').contains(text);
+const addNoteCheck = text =>
+  cy
+    .findDataTag('new-note')
+    .click({ force: true })
+    .get('.new-note-file textarea[name="noteContent"]')
+    .type(text)
+    .get('button[aria-label="submit-btn form-newNote"]')
+    .click()
+    .wait('@fetchNotes')
+    .goToNav('notes')
+    .get('div')
+    .contains(text);
 
 export default () => {
-  cy.get('td.note div').contains('Quote State Changed: Quote Created')
-    .get('td.note div').contains('Quote State Changed: Quote Qualified')
-    .get('td.note div').contains('Quote State Changed: Application Started');
+  cy.get('td.note div')
+    .contains('Quote State Changed: Quote Started')
+    .get('td.note div')
+    .contains('Quote State Changed: Quote Qualified')
+    .get('td.note div')
+    .contains('Quote State Changed: Application Started');
 
   addNoteCheck('test note one');
 
-  cy.findDataTag('new-diary').click({ force: true })
-    .findDataTag('reason').select('estate')
-    .findDataTag('assignee').select('occupancy')
-    .findDataTag('due').type('2021-01-01')
-    .findDataTag('message').type('new diary')
-    .findDataTag('note-submit').click()
-    .get('.new-diary-file').should('not.exist')
-    .get('button').contains('Diaries').click()
-    .get('td.message').contains('new diary')
+  cy.findDataTag('new-diary')
+    .click({ force: true })
+    .findDataTag('reason')
+    .select('estate')
+    .findDataTag('assignee')
+    .select('occupancy')
+    .findDataTag('due')
+    .type('2021-01-01')
+    .findDataTag('message')
+    .type('new diary')
+    .findDataTag('note-submit')
+    .click()
+    .get('.new-diary-file')
+    .should('not.exist')
+    .get('button')
+    .contains('Diaries')
+    .click()
+    .get('td.message')
+    .contains('new diary')
     .checkQuoteState('Application Started')
     .goToNav('coverage');
 
