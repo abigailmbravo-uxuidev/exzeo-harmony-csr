81a7662c81b3edfe2f4d0decde7f6cca00bfe1f6
elamison <elamison@exzeo>
2019-08-22 08:04:46 -0400

    Add Finance module.

---
+++ src/modules/Finance/@components/BulkPayments.jsx
@@ -0,0 +1,8 @@
+import React, { useState, Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+const BulkPayments = ({ auth }) => {
+  return <section data-test="section-finance"></section>;
+};
+
+export default BulkPayments;
---
+++ src/modules/Finance/index.jsx
@@ -0,0 +1,1 @@
+export { default as BulkPayments } from './@components/BulkPayments';
