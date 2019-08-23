2f541f5254f4e2be180b649baa2244ba19ee863e
Joe Sutphin <jsutphin@exzeo.com>
2019-08-23 07:29:14 -0400

    update card header

--- src/modules/Finance/@components/PolicyCard.jsx
+++ src/modules/Finance/@components/PolicyCard.jsx
@@ -13,35 +13,38 @@ const PolicyCard = ({ policy }) => {
   return (
     <div className="results">
       <div className="policy-card card">
-        <div className="icon-name">
-          <i className="card-icon fa fa-file-text" />
+        <div className="icon-name card-header">
+          <i className="icon fa fa-file-text" />
+          <h5>HO3</h5>
         </div>
-        <div className="policy-details">
-          <div>
-            {policy.company} | {policy.policyNumber}
-            <a href="#" target="_blank">
-              Open Policy
-            </a>
+        <div className="card-block">
+          <div className="policy-details">
+            <div>
+              {policy.company} | {policy.policyNumber}
+              <a href="#" target="_blank">
+                Open Policy
+              </a>
+            </div>
+            <div>
+              <span>Effective Date:</span> {policy.effectiveDate}
+            </div>
+            <div>
+              <span>Balance Due:</span> {summaryLedger.balance.$numberDecimal}
+            </div>
           </div>
-          <div>
-            <span>Effective Date:</span> {policy.effectiveDate}
+          <div className="policyholder">
+            {firstName} {lastName}| {address1} {city}, {state} {zip}
           </div>
-          <div>
-            <span>Balance Due:</span> {summaryLedger.balance.$numberDecimal}
+          <div className="policy-status">
+            <div>
+              <span>Policy Status:</span> {policy.status}
+            </div>
+            <div>
+              <span>Billing Status:</span> {summaryLedger.status.displayText}
+            </div>
           </div>
         </div>
-        <div className="policyholder">
-          {firstName} {lastName}| {address1} {city}, {state} {zip}
-        </div>
-        <div className="policy-status">
-          <div>
-            <span>Policy Status:</span> {policy.status}
-          </div>
-          <div>
-            <span>Billing Status:</span> {summaryLedger.status.displayText}
-          </div>
-        </div>
-        <div>
+        <div className="card-footer">
           <Field name="amount" validate={validation.isRequired}>
             {({ input, meta }) => (
               <Input
