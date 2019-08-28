73fb806fa01f3412de48a7c39228b1263f8b1b10
Jordan Rhodes <jsrhodes15@gmail.com>
2019-08-28 09:12:00 -0400

    Update quoteState assertion after adding AI from 'Quote Qualified' to 'Application Started'

--- cypress/support/utils.js
+++ cypress/support/utils.js
@@ -6,24 +6,35 @@
  * @returns {Object} DOM element(s) found.
  */
 Cypress.Commands.add('findDataTag', (name, { timeout = 15000, ...rest } = {}) =>
-  cy.get(`[data-test="${name}"]:not([disabled])`, { timeout, ...rest }));
+  cy.get(`[data-test="${name}"]:not([disabled])`, { timeout, ...rest })
+);
 
 /**
  * @param {string} form - Name of form to submit.
  * @returns {Object} DOM element(s) found.
  */
 Cypress.Commands.add('clickSubmit', (form = 'body', button = 'submit') =>
-  cy.get(form).within(() => cy.findDataTag(button).click({ force: true })));
+  cy.get(form).within(() => cy.findDataTag(button).click({ force: true }))
+);
 
 /**
  * Function to go to a nav tab if we're not currently there
  * @param {string} name - The route name and tab nav name.
  */
 Cypress.Commands.add('goToNav', name =>
-  cy.location()
-    .then(loc =>
-      !loc.pathname.includes(name) &&
-      cy.get('.spinner').should('not.exist')
-        .findDataTag(`nav-${name}`).find('a').click({ force: true })
-        .get('.spinner').should('not.exist'))
-    .wait(4000));
+  cy
+    .location()
+    .then(
+      loc =>
+        !loc.pathname.includes(name) &&
+        cy
+          .get('.spinner')
+          .should('not.exist')
+          .findDataTag(`nav-${name}`)
+          .find('a')
+          .click({ force: true })
+          .get('.spinner')
+          .should('not.exist')
+    )
+    .wait(1000)
+);
