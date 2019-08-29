46a534530f163eac7b47df1b7649a1d78cb7ec21
elamison <elamison@exzeo>
2019-08-29 08:24:23 -0400

    Fix BulkPayments test formatting.

--- src/modules/Finance/__tests__/BulkPayments.spec.js
+++ src/modules/Finance/__tests__/BulkPayments.spec.js
@@ -7,40 +7,41 @@ import BulkPayments from '../@components/BulkPayments';
 const today = date.toUTC();
 const initialBatchNumber = date.currentDay('YYYYMMDD');
 
-describe('BulkPayment testing', () => {
-  it('Test batch form render', async () => {});
-  const props = {
-    errorHandler: jest.fn()
-  };
+describe('BulkPayments testing', () => {
+  it('Test BulkPayments initial render', () => {
+    const props = {
+      errorHandler: jest.fn()
+    };
 
-  const { getByText, getByLabelText, getAllByText } = render(
-    <BulkPayments {...props} />
-  );
+    const { getByText, getByLabelText, getAllByText } = render(
+      <BulkPayments {...props} />
+    );
 
-  // Batch form
-  expect(getByText('Bulk Payments'));
-  expect(getByLabelText('Cash Date').value).toBe(today.format('YYYY-MM-DD'));
-  expect(getByLabelText('Batch Number').value).toBe(initialBatchNumber);
-  expect(getByLabelText('Cash Type'));
-  expect(getByText(/start/i)).toBeDisabled();
-  expect(getByText(/stop/i)).toBeDisabled();
+    // Batch form
+    expect(getByText('Bulk Payments'));
+    expect(getByLabelText('Cash Date').value).toBe(today.format('YYYY-MM-DD'));
+    expect(getByLabelText('Batch Number').value).toBe(initialBatchNumber);
+    expect(getByLabelText('Cash Type'));
+    expect(getByText(/start/i)).toBeDisabled();
+    expect(getByText(/stop/i)).toBeDisabled();
 
-  // Payment form
-  expect(getByLabelText('Policy Number')).toBeDisabled();
-  // The The CurrencyFormat component in core-ui needs to an id attribute for this to work
-  //expect(getByLabelText('Amount')).toBeDisabled();
-  expect(getByText(/apply/i)).toBeDisabled();
+    // Payment form
+    expect(getByLabelText('Policy Number')).toBeDisabled();
+    // The The CurrencyFormat component in core-ui needs to an id attribute for this to work
+    //expect(getByLabelText('Amount')).toBeDisabled();
+    expect(getByText(/apply/i)).toBeDisabled();
 
-  //Payment results table
-  expect(getAllByText('Cash Date')[1]);
-  expect(getByText(today.format('MM/DD/YYYY')));
-  expect(getAllByText('Batch Number')[1]);
-  expect(getByText(initialBatchNumber));
-  expect(getAllByText('Cash Type')[1]);
-  expect(getByText('Policy Number'));
-  expect(getByText('Policyholder'));
-  expect(getByText('Amount'));
-  expect(getByText('0 entires totaling'));
-  expect(getByText('$ 0.00'));
-  expect(getByText('Download')).toBeDisabled();
+    //Payment results table
+    expect(getAllByText('Cash Date')[1]);
+    expect(getByText(today.format('MM/DD/YYYY')));
+    expect(getAllByText('Batch Number')[1]);
+    expect(getByText(initialBatchNumber));
+    expect(getAllByText('Cash Type')[1]);
+    expect(getByText('Policy Number'));
+    expect(getByText('Policyholder'));
+    expect(getByText('Amount'));
+    expect(getByText('0 entries totaling'));
+    expect(getByText('$ 0.00'));
+    expect(getByText('Download')).toBeDisabled();
+  });
 });
