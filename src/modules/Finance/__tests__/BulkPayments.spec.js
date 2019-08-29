9c98ed5089ab708071200f3376fa8c5cca42ce45
elamison <elamison@exzeo>
2019-08-29 08:18:33 -0400

    Add tests for BulkPayments initial render.

--- src/modules/Finance/__tests__/BulkPayments.spec.js
+++ src/modules/Finance/__tests__/BulkPayments.spec.js
@@ -1,8 +1,46 @@
 import React from 'react';
 import { render, fireEvent } from 'react-testing-library';
+import { date } from '@exzeo/core-ui';
 
 import BulkPayments from '../@components/BulkPayments';
 
+const today = date.toUTC();
+const initialBatchNumber = date.currentDay('YYYYMMDD');
+
 describe('BulkPayment testing', () => {
-  it('Test batch', async () => {});
+  it('Test batch form render', async () => {});
+  const props = {
+    errorHandler: jest.fn()
+  };
+
+  const { getByText, getByLabelText, getAllByText } = render(
+    <BulkPayments {...props} />
+  );
+
+  // Batch form
+  expect(getByText('Bulk Payments'));
+  expect(getByLabelText('Cash Date').value).toBe(today.format('YYYY-MM-DD'));
+  expect(getByLabelText('Batch Number').value).toBe(initialBatchNumber);
+  expect(getByLabelText('Cash Type'));
+  expect(getByText(/start/i)).toBeDisabled();
+  expect(getByText(/stop/i)).toBeDisabled();
+
+  // Payment form
+  expect(getByLabelText('Policy Number')).toBeDisabled();
+  // The The CurrencyFormat component in core-ui needs to an id attribute for this to work
+  //expect(getByLabelText('Amount')).toBeDisabled();
+  expect(getByText(/apply/i)).toBeDisabled();
+
+  //Payment results table
+  expect(getAllByText('Cash Date')[1]);
+  expect(getByText(today.format('MM/DD/YYYY')));
+  expect(getAllByText('Batch Number')[1]);
+  expect(getByText(initialBatchNumber));
+  expect(getAllByText('Cash Type')[1]);
+  expect(getByText('Policy Number'));
+  expect(getByText('Policyholder'));
+  expect(getByText('Amount'));
+  expect(getByText('0 entires totaling'));
+  expect(getByText('$ 0.00'));
+  expect(getByText('Download')).toBeDisabled();
 });
