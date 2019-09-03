633ed1790f8bec7e35c9bab4b3d04313def98d19
elamison <elamison@exzeo>
2019-09-03 13:44:03 -0400

    Add unit tests.

--- src/modules/Finance/__tests__/BulkPayments.spec.js
+++ src/modules/Finance/__tests__/BulkPayments.spec.js
@@ -62,44 +62,7 @@ const today = date.toUTC();
 const initialBatchNumber = date.currentDay('YYYYMMDD');
 
 describe('BulkPayments testing', () => {
-  it('Test BulkPayments initial render', () => {
-    const props = {
-      errorHandler: jest.fn()
-    };
-
-    const { getByText, getByLabelText, getAllByText } = render(
-      <BulkPayments {...props} />
-    );
-
-    // Batch form
-    expect(getByText('Bulk Payments'));
-    expect(getByLabelText('Cash Date').value).toBe(today.format('YYYY-MM-DD'));
-    expect(getByLabelText('Batch Number').value).toBe(initialBatchNumber);
-    expect(getByLabelText('Cash Type'));
-    expect(getByText(/start/i)).toBeDisabled();
-    expect(getByText(/stop/i)).toBeDisabled();
-
-    // Payment form
-    expect(getByLabelText('Policy Number')).toBeDisabled();
-    // The The CurrencyFormat component in core-ui needs to an id attribute for this to work
-    //expect(getByLabelText('Amount')).toBeDisabled();
-    expect(getByText(/apply/i)).toBeDisabled();
-
-    //Payment results table
-    expect(getAllByText('Cash Date')[1]);
-    expect(getByText(today.format('MM/DD/YYYY')));
-    expect(getAllByText('Batch Number')[1]);
-    expect(getByText(initialBatchNumber));
-    expect(getAllByText('Cash Type')[1]);
-    expect(getByText('Policy Number'));
-    expect(getByText('Policyholder'));
-    expect(getByText('Amount'));
-    expect(getByText('0 entries totaling'));
-    expect(getByText('$ 0.00'));
-    expect(getByText('Download')).toBeDisabled();
-  });
-
-  it('Test BulkPayments forms', async () => {
+  it('Test BulkPayments forms - fetch error', async () => {
     const props = {
       errorHandler: jest.fn()
     };
@@ -129,12 +92,40 @@ describe('BulkPayments testing', () => {
     fireEvent.blur(await getByLabelText('Policy Number'));
     fireEvent.click(await getByText(/apply/i));
 
-    //const errorMessage = await container.querySelector('.error-message');
     expect(
       await waitForElement(() => container.querySelector('.error-message'))
     );
-    //console.log(errorElement)
-    //expect(errorMessage[0]).toBeDefined();
+
+    //debug()
+  });
+
+  it('Test BulkPayments forms - fetch error', async () => {
+    const props = {
+      errorHandler: jest.fn()
+    };
+
+    const {
+      debug,
+      getByText,
+      getByLabelText,
+      getAllByText,
+      container
+    } = render(<BulkPayments {...props} />);
+
+    fireEvent.change(await getByLabelText('Cash Type'), {
+      target: { value: 'Paper Deposit' }
+    });
+    fireEvent.change(getByLabelText('Batch Number'), {
+      target: { value: `${initialBatchNumber}99` }
+    });
+    fireEvent.blur(await getByLabelText('Batch Number'));
+    fireEvent.click(getByText(/start/i));
+    fireEvent.change(getByLabelText('Policy Number'), {
+      target: { value: '12-0000000-01' }
+    });
+    fireEvent.blur(await getByLabelText('Policy Number'));
+    fireEvent.click(await getByText(/apply/i));
+
     //debug()
   });
 });
