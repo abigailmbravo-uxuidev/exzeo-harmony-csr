edd67553dbf59f223f146dd95229e10c86c2880b
Mark Eads <meads@hcpci.com>
2019-09-03 13:09:19 -0400

    fix scroll problem for endorsement tests

--- cypress/integration/endorsements/endorsements.spec.js
+++ cypress/integration/endorsements/endorsements.spec.js
@@ -14,50 +14,50 @@ describe('CSR_policyEnd_happyPath_multiEnd1', () => {
   before('Login', () => cy.login());
   beforeEach('Set aliases', () => setRouteAliases());
 
-  // it('Bind a quote to a policy for Address 4131 Test Address, Sarasota, FL 00001 using default coverages on the quote', () => {
-  //   navigateThroughNewQuote();
-  //   fillOutCoverage();
-  //   fillOutUnderwriting();
-  //   fillOutMailingBilling();
-  //   fillOutApplication();
-  //   navigateThroughDocusign();
-  //   cy.wait(20000);
-  //   cy.getCookie('id_token').then(cookie => {
-  //     cy.get('@reviewQuote').then(function(xhr) {
-  //       const quoteNumber = xhr.request.body.data.quoteNumber;
-  //       cy.task('log', 'quoteNumber');
-  //       cy.task('log', quoteNumber);
-  //       const endpointURL = Cypress.env('SVC_URL');
-  //       cy.task('log', 'endpointURL');
-  //       cy.task('log', endpointURL);
-  //       cy.task('log', 'cookie.value');
-  //       cy.task('log', cookie.value);
-  //       bindPolicyRequest(quoteNumber, cookie.value, endpointURL).then(
-  //         response => {
-  //           cy.task('log', 'bindPolicyRequest');
-  //           cy.task('log', response.result.policyNumber);
-  //           cy.visit(`/policy/${response.result.policyNumber}/endorsements`);
-  //         }
-  //       );
-  //     });
-  //   });
-  // });
+  it('Bind a quote to a policy for Address 4131 Test Address, Sarasota, FL 00001 using default coverages on the quote', () => {
+    navigateThroughNewQuote();
+    fillOutCoverage();
+    fillOutUnderwriting();
+    fillOutMailingBilling();
+    fillOutApplication();
+    navigateThroughDocusign();
+    cy.wait(20000);
+    cy.getCookie('id_token').then(cookie => {
+      cy.get('@reviewQuote').then(function(xhr) {
+        const quoteNumber = xhr.request.body.data.quoteNumber;
+        cy.task('log', 'quoteNumber');
+        cy.task('log', quoteNumber);
+        const endpointURL = Cypress.env('SVC_URL');
+        cy.task('log', 'endpointURL');
+        cy.task('log', endpointURL);
+        cy.task('log', 'cookie.value');
+        cy.task('log', cookie.value);
+        bindPolicyRequest(quoteNumber, cookie.value, endpointURL).then(
+          response => {
+            cy.task('log', 'bindPolicyRequest');
+            cy.task('log', response.result.policyNumber);
+            cy.visit(`/policy/${response.result.policyNumber}/endorsements`);
+          }
+        );
+      });
+    });
+  });
 
   it('Test Endorsement Page', () => {
-    cy.visit(`/policy/12-1019546-01/endorsements`);
+    //cy.visit(`/policy/12-1019546-01/endorsements`);
     cy.task('log', 'Filling out Endorsements')
-      // .viewport(3000, 3000)
       .findDataTag('coverageLimits.dwelling.value')
       .type(`{selectall}{backspace}${400000}`)
       .findDataTag('coverageLimits.personalProperty.value')
       .select('50')
 
-      .findDataTag('property.burglarAlarm_true_wrapper')
-      .scrollIntoView()
       .findDataTag('property.burglarAlarm_true')
-      .click({ force: true });
+      .click({ force: true })
 
-    cy.findDataTag('coverageOptions.sinkholePerilCoverage.answer_wrapper')
+      .get('#root')
+      .scrollTo('left')
+
+      .findDataTag('coverageOptions.sinkholePerilCoverage.answer_wrapper')
       .scrollIntoView()
       .should('be.visible')
 
@@ -105,11 +105,11 @@ describe('CSR_policyEnd_happyPath_multiEnd1', () => {
       .scrollIntoView()
       .should('be.visible')
       .findDataTag('property.physicalAddress.address2')
-      .type(`{selectall}{backspace}${'APT 101'}`);
+      .type(`{selectall}{backspace}${'APT 101'}`)
 
-    // .findDataTag('modal-submit')
-    // .click({ force: true })
-    // .wait('@rateEndorsement')
+      .findDataTag('modal-submit')
+      .click({ force: true })
+      .wait('@rateEndorsement');
 
     // .findDisabledDataTag('endorsementAmount')
     // .should('have.value', '-$ 211')
