f12ac092ad046e4e960741f090312784d84b1a41
Mark Eads <meads@hcpci.com>
2019-09-03 11:32:37 -0400

    without viewport

--- cypress/integration/endorsements/endorsements.spec.js
+++ cypress/integration/endorsements/endorsements.spec.js
@@ -14,79 +14,102 @@ describe('CSR_policyEnd_happyPath_multiEnd1', () => {
   before('Login', () => cy.login());
   beforeEach('Set aliases', () => setRouteAliases());
 
-  it('Bind a quote to a policy for Address 4131 Test Address, Sarasota, FL 00001 using default coverages on the quote', () => {
-    navigateThroughNewQuote();
-    fillOutCoverage();
-    fillOutUnderwriting();
-    fillOutMailingBilling();
-    fillOutApplication();
-    navigateThroughDocusign();
-    cy.wait(20000);
-    cy.getCookie('id_token').then(cookie => {
-      cy.get('@reviewQuote').then(function(xhr) {
-        const quoteNumber = xhr.request.body.data.quoteNumber;
-        cy.task('log', 'quoteNumber');
-        cy.task('log', quoteNumber);
-        const endpointURL = Cypress.env('SVC_URL');
-        cy.task('log', 'endpointURL');
-        cy.task('log', endpointURL);
-        cy.task('log', 'cookie.value');
-        cy.task('log', cookie.value);
-        bindPolicyRequest(quoteNumber, cookie.value, endpointURL).then(
-          response => {
-            cy.task('log', 'bindPolicyRequest');
-            cy.task('log', response.result.policyNumber);
-            cy.visit(`/policy/${response.result.policyNumber}/endorsements`);
-          }
-        );
-      });
-    });
-  });
+  // it('Bind a quote to a policy for Address 4131 Test Address, Sarasota, FL 00001 using default coverages on the quote', () => {
+  //   navigateThroughNewQuote();
+  //   fillOutCoverage();
+  //   fillOutUnderwriting();
+  //   fillOutMailingBilling();
+  //   fillOutApplication();
+  //   navigateThroughDocusign();
+  //   cy.wait(20000);
+  //   cy.getCookie('id_token').then(cookie => {
+  //     cy.get('@reviewQuote').then(function(xhr) {
+  //       const quoteNumber = xhr.request.body.data.quoteNumber;
+  //       cy.task('log', 'quoteNumber');
+  //       cy.task('log', quoteNumber);
+  //       const endpointURL = Cypress.env('SVC_URL');
+  //       cy.task('log', 'endpointURL');
+  //       cy.task('log', endpointURL);
+  //       cy.task('log', 'cookie.value');
+  //       cy.task('log', cookie.value);
+  //       bindPolicyRequest(quoteNumber, cookie.value, endpointURL).then(
+  //         response => {
+  //           cy.task('log', 'bindPolicyRequest');
+  //           cy.task('log', response.result.policyNumber);
+  //           cy.visit(`/policy/${response.result.policyNumber}/endorsements`);
+  //         }
+  //       );
+  //     });
+  //   });
+  // });
 
   it('Test Endorsement Page', () => {
-    // cy.visit(`/policy/12-1019546-01/endorsements`);
+    cy.visit(`/policy/12-1019546-01/endorsements`);
     cy.task('log', 'Filling out Endorsements')
-      .viewport(3000, 3000)
+      // .viewport(3000, 3000)
       .findDataTag('coverageLimits.dwelling.value')
       .type(`{selectall}{backspace}${400000}`)
       .findDataTag('coverageLimits.personalProperty.value')
       .select('50')
 
+      .findDataTag('property.burglarAlarm_true_wrapper')
+      .scrollIntoView()
+      .findDataTag('property.burglarAlarm_true')
+      .click({ force: true });
+
+    cy.findDataTag('coverageOptions.sinkholePerilCoverage.answer_wrapper')
+      .scrollIntoView()
+      .should('be.visible')
+
       .findDataTag('coverageOptions.sinkholePerilCoverage.answer')
       .select('false')
-      .findDataTag('property.burglarAlarm_true')
-      .click({ force: true })
 
-      .findDataTag('home-scroll-section')
-      .click({ force: true })
+      .findDataTag('property.windMitigation.roofCovering_wrapper')
+      .scrollIntoView()
+      .should('be.visible')
 
       .findDataTag('property.windMitigation.roofCovering')
       .select('FBC')
+
+      .findDataTag('property.windMitigation.roofGeometry_wrapper')
+      .scrollIntoView()
+      .should('be.visible')
+
       .findDataTag('property.windMitigation.roofGeometry')
       .select('Hip')
+
+      .findDataTag('property.protectionClass_wrapper')
+      .scrollIntoView()
+      .should('be.visible')
+
       .findDataTag('property.protectionClass')
       .select('7')
 
-      .findDataTag('policyholder-scroll-section')
-      .click({ force: true })
+      .findDataTag('policyHolders[0].primaryPhoneNumber_wrapper')
+      .scrollIntoView()
+      .should('be.visible')
 
       .findDataTag('policyHolders[0].primaryPhoneNumber')
       .type(`{selectall}{backspace}${'2224445555'}`)
       .findDataTag('policyHolders[0].secondaryPhoneNumber')
       .type(`{selectall}{backspace}${'3337778888'}`)
 
-      .findDataTag('address-scroll-section')
-      .click({ force: true })
+      .findDataTag('policyHolderMailingAddress.city_wrapper')
+      .scrollIntoView()
+      .should('be.visible')
 
       .findDataTag('policyHolderMailingAddress.address2')
       .type(`{selectall}{backspace}${'APT 101'}`)
-      .findDataTag('property.physicalAddress.address2')
-      .type(`{selectall}{backspace}${'APT 101'}`)
 
-      .findDataTag('modal-submit')
-      .click({ force: true })
-      .wait('@rateEndorsement')
-      .viewport(1000, 660);
+      .findDataTag('property.physicalAddress.city_wrapper')
+      .scrollIntoView()
+      .should('be.visible')
+      .findDataTag('property.physicalAddress.address2')
+      .type(`{selectall}{backspace}${'APT 101'}`);
+
+    // .findDataTag('modal-submit')
+    // .click({ force: true })
+    // .wait('@rateEndorsement')
 
     // .findDisabledDataTag('endorsementAmount')
     // .should('have.value', '-$ 211')
