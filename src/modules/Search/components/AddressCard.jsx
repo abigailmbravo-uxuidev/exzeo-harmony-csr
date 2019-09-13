23a43d9ad8c9c2ddc13eae270b57303f122e1f06
Joe Sutphin <jsutphin@exzeo.com>
2019-09-13 18:35:55 -0400

    update address cards

--- src/modules/Search/components/AddressCard.jsx
+++ src/modules/Search/components/AddressCard.jsx
@@ -2,27 +2,33 @@ import React from 'react';
 
 function AddressCard({ handleKeyPress, handleClick, address, product }) {
   return (
-    <li id={address.id} tabIndex="0" onKeyPress={handleKeyPress}>
-      <a
-        id={address.physicalAddress.address1}
-        data-test={address.physicalAddress.address1}
-        className={address.physicalAddress.address1}
-        onClick={handleClick}
-        data-url={`/quote/new/${address.physicalAddress.state}/${product}/${address.id}`}
-      >
+    <div
+      tabIndex="0"
+      onKeyPress={handleKeyPress}
+      data-test={address.physicalAddress.address1}
+      id={address.id}
+      className={`card ${address.physicalAddress.address1}`}
+      onClick={handleClick}
+      data-url={`/quote/new/${address.physicalAddress.state}/${product}/${address.id}`}
+    >
+      <div id={address.physicalAddress.address1} className="icon-name">
         <i className="card-icon fa fa-map-marker" />
-        <section>
-          <h4>{address.physicalAddress.address1}</h4>
-          <p>
-            {address.physicalAddress.city}, {address.physicalAddress.state}{' '}
-            {address.physicalAddress.zip}
-          </p>
-        </section>
-        <footer>
-          <i className="footer-icon fa fa-chevron-right" />
-        </footer>
-      </a>
-    </li>
+      </div>
+      <section>
+        <div className="title">
+          <h4>
+            {address.physicalAddress.address1}&nbsp;|&nbsp;
+            <span className="propertyAddress">
+              {address.physicalAddress.city},&nbsp;
+              {address.physicalAddress.state}&nbsp;{address.physicalAddress.zip}
+            </span>
+          </h4>
+        </div>
+      </section>
+      <footer>
+        <i className="footer-icon fa fa-chevron-right" />
+      </footer>
+    </div>
   );
 }
 
