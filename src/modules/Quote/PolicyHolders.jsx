5969c00ee623f7cc096e453fccdb6edc652cafbc
Mark Eads <meads@hcpci.com>
2019-09-20 13:58:40 -0400

    hide checkbox if there's no secondary PH

--- src/modules/Quote/PolicyHolders.jsx
+++ src/modules/Quote/PolicyHolders.jsx
@@ -5,8 +5,10 @@ import { PolicyHolder } from '@exzeo/core-ui/src/@Harmony';
 
 import PolicyHoldersWatcher from './PolicyHoldersWatcher';
 
-const PolicyHolders = ({ config, initialValues, size }) => {
+const PolicyHolders = ({ config, initialValues, size, ...props }) => {
   const { watchField } = config.extendedProperties;
+
+  console.log(props);
   return (
     <section
       className={classNames('policyholder-component', size)}
@@ -26,17 +28,19 @@ const PolicyHolders = ({ config, initialValues, size }) => {
           title="Secondary Policyholder"
           isPrimary={false}
           showSecondaryPhone
-          renderWatchField={() => (
-            <div className="check-box-wrapper">
-              <Field
-                name={watchField}
-                component="input"
-                type="checkbox"
-                data-test={watchField}
-              />
-              <label htmlFor={watchField}> Remove</label>
-            </div>
-          )}
+          renderWatchField={() =>
+            initialValues.policyHolders.length > 1 ? (
+              <div className="check-box-wrapper">
+                <Field
+                  name={watchField}
+                  component="input"
+                  type="checkbox"
+                  data-test={watchField}
+                />
+                <label htmlFor={watchField}> Remove</label>
+              </div>
+            ) : null
+          }
         />
       </section>
       <PolicyHoldersWatcher
