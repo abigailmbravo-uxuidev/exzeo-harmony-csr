e034e6dddc4a42356d67fc08184bd67f065b8b31
Mark Eads <meads@hcpci.com>
2019-09-23 13:46:35 -0400

    set sinkhole to null

--- src/modules/Policy/utilities.js
+++ src/modules/Policy/utilities.js
@@ -66,7 +66,7 @@ export function formatEndorsementData(data, timezone) {
   } else {
     calculatedData.deductibles.sinkhole = {
       ...calculatedData.deductibles.sinkhole,
-      value: 0
+      value: null
     };
   }
 
