f44a11820384e25bc90c88021356cd5bd452607a
Mark Eads <meads@hcpci.com>
2019-09-30 16:48:41 -0400

    use removeTerm from notes utils

--- src/utilities/format.js
+++ src/utilities/format.js
@@ -1,7 +1,2 @@
 export const formatUrl = url =>
   !/^https?:\/\//i.test(url) ? `http://${url}` : url;
-
-export const removeTerm = id =>
-  id && id.replace
-    ? id.replace(/(\d{2}-\d{7})-\d{2}/g, (_, group) => group)
-    : id;
