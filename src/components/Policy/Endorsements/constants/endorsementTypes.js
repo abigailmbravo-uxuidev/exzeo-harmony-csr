7a7cfcb1f1a3d5c0300b6a9f45040d3681944af7
Mark Eads <meads@hcpci.com>
2019-10-07 15:29:22 -0400

    remove unused files

--- src/components/Policy/Endorsements/constants/endorsementTypes.js
+++
@@ -1,9 +0,0 @@
-export const premiumEndorsementList = [
-  'Coverage Endorsement',
-  'Deductible Endorsement',
-  'Surcharge Endorsement',
-  'Discount Endorsement',
-  'Wind Mitigation Endorsement',
-  'Home / Location Endorsement',
-  'Multiple Endorsements Endorsement'
-];
