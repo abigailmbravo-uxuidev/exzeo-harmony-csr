ad2c2248a68e3b18e95f700c119ee5f504832afc
Mark Eads <meads@hcpci.com>
2019-10-25 09:55:46 -0400

    update for undefined

--- src/modules/Policy/BillingTable.jsx
+++ src/modules/Policy/BillingTable.jsx
@@ -8,9 +8,8 @@ const BillingTable = ({ initialValues }) => {
     let billToName = '';
 
     if (initialValues.billToType === 'Additional Interest') {
-      const ai = additionalInterests.find(
-        p => initialValues.billToId === p._id
-      );
+      const ai =
+        additionalInterests.find(p => initialValues.billToId === p._id) || {};
       billToName = `${ai.type}: ${ai.name1} ${ai.name2}`;
     } else {
       const ph = policyHolders.find(p => initialValues.billToId === p._id);
