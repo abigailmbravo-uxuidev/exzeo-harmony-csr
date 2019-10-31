c95632ed1d55966c59325710731a13aa72262b8f
Mark Eads <meads@hcpci.com>
2019-10-31 10:32:51 -0400

    update logic

--- src/modules/Policy/EndorsementsWatcherAF3.jsx
+++ src/modules/Policy/EndorsementsWatcherAF3.jsx
@@ -1,9 +1,9 @@
 // @ts-nocheck
 import React from 'react';
-import { OnChangeListener, Field } from '@exzeo/core-ui/src';
+import { OnChangeListener, Field, date } from '@exzeo/core-ui/src';
 import _get from 'lodash/get';
 
-const EndorsementsWatcherAF3 = ({ formValues }) => {
+const EndorsementsWatcherAF3 = ({ formValues, initialValues }) => {
   return (
     <React.Fragment>
       <Field
@@ -46,8 +46,9 @@ const EndorsementsWatcherAF3 = ({ formValues }) => {
               {value => {
                 if (
                   Math.ceil(
-                    _get(formValues, 'coverageLimits.building.value', 0) / 4
-                  ) > (value || 0)
+                    _get(formValues, 'coverageLimits.building.value', 0) / 4 >
+                      (value || 0)
+                  )
                 ) {
                   onChange(false);
                 }
@@ -57,10 +58,50 @@ const EndorsementsWatcherAF3 = ({ formValues }) => {
             <OnChangeListener name="coverageLimits.building.value">
               {value => {
                 if (
-                  Math.ceil(value / 4) >
-                  _get(formValues, 'coverageLimits.personalProperty.value', 0)
+                  Math.ceil(
+                    value / 4 >
+                      (_get(
+                        formValues,
+                        'coverageLimits.personalProperty.value',
+                        0
+                      ) || 0)
+                  )
                 ) {
                   onChange(false);
+                }
+              }}
+            </OnChangeListener>
+          </React.Fragment>
+        )}
+      </Field>
+      <Field name="coverageLimits.increasedCompliance.value" subscription={{}}>
+        {({ input: { onChange } }) => (
+          <React.Fragment>
+            <OnChangeListener name="property.floodZone">
+              {value => {
+                const isBeforeDate = date.isBeforeDate(
+                  _get(formValues, 'effectiveDate'),
+                  '2019-10-01',
+                  date.FORMATS.SECONDARY
+                );
+                if (isBeforeDate && value === 'Z') {
+                  onChange('0');
+                } else if (isBeforeDate) {
+                  onChange(30000);
+                }
+              }}
+            </OnChangeListener>
+            <OnChangeListener name="property.floodterritory">
+              {value => {
+                const isAfterDate = date.isAfterDate(
+                  _get(formValues, 'effectiveDate'),
+                  '2019-09-30',
+                  date.FORMATS.SECONDARY
+                );
+                if (isAfterDate && value === '45000') {
+                  onChange('0');
+                } else if (isAfterDate) {
+                  onChange(30000);
                 }
               }}
             </OnChangeListener>
