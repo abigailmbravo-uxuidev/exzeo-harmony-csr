977d726e7cd7405bc7bf046ec808911ffae254bd
elamison <elamison@exzeo>
2019-11-11 13:13:42 -0500

    Fix Addresses in Agency Overview - HAR-7106.

--- src/modules/Agency/components/AddressView.jsx
+++ src/modules/Agency/components/AddressView.jsx
@@ -3,22 +3,6 @@ import React from 'react';
 export const AddressView = ({ agencyBranchData, territoryManagers }) => {
   return (
     <React.Fragment>
-      <div className="physical-address" data-test="physical-address">
-        <h4>Physical Address</h4>
-        <div className="address flex-col">
-          <label>Address</label>
-          <div>
-            {agencyBranchData.physicalAddress.address1}
-            {agencyBranchData.physicalAddress.address2
-              ? `, ${agencyBranchData.physicalAddress.address2}`
-              : ''}
-            <br />
-            {agencyBranchData.physicalAddress.city},{' '}
-            {agencyBranchData.physicalAddress.state}{' '}
-            {agencyBranchData.physicalAddress.zip}
-          </div>
-        </div>
-      </div>
       <div className="mailing-address" data-test="mailing-address">
         <h4>Mailing Address</h4>
         <div className="address form-group">
@@ -32,6 +16,22 @@ export const AddressView = ({ agencyBranchData, territoryManagers }) => {
             {agencyBranchData.mailingAddress.city},{' '}
             {agencyBranchData.mailingAddress.state}{' '}
             {agencyBranchData.mailingAddress.zip}
+          </div>
+        </div>
+      </div>
+      <div className="physical-address" data-test="physical-address">
+        <h4>Physical Address</h4>
+        <div className="address flex-col">
+          <label>Address</label>
+          <div>
+            {agencyBranchData.physicalAddress.address1}
+            {agencyBranchData.physicalAddress.address2
+              ? `, ${agencyBranchData.physicalAddress.address2}`
+              : ''}
+            <br />
+            {agencyBranchData.physicalAddress.city},{' '}
+            {agencyBranchData.physicalAddress.state}{' '}
+            {agencyBranchData.physicalAddress.zip}
           </div>
         </div>
         <div className="county form-group" data-test="county">
