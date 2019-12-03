4bf6293f27338e551d4e2c7044fa5a5e7e67be8d
Jordan Rhodes <jsrhodes15@gmail.com>
2019-12-03 13:50:39 -0500

    Extend retry period to 2 min waiting for quote to have an envelopeId

--- cypress/helpers/requests.js
+++ cypress/helpers/requests.js
@@ -83,7 +83,7 @@ export function retrieveQuote(quoteNumber, apiUrl, token) {
 }
 
 const WAIT_TIME_MS = 2000;
-const RETRY_MAX = 30;
+const RETRY_MAX = 60;
 
 export function envelopeIdCheck(quoteNumber, apiUrl, token, attemptNumber = 0) {
   // Custom functions should return a 'cy chain' to be able to enforce order of ops
@@ -96,7 +96,7 @@ export function envelopeIdCheck(quoteNumber, apiUrl, token, attemptNumber = 0) {
     assert.isBelow(
       attemptNumber,
       RETRY_MAX,
-      'Retries waiting for envelopeId to exist on quote'
+      "Number of retries to 'retrieveQuote' waiting for envelopeId to exist on quote"
     );
     cy.wait(WAIT_TIME_MS);
     envelopeIdCheck(quoteNumber, apiUrl, token, attemptNumber + 1);
