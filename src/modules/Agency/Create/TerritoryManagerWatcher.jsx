23c10c6db8a16cf07335b4a30bc6f79731085736
Mark Eads <meads@hcpci.com>
2020-02-03 15:12:22 -0500

    remove unused lodash get

--- src/modules/Agency/Create/TerritoryManagerWatcher.jsx
+++ src/modules/Agency/Create/TerritoryManagerWatcher.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { OnChangeListener, Field, useField } from '@exzeo/core-ui';
-import _get from 'lodash/get';
 import { setTerritoryManager, setCounty } from './utilities';
 
 const TerritoryManager = ({
