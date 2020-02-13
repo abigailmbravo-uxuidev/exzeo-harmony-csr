fcc3164a9808ebba7ecd159cbf1540a8c47c1a77
Mark Eads <meads@hcpci.com>
2020-02-13 15:46:23 -0500

    fix merge issue

---
+++ src/modules/Agency/__tests__/AgencySideNav.rtl.spec.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { renderWithForm, mockServiceRunner } from '../../../test-utils';
+
+import AgencySideNav from '../AgencySideNav';
+
+mockServiceRunner([]);
+
+describe('Testing the Agency Side-Nav', () => {
+  it('POS:Note Button is disabled while creating an agency', () => {
+    const props = {
+      agencyCode: 'new',
+      match: {
+        url: ''
+      }
+    };
+    const { getByTestId } = renderWithForm(<AgencySideNav {...props} />);
+
+    expect(getByTestId('new-note')).toHaveAttribute('disabled');
+  });
+
+  it('POS:Note Button is active while an agency is selected', () => {
+    const props = {
+      agencyCode: '1',
+      match: {
+        url: ''
+      }
+    };
+    const { getByTestId } = renderWithForm(<AgencySideNav {...props} />);
+
+    expect(getByTestId('new-note')).not.toHaveAttribute('disabled');
+  });
+});
