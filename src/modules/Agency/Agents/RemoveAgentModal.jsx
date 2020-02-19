5106a617c5f9a4a327c4351412d727d0bf290d86
Mark Eads <meads@hcpci.com>
2020-02-19 16:26:10 -0500

    change remove agent modal to use final form

--- src/modules/Agency/Agents/RemoveAgentModal.jsx
+++ src/modules/Agency/Agents/RemoveAgentModal.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { reduxForm } from 'redux-form';
-import { Button } from '@exzeo/core-ui';
+import { Form, Button } from '@exzeo/core-ui';
 
 export class RemoveAgentModal extends Component {
   render() {
@@ -9,55 +8,60 @@ export class RemoveAgentModal extends Component {
       initialValues,
       disabled,
       handleCancel,
-      handleConfirm,
-      handleSubmit,
-      submitting
+      handleConfirm
     } = this.props;
 
     return (
       <div className="modal agent-remove">
-        <form onSubmit={handleSubmit(handleConfirm)}>
-          <div className="card">
-            <div className="card-header">
-              <h4>
-                <i className="fa fa-remove" /> Remove Agent
-              </h4>
-            </div>
-            <div className="card-block">
-              <section className="agent-details">
-                <h4>
-                  Are you sure you want to remove{' '}
-                  {`${initialValues.firstName} ${initialValues.lastName}`} from{' '}
-                  {agencyName}?
-                </h4>
-              </section>
-            </div>
-            <div className="card-footer">
-              <div className="btn-footer">
-                <Button
-                  className={Button.constants.classNames.secondary}
-                  data-test="modal-cancel"
-                  onClick={() => handleCancel()}
-                >
-                  No
-                </Button>
-                <Button
-                  className={Button.constants.classNames.primary}
-                  data-test="modal-submit"
-                  type="submit"
-                  disabled={disabled || submitting}
-                >
-                  Yes
-                </Button>
+        <Form
+          id="RemoveAgent"
+          initialValues={initialValues}
+          onSubmit={handleConfirm}
+          subscription={{ submitting: true }}
+        >
+          {({ handleSubmit, submitting }) => (
+            <form onSubmit={handleSubmit}>
+              <div className="card">
+                <div className="card-header">
+                  <h4>
+                    <i className="fa fa-remove" /> Remove Agent
+                  </h4>
+                </div>
+                <div className="card-block">
+                  <section className="agent-details">
+                    <h4>
+                      Are you sure you want to remove{' '}
+                      {`${initialValues.firstName} ${initialValues.lastName}`}{' '}
+                      from {agencyName}?
+                    </h4>
+                  </section>
+                </div>
+                <div className="card-footer">
+                  <div className="btn-footer">
+                    <Button
+                      className={Button.constants.classNames.secondary}
+                      data-test="modal-cancel"
+                      onClick={handleCancel}
+                    >
+                      No
+                    </Button>
+                    <Button
+                      className={Button.constants.classNames.primary}
+                      data-test="modal-submit"
+                      type="submit"
+                      disabled={disabled || submitting}
+                    >
+                      Yes
+                    </Button>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        </form>
+            </form>
+          )}
+        </Form>
       </div>
     );
   }
 }
 
-export default reduxForm({
-  form: 'RemoveAgent'
-})(RemoveAgentModal);
+export default RemoveAgentModal;
