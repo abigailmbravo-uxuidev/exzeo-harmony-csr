21a1f7912f9177d79a1afe0481d133709ca87320
Mark Eads <meads@hcpci.com>
2020-04-07 12:31:23 -0400

    add section

--- src/modules/BulkMortgage/@components/BulkMortgagee.jsx
+++ src/modules/BulkMortgage/@components/BulkMortgagee.jsx
@@ -1,14 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classNames from 'classnames';
+import {
+  DIARY_TAB,
+  FILES_TAB,
+  NOTE_TAB,
+  NOTE_TYPE
+} from '../../NotesFiles/constants';
+import Notes from '../../NotesFiles/@components/Notes';
+import DiaryTable from '../../NotesFiles/@components/DiaryTable';
+import { BULK_TYPE, BULK_TYPE_LABEL } from '../constants';
 
 const BulkMortgagee = props => {
+  const [selectedTab, setSelectedTab] = useState(BULK_TYPE.policy);
   return (
     <div className="content-wrapper">
-      <div className="route-content">
-        <div className="scroll">
-          <div className="form-group survey-wrapper" role="group">
-            <h3>Bulk Mortgagee</h3>
+      <div className="scroll view-grid">
+        <h3>Bulk Mortgagee</h3>
+        <section className="section-select-type view-col-4">
+          <div className="bulk-mortgagee-wrapper btn-tabs">
+            <div className="filter-tabs">
+              <button
+                type="button"
+                className={classNames('btn btn-tab', {
+                  selected: selectedTab === BULK_TYPE.policy
+                })}
+                onClick={() => setSelectedTab(BULK_TYPE.policy)}
+              >
+                {BULK_TYPE_LABEL.policy}
+              </button>
+              <button
+                type="button"
+                className={classNames('btn btn-tab', {
+                  selected: selectedTab === BULK_TYPE.mortgagee
+                })}
+                onClick={() => setSelectedTab(BULK_TYPE.mortgagee)}
+              >
+                {BULK_TYPE_LABEL.mortgagee}
+              </button>
+            </div>
+            {selectedTab === BULK_TYPE.policy && (
+              <div>{BULK_TYPE_LABEL.policy}</div>
+            )}
+            {selectedTab === BULK_TYPE.mortgagee && (
+              <div>{BULK_TYPE_LABEL.mortgagee}</div>
+            )}
           </div>
-        </div>
+        </section>
       </div>
     </div>
   );
---
+++ src/modules/BulkMortgage/constants.js
@@ -0,0 +1,9 @@
+export const BULK_TYPE = {
+  policy: 'policy',
+  mortgagee: 'mortgagee'
+};
+
+export const BULK_TYPE_LABEL = {
+  [BULK_TYPE.policy]: 'By Policy',
+  [BULK_TYPE.mortgagee]: 'By Mortgagee'
+};
