1eb0157c075d8dbedbc3c8f58312daedc38be348
Mark Eads <meads@hcpci.com>
2020-04-08 11:12:20 -0400

    update address

--- src/modules/BulkMortgage/@components/MortgageeCard.jsx
+++ src/modules/BulkMortgage/@components/MortgageeCard.jsx
@@ -12,11 +12,7 @@ const MortgageeCard = ({ mortgagee, handleQueue }) => (
     <section className="view-col-8">
       <h4>Mortgagee {mortgagee.order + 1}</h4>
       <h5>{mortgagee.name1}</h5>
-      <p className="address">
-        {mortgagee.mailingAddress.address2
-          ? `${mortgagee.mailingAddress.address1}, ${mortgagee.mailingAddress.address2}, ${mortgagee.mailingAddress.city}, ${mortgagee.mailingAddress.state} ${mortgagee.mailingAddress.zip}`
-          : `${mortgagee.mailingAddress.address1}, ${mortgagee.mailingAddress.city}, ${mortgagee.mailingAddress.state} ${mortgagee.mailingAddress.zip}`}
-      </p>
+      <Address address={mortgagee.mailingAddress} className="address" />
       <Field
         name={`${mortgagee._id}.loanNo`}
         dataTest="loanNo"
@@ -34,13 +30,12 @@ const MortgageeCard = ({ mortgagee, handleQueue }) => (
       <label htmlFor={`${mortgagee._id}.makeBillTo`}>Make Bill To</label>
     </section>
     <section className="view-col-4">
-      <h4>{mortgagee.policynumber}</h4>
+      <h4>{mortgagee.policyNumber}</h4>
       <h5>{mortgagee.policyHolderName}</h5>
-      <p className="address">
-        {mortgagee.policyHolderMailingAddress.address2
-          ? `${mortgagee.policyHolderMailingAddress.address1}, ${mortgagee.policyHolderMailingAddress.address2}, ${mortgagee.policyHolderMailingAddress.city}, ${mortgagee.policyHolderMailingAddress.state} ${mortgagee.policyHolderMailingAddress.zip}`
-          : `${mortgagee.policyHolderMailingAddress.address1}, ${mortgagee.policyHolderMailingAddress.city}, ${mortgagee.policyHolderMailingAddress.state} ${mortgagee.policyHolderMailingAddress.zip}`}
-      </p>
+      <Address
+        address={mortgagee.policyHolderMailingAddress}
+        className="address"
+      />
       <Button
         type="button"
         size={BUTTON_SIZE.small}
