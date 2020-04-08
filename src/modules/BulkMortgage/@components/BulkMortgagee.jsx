d2995c36e883dfdd9d93652cc54823e0baab8c14
jsutphin <>
2020-04-08 07:41:43 -0400

    add initial css for bulk m

--- src/modules/BulkMortgage/@components/BulkMortgagee.jsx
+++ src/modules/BulkMortgage/@components/BulkMortgagee.jsx
@@ -89,54 +89,56 @@ const BulkMortgagee = ({ errorHandler }) => {
 
   return (
     <div className="content-wrapper">
-      <div className="scroll view-grid" style={style}>
-        <h3>Bulk Mortgagee</h3>
-        <section>
-          <div className="bulk-mortgagee-wrapper btn-tabs">
-            <div className="filter-tabs">
-              <button
-                type="button"
-                className={classNames('btn btn-tab', {
-                  selected: selectedTab === BULK_TYPE.policy
-                })}
-                onClick={() => setSelectedTab(BULK_TYPE.policy)}
-              >
-                {BULK_TYPE_LABEL.policy}
-              </button>
-              <button
-                type="button"
-                className={classNames('btn btn-tab', {
-                  selected: selectedTab === BULK_TYPE.mortgagee
-                })}
-                onClick={() => setSelectedTab(BULK_TYPE.mortgagee)}
-              >
-                {BULK_TYPE_LABEL.mortgagee}
-              </button>
-            </div>
-            {selectedTab === BULK_TYPE.policy && (
-              <div className="form-group survey-wrapper" role="group">
-                <section className="view-col-4">
-                  <MortgageeForm
-                    handleFormSubmit={noop}
-                    errorHandler={errorHandler}
-                  />
-                </section>
-                <section className="view-col-8">
-                  <SearchByPolicy
-                    handleSearchMortgagee={handleSearchMortgagee}
-                  />
-                  <SearchByPolicyResults
-                    mortgageeResults={mortgageeResults}
-                    handleQueue={handleQueue}
-                  />
-                </section>
+      <div className="scroll" style={style}>
+        <div className="view-grid">
+          <div className="title">Bulk Mortgagee</div>
+          <section>
+            <div className="bulk-mortgagee-wrapper btn-tabs">
+              <div className="filter-tabs">
+                <button
+                  type="button"
+                  className={classNames('btn btn-tab', {
+                    selected: selectedTab === BULK_TYPE.policy
+                  })}
+                  onClick={() => setSelectedTab(BULK_TYPE.policy)}
+                >
+                  {BULK_TYPE_LABEL.policy}
+                </button>
+                <button
+                  type="button"
+                  className={classNames('btn btn-tab', {
+                    selected: selectedTab === BULK_TYPE.mortgagee
+                  })}
+                  onClick={() => setSelectedTab(BULK_TYPE.mortgagee)}
+                >
+                  {BULK_TYPE_LABEL.mortgagee}
+                </button>
               </div>
-            )}
-            {selectedTab === BULK_TYPE.mortgagee && (
-              <div>{BULK_TYPE_LABEL.mortgagee}</div>
-            )}
-          </div>
-        </section>
+              {selectedTab === BULK_TYPE.policy && (
+                <div className="form-group survey-wrapper" role="group">
+                  <section className="view-col-4">
+                    <MortgageeForm
+                      handleFormSubmit={noop}
+                      errorHandler={errorHandler}
+                    />
+                  </section>
+                  <section className="view-col-8">
+                    <SearchByPolicy
+                      handleSearchMortgagee={handleSearchMortgagee}
+                    />
+                    <SearchByPolicyResults
+                      mortgageeResults={mortgageeResults}
+                      handleQueue={handleQueue}
+                    />
+                  </section>
+                </div>
+              )}
+              {selectedTab === BULK_TYPE.mortgagee && (
+                <div>{BULK_TYPE_LABEL.mortgagee}</div>
+              )}
+            </div>
+          </section>
+        </div>
       </div>
     </div>
   );
