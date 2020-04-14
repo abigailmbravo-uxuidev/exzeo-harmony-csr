618796da4629d16643e28a24ad0ddc6044844e50
Mark Eads <meads@hcpci.com>
2020-04-14 12:29:32 -0400

    add errorHandler to test

--- src/modules/BulkMortgage/__tests__/BulkMortgagee.rtl.spec.js
+++ src/modules/BulkMortgage/__tests__/BulkMortgagee.rtl.spec.js
@@ -4,10 +4,13 @@ import { waitForElement, fireEvent, wait } from '@testing-library/react';
 import { renderWithForm } from '../../../test-utils';
 
 import BulkMortgagee from '../@components/BulkMortgagee';
+import { noop } from '@exzeo/core-ui';
 
 describe('BulkMortgagee Testing', () => {
   it('POS: Renders BulkMortgagee', async () => {
-    const props = {};
+    const props = {
+      errorHandler: noop
+    };
     const { getByText, getByTestId } = renderWithForm(
       <BulkMortgagee {...props} />
     );
