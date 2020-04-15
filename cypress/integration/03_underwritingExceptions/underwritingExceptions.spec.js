654620f5a40163f56ad976cfcb2fe7957e78b229
Andrei Shatunov <ashatunov@exzeo.com>
2020-04-15 12:12:09 -0400

    update

--- cypress/integration/03_underwritingExceptions/underwritingExceptions.spec.js
+++ cypress/integration/03_underwritingExceptions/underwritingExceptions.spec.js
@@ -18,16 +18,14 @@ describe('Underwriting Error Testing', () => {
     // Fill out underwriting with bad data.
     fillOutUnderwriting(unQuestionsBAD, 'Quote Stopped');
     // Check for an error.
-    return (
-      cy
-        .get('section.msg-caution .fa-ul li')
-        .should(
-          'contain',
-          'Due to previous claims history, additional review is required.'
-        )
-        // TODO find a better way to handle the fact that we need to 'wait' on network requests. We have put the 'wait' in 'fillOutUnderwriting' for now, since it is used in a lot of places. But in this one case, we never leave the page so the next time we use 'fillOutUnderwriting' it is waiting on a call to 'getZipCodeSettings' that doesn't happen and is not expected to happen.
-        .goToNav('coverage')
-    );
+    cy.get('section.msg-caution .fa-ul li')
+      .should(
+        'contain',
+        'Due to previous claims history, additional review is required.'
+      )
+      // TODO find a better way to handle the fact that we need to 'wait' on network requests. We have put the 'wait' in 'fillOutUnderwriting' for now, since it is used in a lot of places. But in this one case, we never leave the page so the next time we use 'fillOutUnderwriting' it is waiting on a call to 'getZipCodeSettings' that doesn't happen and is not expected to happen.
+      .goToNav('coverage');
+
     // Give good data.
     fillOutUnderwriting();
     // Check that the error is gone.
@@ -37,31 +35,26 @@ describe('Underwriting Error Testing', () => {
   it('Overwriting UW Exception', () => {
     // Fill out underwriting with bad data.
     fillOutUnderwriting(unQuestionsBAD, 'Quote Stopped');
-
-    return (
-      cy
-        .get('section.msg-caution .fa-ul li label')
-        .should('contain', 'Override')
-        // Override the exception manually.
-        .get('section.msg-caution .fa-ul li input')
-        .check()
-        .get('.uw-validation-header button[type="submit"]')
-        .click()
-        .wait('@updateQuote')
-        .then(({ response }) => {
-          // Confirm that there exists an overridden exception
-          const overriddenExceptions = response.body.result.underwritingExceptions.filter(
-            ({ overridden }) => overridden
-          );
-          expect(overriddenExceptions.length).to.equal(
-            1,
-            'Underwriting Exceptions count'
-          );
-        })
-        .findDataTag('quoteDetails')
-        .within(() => {
-          cy.contains('Quote Qualified').should('have.text', 'Quote Qualified');
-        })
-    );
+    cy.get('section.msg-caution .fa-ul li label')
+      .should('contain', 'Override')
+      // Override the exception manually.
+      .get('section.msg-caution .fa-ul li input')
+      .check()
+      .get('.uw-validation-header button[type="submit"]')
+      .click()
+      .wait('@updateQuote')
+      .then(({ response }) => {
+        // Confirm that there exists an overridden exception
+        const overriddenExceptions = response.body.result.underwritingExceptions.filter(
+          ({ overridden }) => overridden
+        );
+        expect(overriddenExceptions.length).to.equal(
+          1,
+          'Underwriting Exceptions count'
+        );
+      })
+      .findDataTag('quoteDetails')
+      .contains('Quote Qualified')
+      .should('have.text', 'Quote Qualified');
   });
 });
