e69aaa5d43b767482244f9e1422f3e18536510fe
Mark Eads <meads@hcpci.com>
2020-04-22 11:21:04 -0400

    update

--- src/modules/BulkMortgage/@components/MortgageeCard.jsx
+++ src/modules/BulkMortgage/@components/MortgageeCard.jsx
@@ -9,12 +9,10 @@ const MortgageeCard = ({ mortgagee, handleQueue }) => (
       <h4>Mortgagee {mortgagee.order + 1}</h4>
       <h5>{mortgagee.name1}</h5>
       <Address address={mortgagee.mailingAddress} className="address" />
-      {mortgagee.currentBillTo === 'YES' && (
-        <div className="bill-to">
-          <label>Current Bill To:</label> {mortgagee.currentBillTo}
-        </div>
-      )}
-      {mortgagee.currentBillTo !== 'YES' && (
+      <div className="bill-to">
+        <label>Current Bill To:</label> {mortgagee.currentBillTo ? 'Yes' : 'No'}
+      </div>
+      {!mortgagee.currentBillTo && (
         <div className="bill-to">
           <label htmlFor={`${mortgagee._id}.makeBillTo`}>Make Bill To</label>
           <Field
