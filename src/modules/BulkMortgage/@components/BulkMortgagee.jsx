a30911f205a062ba5cbb689e3be4b565ef0e5091
Mark Eads <meads@hcpci.com>
2020-04-29 12:50:12 -0400

    refactor

--- src/modules/BulkMortgage/@components/BulkMortgagee.jsx
+++ src/modules/BulkMortgage/@components/BulkMortgagee.jsx
@@ -10,61 +10,6 @@ import { formatMortgagees } from '../utilities';
 
 const BulkMortgagee = ({ errorHandler }) => {
   const [selectedTab, setSelectedTab] = useState(BULK_MORTGAGE_TYPE.policy);
-  const [queuedMortgagees, setQueuedMortgagees] = useState([]);
-  const [mortgageeResults, setMortgageeResults] = useState([]);
-  const [showPolicySearchLoader, setShowPolicySearchLoader] = useState(false);
-  const [hasSearched, setHasSearched] = useState(false);
-
-  const handleSearchByPolicy = async data => {
-    try {
-      setShowPolicySearchLoader(true);
-      const { policyNumber, propertyAddress, lastName, product } = data;
-
-      const results = await fetchMortgageesFromPolicies({
-        product,
-        policyNumber,
-        propertyAddress,
-        lastName,
-        latestTerm: true
-      });
-      setMortgageeResults(formatMortgagees(results, queuedMortgagees));
-      setHasSearched(true);
-    } catch (ex) {
-      errorHandler(ex);
-    } finally {
-      setShowPolicySearchLoader(false);
-    }
-  };
-  const addToQueue = mortgagee => {
-    const existingMortgagee = queuedMortgagees.some(
-      m => m._id === mortgagee._id
-    );
-    if (!existingMortgagee) {
-      setQueuedMortgagees([mortgagee, ...queuedMortgagees]);
-      const filterMortgagees = mortgageeResults.filter(
-        m => m._id !== mortgagee._id
-      );
-      setMortgageeResults(filterMortgagees);
-    }
-  };
-
-  const removeFromQueue = mortgagee => {
-    const filterMortgagees = queuedMortgagees.filter(
-      m => m._id !== mortgagee._id
-    );
-    setQueuedMortgagees(filterMortgagees);
-  };
-
-  const removeAllFromQueue = () => {
-    setQueuedMortgagees([]);
-  };
-
-  const handleBulkUpdateSubmit = async () => {
-    /*
-    // TODO: need to grab AI data from the form then grab all policyNumbers and place in array
-     */
-  };
-
   return (
     <React.Fragment>
       <div className="content-wrapper scroll">
@@ -84,34 +29,9 @@ const BulkMortgagee = ({ errorHandler }) => {
                 </button>
               </div>
               {selectedTab === BULK_MORTGAGE_TYPE.policy && (
-                <ByPolicyTab
-                  errorHandler={errorHandler}
-                  handleSearchByPolicy={handleSearchByPolicy}
-                  showPolicySearchLoader={showPolicySearchLoader}
-                  mortgageeResults={mortgageeResults}
-                  addToQueue={addToQueue}
-                  handleBulkUpdateSubmit={handleBulkUpdateSubmit}
-                  hasSearched={hasSearched}
-                />
+                <ByPolicyTab errorHandler={errorHandler} />
               )}
             </div>
-          </section>
-          <QueuedMortgagees
-            queuedMortgagees={queuedMortgagees}
-            removeFromQueue={removeFromQueue}
-            removeAllFromQueue={removeAllFromQueue}
-            setQueuedMortgagees={setQueuedMortgagees}
-          />
-          <section className="btn-footer">
-            <Button
-              dataTest="bulk-mortgage-submit"
-              className={BUTTON_CLASS.primary}
-              type="submit"
-              form="BulkMortgagee"
-              disabled={!queuedMortgagees.length}
-            >
-              Update
-            </Button>
           </section>
         </div>
       </div>
