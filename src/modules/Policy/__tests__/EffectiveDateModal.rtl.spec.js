1b054384c7c944b9b0eac4546482ba8e7ff85231
Mark Eads <meads@hcpci.com>
2020-05-21 11:10:56 -0400

    set testing templates

--- src/modules/Policy/__tests__/EffectiveDateModal.rtl.spec.js
+++ src/modules/Policy/__tests__/EffectiveDateModal.rtl.spec.js
@@ -5,7 +5,7 @@ import EffectiveDateModal from '../EffectiveDateModal';
 import { noop } from '@exzeo/core-ui';
 
 describe('EffectiveDateModal testing', () => {
-  it('render', async () => {
+  it('render Effective Date Modal', async () => {
     const props = {
       initialValues: { effectiveDate: '2020-02-02' },
       closeModal: noop,
@@ -23,4 +23,14 @@ describe('EffectiveDateModal testing', () => {
 
     const { getByText } = render(<EffectiveDateModal {...props} />);
   });
+
+  it('should reset form values after modifying when closed then opened', () => {});
+
+  it('should trigger validation errors for effective date and change reasons', () => {});
+
+  it('should get rate with valid form data and toggle to submit', () => {});
+
+  it('should switch submit button back and clear out rate if form is changed', () => {});
+
+  it('should get rate and save effective date then close modal', () => {});
 });
