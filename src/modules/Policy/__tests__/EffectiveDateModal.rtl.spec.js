0c6d9fed018ebb917928f92249ea4bcd4a74d49f
Mark Eads <meads@hcpci.com>
2020-05-21 12:30:46 -0400

    tests

--- src/modules/Policy/__tests__/EffectiveDateModal.rtl.spec.js
+++ src/modules/Policy/__tests__/EffectiveDateModal.rtl.spec.js
@@ -1,30 +1,91 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, wait } from '@testing-library/react';
+import {
+  renderWithForm,
+  defaultPolicyWorkflowProps,
+  mockServiceRunner
+} from '../../../test-utils';
+import { PolicyWorkflow } from '../PolicyWorkflow';
 
-import EffectiveDateModal from '../EffectiveDateModal';
-import { noop } from '@exzeo/core-ui';
+mockServiceRunner([]);
 
-describe('EffectiveDateModal testing', () => {
-  it('render Effective Date Modal', async () => {
-    const props = {
-      initialValues: { effectiveDate: '2020-02-02' },
-      closeModal: noop,
-      effectiveDateReasons: [
-        'HUD Statement/Property Deed',
-        "Agent's Request",
-        'Internal User Error',
+describe('Change Effective Date Testing', () => {
+  const props = {
+    effectiveDateReasons: [
+      {
+        answer: 'HUD Statement/Property Deed',
+        label: 'HUD Statement/Property Deed'
+      },
+      { answer: "Agent's Request", label: "Agent's Request" },
+      { answer: 'Internal User Error', label: 'Internal User Error' },
+      { answer: 'Other', label: 'Other' }
+    ],
+    ...defaultPolicyWorkflowProps,
+    summaryLedger: {
+      currentPremium: 2058
+    },
+    match: {
+      params: { policyNumber: '12-345-67' },
+      path: '/policy/:policyNumber/coverage'
+    },
+    location: {
+      pathname: '/policy/12-345-67/coverage'
+    },
+    policyFormData: defaultPolicyWorkflowProps.policy
+  };
+
+  it('open model, test field values, then test reset values on modal close', async () => {
+    const { getByText, getByLabelText, getByTestId } = renderWithForm(
+      <PolicyWorkflow {...props} />
+    );
+
+    fireEvent.click(getByTestId('edit-effective-date'));
+    await wait(() => {
+      //edit-effective-date
+      expect(getByText('Edit Effective Date'));
+      expect(getByTestId('effective-date-change-reason').value).toBe('');
+      expect(getByTestId('effective-date-change-reason').options[0].value).toBe(
+        ''
+      );
+      expect(getByTestId('effective-date-change-reason').options[1].value).toBe(
+        'HUD Statement/Property Deed'
+      );
+      expect(getByTestId('effective-date-change-reason').options[2].value).toBe(
+        "Agent's Request"
+      );
+      expect(getByTestId('effective-date-change-reason').options[3].value).toBe(
+        'Internal User Error'
+      );
+      expect(getByTestId('effective-date-change-reason').options[4].value).toBe(
         'Other'
-      ],
-      errorHandler: noop,
-      zipCodeSettings: [],
-      currentPremium: 2507,
-      getPolicy: noop
-    };
+      ); // expect(getByTestId('effective-date-change-reason').value).toBe('');
+      expect(getByTestId('effective-date').value).toBe('2019-10-03');
+    });
 
-    const { getByText } = render(<EffectiveDateModal {...props} />);
+    fireEvent.change(getByTestId('effective-date'), {
+      target: { value: '2020-03-03' }
+    });
+
+    fireEvent.change(getByTestId('effective-date-change-reason'), {
+      target: { value: 'Internal User Error' }
+    });
+
+    await wait(() => {
+      expect(getByTestId('effective-date-change-reason').value).toBe(
+        'Internal User Error'
+      );
+      expect(getByTestId('effective-date').value).toBe('2020-03-03');
+    });
+
+    fireEvent.click(getByTestId('modal-cancel'));
+
+    fireEvent.click(getByTestId('edit-effective-date'));
+
+    await wait(() => {
+      expect(getByTestId('effective-date-change-reason').value).toBe('');
+      expect(getByTestId('effective-date').value).toBe('2019-10-03');
+    });
   });
-
-  it('should reset form values after modifying when closed then opened', () => {});
 
   it('should trigger validation errors for effective date and change reasons', () => {});
 
