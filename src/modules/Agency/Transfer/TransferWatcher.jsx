a03e3b33489a92a1a644f4606489bf733edfa0de
Mark Eads <meads@hcpci.com>
2020-08-03 08:27:25 -0400

    add debounce

--- src/modules/Agency/Transfer/TransferWatcher.jsx
+++ src/modules/Agency/Transfer/TransferWatcher.jsx
@@ -1,5 +1,24 @@
 import { useEffect } from 'react';
 import { useFormState } from '@exzeo/core-ui';
+import debounce from 'lodash/debounce';
+
+const getPolicies = async (
+  getPoliciesForAgency,
+  { policyNumber, state, product, agentCode, agencyCode, companyCode }
+) => {
+  try {
+    await getPoliciesForAgency({
+      policyNumber,
+      state,
+      product,
+      agentCode,
+      agencyCode,
+      companyCode
+    });
+  } catch (error) {}
+};
+
+const debounceFilter = debounce(getPolicies, 500);
 
 const TransferWatcher = ({ getPoliciesForAgency, agencyCode, refresh }) => {
   const {
@@ -11,19 +30,14 @@ const TransferWatcher = ({ getPoliciesForAgency, agencyCode, refresh }) => {
   } = useFormState().values;
 
   useEffect(() => {
-    const getPolicies = async () => {
-      try {
-        await getPoliciesForAgency({
-          policyNumber,
-          state,
-          product,
-          agentCode,
-          agencyCode,
-          companyCode
-        });
-      } catch (error) {}
-    };
-    getPolicies();
+    debounceFilter(getPoliciesForAgency, {
+      policyNumber,
+      state,
+      product,
+      agentCode,
+      agencyCode,
+      companyCode
+    });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [
     policyNumber,
