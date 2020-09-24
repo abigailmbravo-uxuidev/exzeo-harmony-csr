77a8248eedf30c17f8742b54aeab2145f9740aa8
Mark Eads <meads@hcpci.com>
2020-09-24 09:34:58 -0400

    update

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import { date } from '@exzeo/core-ui';
 
-const Clock = ({ timezone = date.DEFAULT_TIME_ZONE, format = 'h:mm A zz' }) => {
-  const getTime = () => date.formattedDate(undefined, format, timezone);
-  const [dateString, setDateString] = React.useState(getTime());
-  React.useEffect(() => {
-    const timeId = setInterval(() => setDateString(getTime()), 1000);
-    return function cleanup() {
-      clearInterval(timeId);
-    };
-  });
+const Clock = React.memo(
+  ({ timezone = date.DEFAULT_TIME_ZONE, format = 'h:mm A zz' }) => {
+    const getTime = () => date.formattedDate(undefined, format, timezone);
+    const [dateString, setDateString] = React.useState(getTime());
+    React.useEffect(() => {
+      const timeId = setInterval(() => setDateString(getTime()), 1000);
+      return function cleanup() {
+        clearInterval(timeId);
+      };
+    });
 
-  return (
-    <div className="property-time-wrapper">
-      <label>Property Time</label>
-      {dateString}
-    </div>
-  );
-};
+    return (
+      <div className="property-time-wrapper">
+        <label>Property Time</label>
+        {dateString}
+      </div>
+    );
+  }
+);
 
-export default React.memo(Clock);
+export default Clock;
