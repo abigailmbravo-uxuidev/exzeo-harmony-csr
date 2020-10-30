b4020d4771faeed5236716e10270c4de76e37530
Jordan Rhodes <jsrhodes15@gmail.com>
2020-10-30 09:58:30 -0400

    WIP

--- src/components/GenerateDocsForm.jsx
+++ src/components/GenerateDocsForm.jsx
@@ -52,9 +52,7 @@ export class GenerateDocsForm extends Component {
       const {
         data: { result }
       } = await callService(config, 'getDocumentPacketFiles');
-      if (window.location.pathname.includes('/notes')) {
-        updateNotes();
-      }
+      if (window.location.pathname.includes('/notes')) updateNotes();
       const { fileName, fileUrl } = result[0];
 
       // GET the file
